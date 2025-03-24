591dd8b7ab6f7f06ce0cbb87214a3c766f79021b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-03-24 23:19:55 +0100

    feat: allow to use custom connection properties

--- src/gateway/structures/GatewayManager.ts
+++ src/gateway/structures/GatewayManager.ts
@@ -1,9 +1,14 @@
 import type { Client } from "#client";
-import type { CreateGatewayManagerOptions, GatewayVersion } from "#types";
+import type {
+  CreateGatewayManagerConnectionPropertiesOptions,
+  CreateGatewayManagerOptions,
+  GatewayVersion,
+} from "#types";
 import { resolveGatewayIntents } from "#util";
 
 export class GatewayManager {
   protected _client: Client;
+  protected _connectionProperties: CreateGatewayManagerConnectionPropertiesOptions;
   protected _gatewayUrl: string;
   protected _intents: number;
   protected _token: string;
@@ -13,11 +18,17 @@ export class GatewayManager {
   readonly version: GatewayVersion;
 
   constructor(client: Client, options: CreateGatewayManagerOptions) {
-    let { token, intents, shards, version } = options;
+    let { connectionProperties, token, intents, shards, version } = options;
+    const { platform } = process;
 
     version ??= 10;
 
     this._client = client;
+    this._connectionProperties = connectionProperties ?? {
+      browser: "Linkcord",
+      device: "Linkcord",
+      os: platform,
+    };
     this._gatewayUrl = `wss://gateway.discord.gg/?v=${version}&encoding=json`;
     this._intents = resolveGatewayIntents(intents);
     this._token = token;
