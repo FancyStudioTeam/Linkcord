1e9bd65788a68c71e033588e51ddd92c843b71b4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-03-27 21:22:23 +0100

    fix: add missing rest

--- src/gateway/structures/dispatcher/handlers/messages/MESSAGE_CREATE.ts
+++ src/gateway/structures/dispatcher/handlers/messages/MESSAGE_CREATE.ts
@@ -5,7 +5,8 @@ import type { GatewayDispatchMessageCreateEvent } from "#types/gateway/events";
 // biome-ignore lint/suspicious/useAwait: Handlers must be async.
 export const handleMessageCreate = async (client: Client, gatewayEvent: GatewayDispatchMessageCreateEvent) => {
   const { d } = gatewayEvent;
-  const channelsTransformer = new ChannelsTransformer();
+  const { rest } = client;
+  const channelsTransformer = new ChannelsTransformer(rest);
   const parsedMessage = channelsTransformer.rawMessageToParsed(d);
 
   client.emit("messageCreate", parsedMessage);
