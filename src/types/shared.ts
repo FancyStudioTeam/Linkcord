a95aab8d8a0122b41b9334842e726d1e9aa940c7
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-03-31 23:30:13 +0200

    docs: update shard types comments

--- src/types/shared.ts
+++ src/types/shared.ts
@@ -1,11 +1,24 @@
 /**
- * https://discord.com/developers/docs/reference#snowflakes
+ * =================================================
+ * = Types referenced in the Discord API Reference =
+ * =================================================
  */
-export type Snowflake = string;
 
 /**
  * https://discord.com/developers/docs/reference#iso8601-datetime
  */
 export type ISO8601Date = string;
 
-export type Nullable<T> = T | null;
+/**
+ * https://discord.com/developers/docs/reference#snowflakes
+ */
+export type Snowflake = string;
+
+/**
+ * =========================================
+ * = Utility types used in some interfaces =
+ * =========================================
+ */
+
+export type Nullable<Type> = Type | null;
+export type Optional<Type> = Type | undefined;
