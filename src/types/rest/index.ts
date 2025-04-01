72b374c357f8bfc717475a91cc16796ac4f48444
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-01 21:12:35 +0200

    feat: add `RESTStatusCodes`

--- src/types/rest/index.ts
+++ src/types/rest/index.ts
@@ -236,3 +236,21 @@ export enum JSONErrorCodes {
   CannotUseAnEmojiIncludedWithThePoll = 520004,
   CannotExpireANonPollMessage = 520006,
 }
+
+/**
+ * https://discord.com/developers/docs/topics/opcodes-and-status-codes#http-http-response-codes
+ */
+export enum RESTStatusCodes {
+  Ok = 200,
+  Created = 201,
+  NoContent = 204,
+  NotModified = 304,
+  BadRequest = 400,
+  Unauthorized = 401,
+  Forbidden = 403,
+  NotFound = 404,
+  MethodNotAllowed = 405,
+  TooManyRequests = 429,
+  InternalServerError = 500,
+  GatewayUnavailable = 502,
+}
