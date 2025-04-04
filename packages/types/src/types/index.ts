cea0f897b3a1b5d7f6d40921fc54eff4b70bcf58
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-04 16:32:32 +0200

    chore: add user payload types

--- packages/types/src/types/index.ts
+++ packages/types/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./gateway/index.js";
+export * from "./payloads/index.js";
 export * from "./rest/index.js";
 export * from "./shared/index.js";
---
+++ packages/types/src/types/payloads/user.ts
@@ -0,0 +1,128 @@
+import type { Nullable, Snowflake } from "#types/shared";
+
+/**
+ * https://discord.com/developers/docs/resources/user#application-role-connection-object-application-role-connection-structure
+ */
+export interface APIApplicationRoleConnection {
+  metadata: object;
+  platform_name: Nullable<string>;
+  platform_username: Nullable<string>;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#avatar-decoration-data-object-avatar-decoration-data-structure
+ */
+export interface APIAvatarDecorationData {
+  asset: string;
+  sku_id: Snowflake;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#connection-object-connection-structure
+ */
+export interface APIConnection {
+  friend_sync: boolean;
+  id: Snowflake;
+  // TODO: Add "APIIntegration" or "APIPartialIntegration" type
+  // integrations?: APIIntegration[];
+  name: string;
+  revoked?: boolean;
+  show_activity: boolean;
+  two_way_link: boolean;
+  type: Services;
+  verified: boolean;
+  visibility: VisibilityTypes;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#user-object-user-structure
+ */
+export interface APIUser {
+  accent_color?: Nullable<number>;
+  avatar: Nullable<string>;
+  avatar_decoration_data?: Nullable<APIAvatarDecorationData>;
+  banner?: Nullable<string>;
+  bot?: boolean;
+  discriminator: string;
+  email?: Nullable<string>;
+  flags?: number;
+  global_name: Nullable<string>;
+  id: Snowflake;
+  locale?: string;
+  mfa_enabled?: boolean;
+  premium_type?: PremiumTypes;
+  public_flags?: number;
+  system?: boolean;
+  username: string;
+  verified?: boolean;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#connection-object-visibility-types
+ */
+export enum VisibilityTypes {
+  Everyone = 1,
+  None = 0,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#user-object-premium-types
+ */
+export enum PremiumTypes {
+  Nitro = 2,
+  NitroBasic = 3,
+  NitroClassic = 1,
+  None = 0,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#connection-object-services
+ */
+export enum Services {
+  AmazonMusic = "amazon-music",
+  BattleNet = "battlenet",
+  Bluesky = "bluesky",
+  BungieNet = "bungie",
+  Crunchyroll = "crunchyroll",
+  Domain = "domain",
+  EpicGames = "epicgames",
+  Facebook = "facebook",
+  Github = "github",
+  Instagram = "instagram",
+  LeagueOfLegends = "leagueoflegends",
+  Mastodon = "mastodon",
+  PayPal = "paypal",
+  PlayStationNetwork = "playstation",
+  Reddit = "reddit",
+  RiotGames = "riotgames",
+  Roblox = "roblox",
+  Skype = "skype",
+  Spotify = "spotify",
+  Steam = "steam",
+  TikTok = "tiktok",
+  Twitch = "twitch",
+  Twitter = "twitter",
+  Xbox = "xbox",
+  YouTube = "youtube",
+}
+
+/**
+ * https://discord.com/developers/docs/resources/user#user-object-user-flags
+ */
+export enum UserFlags {
+  ActiveDeveloper = 1 << 22,
+  BotHTTPInteractions = 1 << 19,
+  BugHunterLevel1 = 1 << 3,
+  BugHunterLevel2 = 1 << 14,
+  CertifiedModerator = 1 << 18,
+  EarlySupporter = 1 << 9,
+  HypeSquadEvents = 1 << 2,
+  HypeSquadHouseBalance = 1 << 8,
+  HypeSquadHouseBravery = 1 << 6,
+  HypeSquadHouseBrilliance = 1 << 7,
+  Partner = 1 << 1,
+  Staff = 1 << 0,
+  TeamUser = 1 << 10,
+  VerifiedBot = 1 << 16,
+  VerifiedBotDeveloper = 1 << 17,
+}
