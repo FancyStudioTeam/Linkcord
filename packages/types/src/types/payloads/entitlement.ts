63c2ec65c9022ae0712e62c33a1a0f7d12bf5d38
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-04 18:08:33 +0200

    style: sort properties in `APIEntitlement`

--- packages/types/src/types/payloads/entitlement.ts
+++ packages/types/src/types/payloads/entitlement.ts
@@ -4,16 +4,16 @@ import type { ISO8601Date, Nullable, Snowflake } from "#types/shared";
  * https://discord.com/developers/docs/resources/entitlement#entitlement-object-entitlement-structure
  */
 export interface APIEntitlement {
-  id: Snowflake;
-  sku_id: Snowflake;
   application_id: Snowflake;
-  user_id?: Snowflake;
-  type: EntitlementTypes;
+  consumed?: boolean;
   deleted: boolean;
-  starts_at: Nullable<ISO8601Date>;
   ends_at: Nullable<ISO8601Date>;
   guild_id?: Snowflake;
-  consumed?: boolean;
+  id: Snowflake;
+  sku_id: Snowflake;
+  starts_at: Nullable<ISO8601Date>;
+  type: EntitlementTypes;
+  user_id?: Snowflake;
 }
 
 /**
