4c65a7673b376e977d3851c0e2ef7145a8ce3152
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-05 00:11:33 +0200

    feat: add `OAuth2Scopes`

--- packages/types/src/types/payloads/miscellaneous.ts
+++ packages/types/src/types/payloads/miscellaneous.ts
@@ -23,3 +23,37 @@ export interface APIGatewayBotSessionStartLimit {
   reset_after: number;
   total: number;
 }
+
+/**
+ * https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+ */
+export enum OAuth2Scopes {
+  ActivitiesRead = "activities.read",
+  ActivitiesWrite = "activities.write",
+  ApplicationsBuildsRead = "applications.builds.read",
+  ApplicationsBuildsUpload = "applications.builds.upload",
+  ApplicationsCommands = "applications.commands",
+  ApplicationsCommandsPermissionsUpdate = "applications.commands.permissions.update",
+  ApplicationsCommandsUpdate = "applications.commands.update",
+  ApplicationsEntitlements = "applications.entitlements",
+  ApplicationsStoreUpdate = "applications.store.update",
+  Bot = "bot",
+  Connections = "connections",
+  DMChannelsRead = "dm_channels.read",
+  Email = "email",
+  GroupDMJoin = "gdm.join",
+  Guilds = "guilds",
+  GuildsJoin = "guilds.join",
+  GuildsMembersRead = "guilds.members.read",
+  Identify = "identify",
+  MessagesRead = "messages.read",
+  RelationshipsRead = "relationships.read",
+  RoleConnectionsWrite = "role_connections.write",
+  Rpc = "rpc",
+  RpcActivitiesWrite = "rpc.activities.write",
+  RpcNotificationsRead = "rpc.notifications.read",
+  RpcVoiceRead = "rpc.voice.read",
+  RpcVoiceWrite = "rpc.voice.write",
+  Voice = "voice",
+  WebhookIncoming = "webhook.incoming",
+}
