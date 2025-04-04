2983f9011cc4e05179870107005c30d6a13eebb4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-04 18:01:51 +0200

    feat: add `poll` payload types

---
+++ packages/types/src/types/payloads/poll.ts
@@ -0,0 +1,66 @@
+import type { ISO8601Date, Nullable, Snowflake } from "#types/shared";
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-object-poll-object-structure
+ */
+export interface APIPoll {
+  allow_multiselect: boolean;
+  answers: APIPollAnswer[];
+  expiry: Nullable<ISO8601Date>;
+  layout_type: LayoutType;
+  question: APIPollQuestion;
+  results?: APIPollResults;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-answer-object-poll-answer-object-structure
+ */
+export interface APIPollAnswer {
+  answer_id: number;
+  poll_media: APIPollMedia;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-results-object-poll-answer-count-object-structure
+ */
+export interface APIPollAnswerCount {
+  count: number;
+  id: number;
+  me_voted: boolean;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
+ */
+export interface APIPollMedia {
+  text?: string;
+  emoji?: APIPollMediaEmoji;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
+ */
+export interface APIPollMediaEmoji {
+  id?: Nullable<Snowflake>;
+  name?: Nullable<string>;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-results-object-poll-results-object-structure
+ */
+export interface APIPollResults {
+  answer_counts: APIPollAnswerCount[];
+  is_finalized: boolean;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/poll#poll-object-poll-object-structure
+ */
+export type APIPollQuestion = Pick<APIPollMedia, "text">;
+
+/**
+ * https://discord.com/developers/docs/resources/poll#layout-type
+ */
+export enum LayoutType {
+  Default = 1,
+}
