b5c51ab886ba145cba2ad298cc7bf7af6cf2f447
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-04 23:56:08 +0200

    feat: add `guild` payload structures

--- packages/types/src/types/payloads/guild.ts
+++ packages/types/src/types/payloads/guild.ts
@@ -1,5 +1,66 @@
+import type { StatusTypes } from "#types/gateway";
 import type { ISO8601Date, Nullable, Snowflake } from "#types/shared";
+import type { APIEmoji } from "./emoji.js";
+import type { OAuth2Scopes } from "./miscellaneous.js";
+import type { APIRole } from "./permission.js";
+import type { APISticker } from "./sticker.js";
 import type { APIAvatarDecorationData, APIUser } from "./user.js";
+
+/**
+ * https://discord.com/developers/docs/resources/guild#ban-object-ban-structure
+ */
+export interface APIBan {
+  reason: Nullable<string>;
+  user: APIUser;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-guild-structure
+ */
+export interface APIGuild {
+  afk_channel_id: Nullable<Snowflake>;
+  afk_timeout: number;
+  application_id: Nullable<Snowflake>;
+  approximate_member_count?: number;
+  approximate_presence_count?: number;
+  banner: Nullable<string>;
+  default_message_notifications: DefaultMessageNotificationLevel;
+  description: Nullable<string>;
+  discovery_splash: Nullable<string>;
+  emojis: APIEmoji[];
+  explicit_content_filter: ExplicitContentFilterLevel;
+  features: GuildFeatures[];
+  icon: Nullable<string>;
+  icon_hash?: Nullable<string>;
+  id: Snowflake;
+  incidents_data: Nullable<APIIncidentsData>;
+  max_members?: number;
+  max_presences?: Nullable<number>;
+  max_video_channel_users?: number;
+  mfa_level: MFALevel;
+  name: string;
+  nsfw_level: GuildNSFWLevel;
+  owner?: boolean;
+  owner_id: Snowflake;
+  permissions?: string;
+  preferred_locale: string;
+  premium_progress_bar_enabled?: boolean;
+  premium_subscription_count?: number;
+  premium_tier: PremiumTier;
+  public_updates_channel_id: Nullable<Snowflake>;
+  roles: APIRole[];
+  rules_channel_id: Nullable<Snowflake>;
+  safety_alerts_channel_id: Nullable<Snowflake>;
+  splash: Nullable<string>;
+  stickers?: APISticker[];
+  system_channel_flags: SystemChannelFlags;
+  system_channel_id: Nullable<Snowflake>;
+  vanity_code_url: Nullable<string>;
+  verification_level: VerificationLevel;
+  welcome_screen?: APIWelcomeScreen;
+  widget_channel_id?: Nullable<Snowflake>;
+  widget_enabled?: boolean;
+}
 
 /**
  * https://discord.com/developers/docs/resources/guild#guild-member-object-guild-member-structure
@@ -22,6 +83,232 @@ export interface APIGuildMember {
 }
 
 /**
+ * https://discord.com/developers/docs/resources/guild#guild-onboarding-object-guild-onboarding-structure
+ */
+export interface APIGuildOnboarding {
+  default_channel_ids: Snowflake[];
+  enabled: boolean;
+  guild_id: Snowflake;
+  mode: OnboardingMode;
+  prompts: APIGuildOnboardingPrompt[];
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-onboarding-object-onboarding-prompt-structure
+ */
+export interface APIGuildOnboardingPrompt {
+  id: Snowflake;
+  in_onboarding: boolean;
+  options: APIGuildOnboardingPromptOption[];
+  required: boolean;
+  single_select: boolean;
+  title: string;
+  type: PromptTypes;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-onboarding-object-prompt-option-structure
+ */
+export interface APIGuildOnboardingPromptOption {
+  channel_ids: Snowflake[];
+  description: Nullable<string>;
+  emoji?: APIEmoji;
+  emoji_animated?: boolean;
+  emoji_id?: Snowflake;
+  emoji_name?: string;
+  id: Snowflake;
+  role_ids: Snowflake[];
+  title: string;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-preview-object-guild-preview-structure
+ */
+export interface APIGuildPreview {
+  approximate_member_count: number;
+  approximate_presence_count: number;
+  description: Nullable<string>;
+  discovery_splash: Nullable<string>;
+  emojis: APIEmoji[];
+  features: GuildFeatures[];
+  icon: Nullable<string>;
+  id: Snowflake;
+  name: string;
+  splash: Nullable<string>;
+  stickers: APISticker[];
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-widget-object-guild-widget-structure
+ */
+export interface APIGuildWidget {
+  channels: APIGuildWidgetChannel[];
+  id: Snowflake;
+  instant_invite: Nullable<string>;
+  members: APIGuildWidgetMember[];
+  name: string;
+  presence_count: number;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-widget-object-guild-widget-structure
+ */
+export interface APIGuildWidgetChannel {
+  id: Snowflake;
+  name: string;
+  position: number;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-widget-object-guild-widget-structure
+ */
+export interface APIGuildWidgetMember {
+  avatar: Nullable<string>;
+  avatar_url: string;
+  discriminator: string;
+  id: Snowflake;
+  status: StatusTypes;
+  username: string;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-widget-settings-object-guild-widget-settings-structure
+ */
+export interface APIGuildWidgetSettings {
+  channel_id: Nullable<Snowflake>;
+  enabled: boolean;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#incidents-data-object-incidents-data-structure
+ */
+export interface APIIncidentsData {
+  dm_spam_detected_at?: Nullable<ISO8601Date>;
+  dms_disabled_until: Nullable<ISO8601Date>;
+  invites_disabled_until: Nullable<ISO8601Date>;
+  raid_detected_at?: Nullable<ISO8601Date>;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#integration-object-integration-structure
+ */
+export interface APIIntegration {
+  account: APIIntegrationAccount;
+  application?: APIIntegrationApplication;
+  enable_emoticons?: boolean;
+  enabled: boolean;
+  expire_behavior?: IntegrationExpireBehaviors;
+  expire_grace_period?: number;
+  id: Snowflake;
+  name: string;
+  revoked?: boolean;
+  role_id?: Snowflake;
+  scopes?: OAuth2Scopes[];
+  subscriber_count?: number;
+  synced_at?: ISO8601Date;
+  syncing?: boolean;
+  type: IntegrationTypes;
+  user?: APIUser;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#integration-account-object-integration-account-structure
+ */
+export interface APIIntegrationAccount {
+  id: Snowflake;
+  name: string;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#integration-application-object-integration-application-structure
+ */
+export interface APIIntegrationApplication {
+  bot?: APIUser;
+  description: string;
+  icon: Nullable<string>;
+  id: Snowflake;
+  name: string;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#unavailable-guild-object-example-unavailable-guild
+ */
+export interface APIUnavailableGuild {
+  id: Snowflake;
+  unavailable: boolean;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#welcome-screen-object-welcome-screen-structure
+ */
+export interface APIWelcomeScreen {
+  description: Nullable<string>;
+  welcome_channels: APIWelcomeScreenChannel[];
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#welcome-screen-object-welcome-screen-channel-structure
+ */
+export interface APIWelcomeScreenChannel {
+  channel_id: Snowflake;
+  description: string;
+  emoji_id: Nullable<Snowflake>;
+  emoji_name: Nullable<string>;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-default-message-notification-level
+ */
+export enum DefaultMessageNotificationLevel {
+  AllMessages = 0,
+  OnlyMentions = 1,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-explicit-content-filter-level
+ */
+export enum ExplicitContentFilterLevel {
+  AllMembers = 2,
+  Disabled = 0,
+  MembersWithoutRoles = 1,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-guild-features
+ */
+export enum GuildFeatures {
+  AnimatedBanner = "ANIMATED_BANNER",
+  AnimatedIcon = "ANIMATED_ICON",
+  ApplicationCommandPermissionsV2 = "APPLICATION_COMMAND_PERMISSIONS_V2",
+  AutoModeration = "AUTO_MODERATION",
+  Banner = "BANNER",
+  Community = "COMMUNITY",
+  CreatorMonetizableProvisional = "CREATOR_MONETIZABLE_PROVISIONAL",
+  CreatorStorePage = "CREATOR_STORE_PAGE",
+  DeveloperSupportServer = "DEVELOPER_SUPPORT_SERVER",
+  Discoverable = "DISCOVERABLE",
+  Featurable = "FEATURABLE",
+  InviteSplash = "INVITE_SPLASH",
+  InvitesDisabled = "INVITES_DISABLED",
+  MemberVerificationGateEnabled = "MEMBER_VERIFICATION_GATE_ENABLED",
+  MoreSoundboard = "MORE_SOUNDBOARD",
+  MoreStickers = "MORE_STICKERS",
+  News = "NEWS",
+  Partnered = "PARTNERED",
+  PreviewEnabled = "PREVIEW_ENABLED",
+  RaidAlertsDisabled = "RAID_ALERTS_DISABLED",
+  RoleIcons = "ROLE_ICONS",
+  RoleSubscriptionsAvailableForPurchase = "ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE",
+  RoleSubscriptionsEnabled = "ROLE_SUBSCRIPTIONS_ENABLED",
+  Soundboard = "SOUNDBOARD",
+  TicketedEventsEnabled = "TICKETED_EVENTS_ENABLED",
+  VIPRegions = "VIP_REGIONS",
+  VanityUrl = "VANITY_URL",
+  Verified = "VERIFIED",
+  WelcomeScreenEnabled = "WELCOME_SCREEN_ENABLED",
+}
+
+/**
  * https://discord.com/developers/docs/resources/guild#guild-member-object-guild-member-flags
  */
 export enum GuildMemberFlags {
@@ -34,4 +321,89 @@ export enum GuildMemberFlags {
   IsGuest = 1 << 4,
   StartedHomeActions = 1 << 5,
   StartedOnboarding = 1 << 3,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-guild-nsfw-level
+ */
+export enum GuildNSFWLevel {
+  AgeRestricted = 3,
+  Default = 0,
+  Explicit = 1,
+  Safe = 2,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#integration-object-integration-expire-behaviors
+ */
+export enum IntegrationExpireBehaviors {
+  Kick = 1,
+  RemoveRole = 0,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#integration-object-integration-structure
+ */
+export enum IntegrationTypes {
+  Discord = "discord",
+  GuildSubscription = "guild_subscription",
+  Twitch = "twitch",
+  YouTube = "youtube",
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-mfa-level
+ */
+export enum MFALevel {
+  Elevated = 1,
+  None = 0,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-onboarding-object-onboarding-mode
+ */
+export enum OnboardingMode {
+  Advanced = 1,
+  Default = 0,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-premium-tier
+ */
+export enum PremiumTier {
+  None = 0,
+  Tier1 = 1,
+  Tier2 = 2,
+  Tier3 = 3,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-onboarding-object-prompt-types
+ */
+export enum PromptTypes {
+  Dropdown = "DROPDOWN",
+  MultipleChoice = "MULTIPLE_CHOICE",
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-system-channel-flags
+ */
+export enum SystemChannelFlags {
+  SuppressGuildReminderNotifications = 1 << 2,
+  SuppressJoinNotifications = 1 << 0,
+  SuppressPremiumSubscriptions = 1 << 1,
+  SupressJoinNotificationReplies = 1 << 3,
+  SupressRoleSubscriptionPurchaseNotificationReplies = 1 << 5,
+  SupressRoleSubscriptionPurchaseNotifications = 1 << 4,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/guild#guild-object-verification-level
+ */
+export enum VerificationLevel {
+  High = 3,
+  Low = 1,
+  Medium = 2,
+  None = 0,
+  VeryHigh = 4,
 }
