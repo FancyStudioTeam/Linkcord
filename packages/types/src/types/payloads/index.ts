2ae0c7e93924f0637300a84b096372b6597f4130
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-04 19:19:40 +0200

    feat: add `sku` payload types

--- packages/types/src/types/payloads/index.ts
+++ packages/types/src/types/payloads/index.ts
@@ -1,6 +1,7 @@
 export * from "./entitlement.js";
 export * from "./guild.js";
 export * from "./poll.js";
+export * from "./sku.js";
 export * from "./soundboard.js";
 export * from "./sticker.js";
 export * from "./user.js";
---
+++ packages/types/src/types/payloads/sku.ts
@@ -0,0 +1,33 @@
+import type { Snowflake } from "#types/shared";
+
+/**
+ * https://discord.com/developers/docs/resources/sku#sku-object-sku-structure
+ */
+// biome-ignore lint/style/useNamingConvention: SKU is uppercase.
+export interface APISKU {
+  application_id: Snowflake;
+  flags: number;
+  id: Snowflake;
+  name: string;
+  slug: string;
+  type: SKUTypes;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/sku#sku-object-sku-flags
+ */
+export enum SKUFlags {
+  Available = 1 << 2,
+  GuildSubscription = 1 << 7,
+  UserSubscription = 1 << 8,
+}
+
+/**
+ * https://discord.com/developers/docs/resources/sku#sku-object-sku-types
+ */
+export enum SKUTypes {
+  Consumable = 3,
+  Durable = 2,
+  Subscription = 5,
+  SubscriptionGroup = 6,
+}
