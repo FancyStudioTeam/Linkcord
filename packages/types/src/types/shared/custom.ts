a6b404245495cbda2726d4779e97405aeea32cd0
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-05 18:35:55 +0200

    chore: use `@fancystudioteam/linkcord-utils`

--- packages/types/src/types/shared/custom.ts
+++ packages/types/src/types/shared/custom.ts
@@ -1,9 +1,1 @@
-export type If<Condition, TrueResult, FalseResult = null> = Condition extends true
-  ? TrueResult
-  : Condition extends false
-    ? FalseResult
-    : TrueResult | FalseResult;
-
-export type Nullable<Type> = Type | null;
-
-export type Optional<Type> = Type | undefined;
+export type * from "@fancystudioteam/linkcord-utils";
