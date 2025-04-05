1cad13f93531b510eebbe02afae74bd436542305
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-05 20:27:44 +0200

    chore: remove utils

--- packages/utils/src/types/__tests__/Nullable.test-d.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expectTypeOf, it } from "vitest";
-import type { Nullable } from "../index.js";
-
-describe("Types: Nullable", () =>
-  it("Should return 'boolean | null' when giving 'boolean'.", () => {
-    type NullableBoolean = Nullable<boolean>;
-
-    expectTypeOf<NullableBoolean>().toEqualTypeOf<boolean | null>();
-  }));
--- packages/utils/src/types/__tests__/Optional.test-d.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expectTypeOf, it } from "vitest";
-import type { Optional } from "../index.js";
-
-describe("Types: Optional", () =>
-  it("Should return 'boolean | undefined' when giving 'boolean'.", () => {
-    type OptionalBoolean = Optional<boolean>;
-
-    expectTypeOf<OptionalBoolean>().toEqualTypeOf<boolean | undefined>();
-  }));
