ac4db2e382f7edd713d0bc6be0d10528ea58c826
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-05 18:38:58 +0200

    docs: add comments to types

--- packages/utils/src/types/index.ts
+++ packages/utils/src/types/index.ts
@@ -1,9 +1,12 @@
+/** Represents a type depending on a boolean condition. */
 export type If<Condition, TrueResult, FalseResult = null> = Condition extends true
   ? TrueResult
   : Condition extends false
     ? FalseResult
     : TrueResult | FalseResult;
 
+/** Represents a type that is either the defined type or `null`. */
 export type Nullable<Type> = Type | null;
 
+/** Represents a type that is either the defined type or `undefined`. */
 export type Optional<Type> = Type | undefined;
