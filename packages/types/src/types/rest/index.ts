fa599eb223ff18c9da07e15120a437ed9c58664c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-05 23:48:29 +0200

    feat: add `sku` rest types

--- packages/types/src/types/rest/index.ts
+++ packages/types/src/types/rest/index.ts
@@ -1,5 +1,6 @@
 export * from "./gateway.js";
 export * from "./poll.js";
+export * from "./sku.js";
 export * from "./soundboard.js";
 export * from "./user.js";
 export * from "./voice.js";
---
+++ packages/types/src/types/rest/sku.ts
@@ -0,0 +1,6 @@
+import type { APISKU } from "#types/payloads";
+
+/**
+ * https://discord.com/developers/docs/resources/sku#list-skus
+ */
+export type RESTListSKUs = APISKU[];
