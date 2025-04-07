73768ed6030ca06d31bdaeb1336a4af46a1e3a43
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-07 16:43:04 +0200

    docs: document `poll` type members

--- packages/types/src/types/payloads/poll.ts
+++ packages/types/src/types/payloads/poll.ts
@@ -1,66 +1,95 @@
-import type { ISO8601Date, Nullable, Snowflake } from "#types/shared";
+import type { ISO8601Date, Nullable } from "#types/shared";
+import type { APIPartialEmoji } from "./emoji.js";
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-object-poll-object-structure
+ * Represents a Discord poll structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-object-poll-object-structure
  */
 export interface APIPoll {
+  /** Whether a user can select multiple answers of the poll. */
   allow_multiselect: boolean;
+  /** The list of answers of the poll. */
   answers: APIPollAnswer[];
+  /**
+   * The time at which the poll expires.
+   * @remarks This property is nullable to support future non-expiring polls, but currently all polls have an expiration time.
+   */
   expiry: Nullable<ISO8601Date>;
+  /**
+   * The layout type of the poll.
+   * @remarks Currently Discord only supports the {@link LayoutType.Default | Default} layout type.
+   */
   layout_type: LayoutType;
+  /**
+   * The question of the poll.
+   * @remarks This is an {@link APIPollMedia} object which only allows the {@link APIPollMedia.text | text} property.
+   */
   question: APIPollQuestion;
+  /** The results of the poll. */
   results?: APIPollResults;
 }
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-answer-object-poll-answer-object-structure
+ * Represents a Discord poll answer structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-answer-object-poll-answer-object-structure
  */
 export interface APIPollAnswer {
-  /** @remarks Only present in Gateway events. */
+  /**
+   * The id of the answer.
+   * @remarks This is only present in Gateway responses.
+   */
   answer_id: number;
+  /** The poll media of the answer. */
   poll_media: APIPollMedia;
 }
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-results-object-poll-answer-count-object-structure
+ * Represents a Discord poll answer count structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-results-object-poll-answer-count-object-structure
  */
 export interface APIPollAnswerCount {
+  /** The number of votes of the answer. */
   count: number;
+  /** The id of the answer. */
   id: number;
+  /** Whether the current user voted for the answer. */
   me_voted: boolean;
 }
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
+ * Represents a Discord poll media structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
  */
 export interface APIPollMedia {
-  emoji?: APIPollMediaEmoji;
+  /** The emoji of the poll media. */
+  emoji?: APIPartialEmoji;
+  /**
+   * The text of the poll media.
+   * @remarks This should be always non-null for questions and answers, but this may change in the future.
+   */
   text?: string;
 }
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
+ * Represents a Discord poll question structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-media-object-poll-media-object-structure
  */
-export interface APIPollMediaEmoji {
-  id?: Nullable<Snowflake>;
-  name?: Nullable<string>;
-}
+export interface APIPollQuestion extends Pick<APIPollMedia, "text"> {}
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-results-object-poll-results-object-structure
+ * Represents a Discord poll results structure.
+ * @see https://discord.com/developers/docs/resources/poll#poll-results-object-poll-results-object-structure
  */
 export interface APIPollResults {
+  /** The list of answer counts of the poll. */
   answer_counts: APIPollAnswerCount[];
+  /** Whether the answer counts have been precisely counted. */
   is_finalized: boolean;
 }
 
 /**
- * https://discord.com/developers/docs/resources/poll#poll-object-poll-object-structure
- */
-export type APIPollQuestion = Pick<APIPollMedia, "text">;
-
-/**
- * https://discord.com/developers/docs/resources/poll#layout-type
+ * The available layout types for a poll.
+ * @see https://discord.com/developers/docs/resources/poll#layout-type
  */
 export enum LayoutType {
   Default = 1,
