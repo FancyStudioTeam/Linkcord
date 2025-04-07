7e749b14d1c1f3de82a4a1e18c68d619509d5be4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-07 19:18:39 +0200

    docs: document `gateway` type members

--- packages/types/src/types/payloads/gateway.ts
+++ packages/types/src/types/payloads/gateway.ts
@@ -1,25 +1,34 @@
 /**
- * https://discord.com/developers/docs/events/gateway#get-gateway
+ * Represents a Discord gateway structure.
+ * @see https://discord.com/developers/docs/topics/gateway#get-gateway
  */
 export interface APIGateway {
+  /** A gateway url which can be used to connect the bot to the gateway. */
   url: string;
 }
 
 /**
- * https://discord.com/developers/docs/events/gateway#get-gateway-bot
+ * Represents a Discord gateway bot structure.
+ * @see https://discord.com/developers/docs/events/gateway#get-gateway-bot
  */
-export interface APIGatewayBot {
-  url: string;
+export interface APIGatewayBot extends APIGateway {
+  /** The recommended number of shards to use. */
   shards: number;
+  /** An object containing information about the session start limit. */
   session_start_limit: APIGatewayBotSessionStartLimit;
 }
 
 /**
- * https://discord.com/developers/docs/events/gateway#session-start-limit-object-session-start-limit-structure
+ * Represents a Discord gateway bot session start limit structure.
+ * @see https://discord.com/developers/docs/events/gateway#session-start-limit-object-session-start-limit-structure
  */
 export interface APIGatewayBotSessionStartLimit {
+  /** The number of `Identify` request allowed per 5 seconds. */
   max_concurrency: number;
+  /** The number of remaining sessions the bot can start. */
   remaining: number;
+  /** The time in seconds after which the limit resets. */
   reset_after: number;
+  /** The total number of sessions the bot can start. */
   total: number;
 }
