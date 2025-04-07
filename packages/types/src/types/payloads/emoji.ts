4f0f2db8c2e3439787d303356926a34d9e2bcfc4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-07 16:25:49 +0200

    docs: document `emoji` members

--- packages/types/src/types/payloads/emoji.ts
+++ packages/types/src/types/payloads/emoji.ts
@@ -2,20 +2,37 @@ import type { Nullable, Snowflake } from "#types/shared";
 import type { APIUser } from "./user.js";
 
 /**
- * https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure
+ * Represents a Discord emoji structure.
+ * @see https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure
  */
 export interface APIEmoji {
+  /** Whether the emoji is animated. */
   animated?: boolean;
+  /**
+   * Whether the emoji can be used.
+   * @remarks This may be `false` due to the loss of Server Boosts.
+   */
   available?: boolean;
+  /** The id of the emoji. */
   id: Nullable<Snowflake>;
+  /** Whether the emoji is managed. */
   managed?: boolean;
+  /**
+   * The name of the emoji.
+   * @remarks This may be `null` for reaction emoji objects.
+   */
   name: Nullable<string>;
+  /** Whether the emoji must be wrapped in colons. */
   require_colons?: boolean;
+  /** The list of role ids that can use the emoji. */
   roles?: Snowflake[];
+  /** The user who created the emoji. */
   user?: APIUser;
 }
 
 /**
- * https://discord.com/developers/docs/resources/emoji#emoji-object-emoji-structure
+ * Represents a Discord partial emoji structure.
+ * @remarks This is not documented in the Discord API documentation.
+ * @remarks Commontly used in components and polls.
  */
-export interface APIPartialEmoji extends Partial<APIEmoji> {}
+export interface APIPartialEmoji extends Partial<Pick<APIEmoji, "animated" | "id" | "name">> {}
