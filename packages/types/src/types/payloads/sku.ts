a92e16b3269a3279ad84e19e729c7926ae160773
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-08 00:00:27 +0200

    docs: document `sku` type members

--- packages/types/src/types/payloads/sku.ts
+++ packages/types/src/types/payloads/sku.ts
@@ -1,20 +1,28 @@
 import type { Snowflake } from "#types/shared";
 
 /**
- * https://discord.com/developers/docs/resources/sku#sku-object-sku-structure
+ * Represents a Discord SKU structure.
+ * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-structure
  */
 // biome-ignore lint/style/useNamingConvention:
 export interface APISKU {
+  /** The id of the parent application of the SKU. */
   application_id: Snowflake;
+  /** The flags of the SKU. */
   flags: SKUFlags;
+  /** The id of the SKU. */
   id: Snowflake;
+  /** The customer-facing name of the SKU. */
   name: string;
+  /** The system-generated url based on the SKU's name. */
   slug: string;
+  /** The type of the SKU. */
   type: SKUTypes;
 }
 
 /**
- * https://discord.com/developers/docs/resources/sku#sku-object-sku-flags
+ * The available flags for a SKU.
+ * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-flags
  */
 export enum SKUFlags {
   Available = 1 << 2,
@@ -23,7 +31,8 @@ export enum SKUFlags {
 }
 
 /**
- * https://discord.com/developers/docs/resources/sku#sku-object-sku-types
+ * The available types for a SKU.
+ * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-types
  */
 export enum SKUTypes {
   Consumable = 3,
