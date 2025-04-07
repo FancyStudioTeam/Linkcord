af651c6681d811ef9ac1f1c868e388ef09f4f514
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-07 16:02:05 +0200

    docs: document `team` member types

--- packages/types/src/types/payloads/team.ts
+++ packages/types/src/types/payloads/team.ts
@@ -2,23 +2,34 @@ import type { Nullable, Snowflake } from "#types/shared";
 import type { APIUser } from "./user.js";
 
 /**
- * https://discord.com/developers/docs/topics/teams#data-models-team-object
+ * Represents a Discord team structure.
+ * @see https://discord.com/developers/docs/topics/teams#data-models-team-object
  */
 export interface APITeam {
+  /** The icon of the team. */
   icon: Nullable<string>;
+  /** The id of the team. */
   id: Snowflake;
+  /** The list of members of the team. */
   members: APITeamMember[];
+  /** The name of the team. */
   name: string;
+  /** The id of the user who owns the team. */
   owner_user_id: Snowflake;
 }
 
 /**
- * https://discord.com/developers/docs/topics/teams#data-models-team-member-object
+ * Represents a Discord team member structure.
+ * @see https://discord.com/developers/docs/topics/teams#data-models-team-member-object
  */
 export interface APITeamMember {
+  /** The membership state of the team member. */
   membership_state: TeamMembershipState;
+  /** The role of the team member. */
   role: TeamMemberRoleTypes;
+  /** The id of the team to which the team member belongs. */
   team_id: Snowflake;
+  /** The user of the team member. */
   user: APIUser;
 }
 
