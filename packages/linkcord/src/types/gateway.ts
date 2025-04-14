74dc3d11c86c1084fc53a23cd266fed6403a0479
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-14 19:01:39 +0200

    docs: delete these comments

--- packages/linkcord/src/types/gateway.ts
+++ packages/linkcord/src/types/gateway.ts
@@ -1,27 +1,4 @@
 /**
- * La interfaz "Gateway" representa la estructura parseada de la interfaz "APIGateway".
- * Las estructuras parseadas se diferencian de las estructuras originales por estas razones:
- *   1. Las estructuras parseadas no contienen el prefijo "API" en su nombre.
- *
- *   2. Las estructuras parseadas usan la convención estructural de "camel case".
- *        Un ejemplo sería "session_start_limit" de la estructura "APIGatewayBot".
- *
- *        En su estructura parseada (GatewayBot), la propiedad sería "sessionStartLimit".
- *
- *   3. Las estructuras parseadas pueden contener algunas propiedades opcionales de su estructura original que siempre estarán presentes en la estructura parseada.
- *        Un ejemplo sería "bot" de la estructura "APIUser". En su caso, "bot" es opcional y no siempre estará presente.
- *
- *        En su estructura parseada, "bot" es una propiedad que siempre estará presente, esto debido a que se puede considerar una propiedad "importante" de la estructura.
- *
- *   4. Las propiedades de las estructuras parseadas deben estar documentadas usando la misma documentación que la estructura original.
- *
- *   5. En caso de que una propiedad sea un enum, el enum deberá ser importado desde `@fancystudioteam/linkcord-types`.
- *        Si da problemas de tipado, asegúrese de ejecutar la build de la carpeta `types`.
- *
- *        Una vez hecha la build, no se necesitará instalar de nuevo el paquete `@fancystudioteam/linkcord-types`, esto debido a que PNPM actualiza automáticamente los paquetes del workspace.
- */
-
-/**
  * Represents a parsed gateway structure.
  * @see https://discord.com/developers/docs/topics/gateway#get-gateway
  */
