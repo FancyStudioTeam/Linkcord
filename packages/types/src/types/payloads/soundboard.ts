d14f961286ee81b4732c2b6c606cbb2e822412d6
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-24 23:01:56 +0200

    docs: update `soundboard` comment types

--- packages/types/src/types/payloads/soundboard.ts
+++ packages/types/src/types/payloads/soundboard.ts
@@ -2,30 +2,24 @@ import type { Nullable, Snowflake } from "#types/shared";
 import type { APIUser } from "./user.js";
 
 /**
- * Represents a Discord soundboard sound structure.
  * @see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object-soundboard-sound-structure
  */
 export interface APISoundboardSound {
   /**
-   * Whether the sounboard sound can be used.
-   * @remarks This may be `false` due to the loss of Server Boosts.
+   * @remarks
+   * - This field value may be `false` if the guild at which the soundboard sound
+   *   belongs lost Server Boosts.
    */
   available: boolean;
-  /** The id of the custom emoji of the soundboard sound. */
   emoji_id: Nullable<Snowflake>;
-  /** The unicode character of the soundboard sound. */
   emoji_name: Nullable<string>;
-  /** The id of the guild where the soundboard sound was created. */
   guild_id?: Snowflake;
-  /** The name of the soundboard sound. */
   name: string;
-  /** The id of the soundboard sound. */
   sound_id: Snowflake;
-  /** The user who created the soundboard sound. */
   user?: APIUser;
   /**
-   * The volume of the soundboard sound.
-   * @remarks This is a double value from 0 to 1.
+   * @remarks
+   * - This field is a double value between `0` and `1`. Example: `0.75`.
    */
   volume: number;
 }
