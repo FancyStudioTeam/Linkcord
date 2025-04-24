409229e891c9b0d48b9a268087746580e2d2043c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-24 21:34:02 +0200

    docs: change `team` comment types

--- packages/types/src/types/payloads/team.ts
+++ packages/types/src/types/payloads/team.ts
@@ -2,39 +2,27 @@ import type { Nullable, Snowflake } from "#types/shared";
 import type { APIUser } from "./user.js";
 
 /**
- * Represents a Discord team structure.
  * @see https://discord.com/developers/docs/topics/teams#data-models-team-object
  */
 export interface APITeam {
-  /** The icon of the team. */
   icon: Nullable<string>;
-  /** The id of the team. */
   id: Snowflake;
-  /** The list of members of the team. */
   members: APITeamMember[];
-  /** The name of the team. */
   name: string;
-  /** The id of the user who owns the team. */
   owner_user_id: Snowflake;
 }
 
 /**
- * Represents a Discord team member structure.
  * @see https://discord.com/developers/docs/topics/teams#data-models-team-member-object
  */
 export interface APITeamMember {
-  /** The membership state of the team member. */
   membership_state: TeamMembershipState;
-  /** The role of the team member. */
   role: TeamMemberRoleTypes;
-  /** The id of the team to which the team member belongs. */
   team_id: Snowflake;
-  /** The user of the team member. */
   user: APIUser;
 }
 
 /**
- * The available role types for a team member.
  * @see https://discord.com/developers/docs/topics/teams#team-member-roles-team-member-role-types
  */
 export enum TeamMemberRoleTypes {
@@ -44,7 +32,6 @@ export enum TeamMemberRoleTypes {
 }
 
 /**
- * The available membership states for a team member.
  * @see https://discord.com/developers/docs/topics/teams#data-models-membership-state-enum
  */
 export enum TeamMembershipState {
