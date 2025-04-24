916a8977bccb40900af2f56bc917322f9746bef1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-24 23:03:20 +0200

    docs: update `sku` comment types

--- packages/types/src/types/payloads/sku.ts
+++ packages/types/src/types/payloads/sku.ts
@@ -1,27 +1,19 @@
 import type { Snowflake } from "#types/shared";
 
 /**
- * Represents a Discord SKU structure.
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-structure
  */
 // biome-ignore lint/style/useNamingConvention:
 export interface APISKU {
-  /** The id of the parent application of the SKU. */
   application_id: Snowflake;
-  /** The flags of the SKU. */
   flags: SKUFlags;
-  /** The id of the SKU. */
   id: Snowflake;
-  /** The customer-facing name of the SKU. */
   name: string;
-  /** The system-generated url based on the SKU's name. */
   slug: string;
-  /** The type of the SKU. */
   type: SKUTypes;
 }
 
 /**
- * The available flags for a SKU.
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-flags
  */
 export enum SKUFlags {
@@ -31,7 +23,6 @@ export enum SKUFlags {
 }
 
 /**
- * The available types for a SKU.
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-types
  */
 export enum SKUTypes {
