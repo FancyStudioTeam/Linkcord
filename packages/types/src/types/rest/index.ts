f8fa2abb0638169d38438f4f9b3f43823d08c241
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-26 01:19:41 +0200

    chore: delete old `rest` files

--- packages/types/src/types/rest/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./gateway.js";
-export * from "./poll.js";
-export * from "./sku.js";
-export * from "./soundboard.js";
-export * from "./user.js";
-export * from "./voice.js";
--- packages/types/src/types/rest/sku.ts
+++
@@ -1,6 +0,0 @@
-import type { APISKU } from "#types/payloads";
-
-/**
- * https://discord.com/developers/docs/resources/sku#list-skus
- */
-export type RESTListSKUs = APISKU[];
