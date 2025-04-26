44d01cc727b06e287e5b343a8b9f001f02688285
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-04-26 04:46:49 +0200

    chore!: use pascal case for `SKU`

--- packages/types/src/types/payloads/sku.ts
+++ packages/types/src/types/payloads/sku.ts
@@ -3,20 +3,19 @@ import type { Snowflake } from "#types/shared";
 /**
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-structure
  */
-// biome-ignore lint/style/useNamingConvention:
-export interface APISKU {
+export interface APISku {
   application_id: Snowflake;
-  flags: SKUFlags;
+  flags: SkuFlags;
   id: Snowflake;
   name: string;
   slug: string;
-  type: SKUTypes;
+  type: SkuTypes;
 }
 
 /**
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-flags
  */
-export enum SKUFlags {
+export enum SkuFlags {
   Available = 1 << 2,
   GuildSubscription = 1 << 7,
   UserSubscription = 1 << 8,
@@ -25,7 +24,7 @@ export enum SKUFlags {
 /**
  * @see https://discord.com/developers/docs/resources/sku#sku-object-sku-types
  */
-export enum SKUTypes {
+export enum SkuTypes {
   Consumable = 3,
   Durable = 2,
   Subscription = 5,
