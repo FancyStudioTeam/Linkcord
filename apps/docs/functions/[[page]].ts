105ff3010837773cfd191da2cef94b59cca10840
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-02 01:05:03 +0200

    chore: remove `.js` extension

--- apps/docs/functions/[[page]].ts
+++ apps/docs/functions/[[page]].ts
@@ -1,8 +1,10 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 // @ts-expect-error
 // biome-ignore lint/style/noNamespaceImport:
-import * as build from "../build/server.js";
+// biome-ignore lint/correctness/useImportExtensions:
+import * as build from "../build/server";
 
 export const onRequest = createPagesFunctionHandler({
   build,
+  mode: process.env.NODE_ENV,
 });
