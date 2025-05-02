e860cd13039f602ef0b2496afea7815c6e18ebdf
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 00:42:32 +0200

    chore: update files

--- apps/docs/types/APIMember.ts
+++ apps/docs/types/APIMember.ts
@@ -1,5 +1,9 @@
 import type { APIEnum } from "./members/APIEnum.js";
+import type { APIEnumMember } from "./members/APIEnumMember.js";
 import type { APIInterface } from "./members/APIInterface.js";
+import type { APIPropertySignature } from "./members/APIPropertySignature.js";
 import type { APITypeAlias } from "./members/APITypeAlias.js";
 
 export type AnyEntryPointMember = APIEnum | APIInterface | APITypeAlias;
+
+export type AnyMember = APIEnum | APIEnumMember | APIInterface | APIPropertySignature | APITypeAlias;
