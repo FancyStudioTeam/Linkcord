0992cf720139e6ea9ca9443e7baa4382ff35e42b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 01:22:09 +0200

    docs: delete comments

--- apps/docs/util/apiExtractor.ts
+++ apps/docs/util/apiExtractor.ts
@@ -3,16 +3,8 @@ import { cache } from "react";
 import type { APIExtractor } from "#types/APIExtractor.js";
 import type { AnyEntryPointMember } from "#types/APIMember";
 
-/**
- * Gets the API Extractor JSON schema.
- * @returns The API Extractor JSON schema.
- */
 export const getTypeSchema = async (): Promise<APIExtractor> => typesApi as APIExtractor;
 
-/**
- * Gets the API Extractor members.
- * @returns The API Extractor members.
- */
 export const _getTypeMembers = async (): Promise<AnyEntryPointMember[]> => {
   const schema = await getTypeSchema();
   const entryPoint = schema.members[0];
