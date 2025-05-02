d557f503f90f23f1f5bac35bad25e5b3f77965d2
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-02 23:13:41 +0200

    chore: delete this thing

--- apps/docs/util/responses/internalServerError.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Creates an "Internal Server Error" response.
- * @returns The created response instance.
- */
-export const notFound = (data = "Internal Server Error") =>
-  new Response(data, {
-    status: 500,
-    statusText: data,
-  });
