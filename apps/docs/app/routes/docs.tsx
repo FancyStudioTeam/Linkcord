6ac42c64d26f0674502a8cde07c74e2ba064637b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 02:43:31 +0200

    chore: update docs layout

--- apps/docs/app/routes/docs.tsx
+++ apps/docs/app/routes/docs.tsx
@@ -16,8 +16,8 @@ export default function Layout() {
   return (
     <div className="flex h-screen flex-row gap-6 p-6">
       <Sidebar members={members} />
-      <div className="flex h-full w-full items-center justify-center">
-        <div className="h-full w-full max-w-7xl">
+      <div className="flex h-full max-h-screen w-full justify-center overflow-y-auto rounded-lg border border-zinc-700 bg-zinc-900 p-6">
+        <div className="w-full max-w-5xl">
           <Outlet />
         </div>
       </div>
