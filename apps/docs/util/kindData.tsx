1a5521ec7c70564a3b5b4324a97c9c7526d4b754
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-04 00:22:51 +0200

    style(docs): update kind colors

--- apps/docs/util/kindData.tsx
+++ apps/docs/util/kindData.tsx
@@ -1,5 +1,5 @@
 import { Cuboid, ListTree, type LucideIcon, SquareChartGantt, Wrench } from "lucide-react";
-import { APIMemberKind, type AnyTopLevelKind } from "#types/APIExtractor";
+import { APIMemberKind, type AnyTopLevelKind } from "#extractor/types";
 
 export const KIND_ICONS: Record<AnyTopLevelKind, LucideIcon> = {
   [APIMemberKind.Enum]: SquareChartGantt,
@@ -11,23 +11,28 @@ export const KIND_ICONS: Record<AnyTopLevelKind, LucideIcon> = {
 export const KIND_COLORS: Record<AnyTopLevelKind, KindColors> = {
   [APIMemberKind.Enum]: {
     background: "bg-amber-950",
-    text: "text-amber-500",
+    icon: "text-amber-500",
+    text: "text-amber-400",
   },
   [APIMemberKind.Interface]: {
     background: "bg-emerald-950",
-    text: "text-emerald-500",
+    icon: "text-emerald-500",
+    text: "text-emerald-400",
   },
   [APIMemberKind.TypeAlias]: {
     background: "bg-rose-950",
-    text: "text-rose-500",
+    icon: "text-rose-500",
+    text: "text-rose-400",
   },
   [APIMemberKind.Variable]: {
     background: "bg-cyan-950",
-    text: "text-cyan-500",
+    icon: "text-cyan-500",
+    text: "text-cyan-400",
   },
 };
 
 interface KindColors {
   background: string;
+  icon: string;
   text: string;
 }
