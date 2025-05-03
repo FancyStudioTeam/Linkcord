bcdb6eb5eb1d0812785e154466dbc1934320a1a7
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 02:36:16 +0200

    chore: little updates

---
+++ apps/docs/util/links.ts
@@ -0,0 +1,6 @@
+export const LINKS = {
+  // biome-ignore lint/style/useNamingConvention:
+  Discord: "https://discord.gg/yWjeDA6ewJ",
+  // biome-ignore lint/style/useNamingConvention:
+  GitHub: "https://github.com/FancyStudioTeam/Linkcord",
+};
--- apps/docs/util/makeCodeBlock.ts
+++ apps/docs/util/makeCodeBlock.ts
@@ -1,17 +1,13 @@
 import { createHighlighter } from "shiki";
 import { createJavaScriptRegexEngine } from "shiki/engine/javascript";
 
-/**
- * Creates a code block from a string of code.
- * @param code - The code to create the code block from.
- * @returns The created HTML code block.
- */
-export const makeCodeBlock = async (code: string): Promise<string> => {
-  const highlighter = await createHighlighter({
-    engine: createJavaScriptRegexEngine(),
-    langs: ["typescript"],
-    themes: ["github-dark-default"],
-  });
+const highlighter = await createHighlighter({
+  engine: createJavaScriptRegexEngine(),
+  langs: ["typescript"],
+  themes: ["github-dark-default"],
+});
+
+export const makeCodeBlock = (code: string): string => {
   const highlightedCode = highlighter.codeToHtml(code, {
     lang: "typescript",
     theme: "github-dark-default",
