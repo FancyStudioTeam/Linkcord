2a8fccef99b60b5b6aeb54bfdd2aa3372e28b504
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 21:39:23 +0200

    chore(docs): update some util files

--- apps/docs/util/extractor.ts
+++ apps/docs/util/extractor.ts
@@ -1,5 +1,5 @@
 import { cache } from "react";
-import { APIExtractor } from "./extractor/APIExtractor.js";
+import { APIExtractor } from "#extractor/APIExtractor";
 
 export const extractor = new APIExtractor();
 export const getTypeMembers = cache(extractor.getTypesPackageMembers);
--- apps/docs/util/links.ts
+++ apps/docs/util/links.ts
@@ -4,3 +4,14 @@ export const LINKS = {
   // biome-ignore lint/style/useNamingConvention:
   GitHub: "https://github.com/FancyStudioTeam/Linkcord",
 };
+
+export const MDN_LINKS = {
+  // biome-ignore lint/style/useNamingConvention:
+  Array: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array",
+  // biome-ignore lint/style/useNamingConvention:
+  Boolean: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean",
+  // biome-ignore lint/style/useNamingConvention:
+  Number: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number",
+  // biome-ignore lint/style/useNamingConvention:
+  String: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String",
+};
