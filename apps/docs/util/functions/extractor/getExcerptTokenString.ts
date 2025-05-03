7bd8369cb6dc9fd7c9bb8f478260929fb97dfe6d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-03 22:10:55 +0200

    chore(docs): remove old extractor functions

--- apps/docs/util/functions/extractor/getExcerptTokenString.ts
+++
@@ -1,7 +0,0 @@
-import type { APIExcerptToken } from "#types/shared/APIExcerptToken";
-
-export const getExcerptTokenString = (excerptTokens: APIExcerptToken[]): string =>
-  excerptTokens
-    .map((token) => token.text)
-    .join("")
-    .trim();
