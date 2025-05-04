a8b65e8c0e02a74dc768f721727de414c7076fe5
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-04 15:48:56 +0200

    chore(docs): try resvg with WASM

--- apps/docs/util/functions/createOpenGraphImage.tsx
+++ apps/docs/util/functions/createOpenGraphImage.tsx
@@ -1,7 +1,9 @@
+import { Resvg, initWasm } from "@resvg/resvg-wasm";
+// import { Canvg } from "canvg";
 // import { Resvg } from "@resvg/resvg-js";
 import satori from "satori";
 
-export const createOpenGraphImage = async (): Promise<string> => {
+export const createOpenGraphImage = async (): Promise<Uint8Array> => {
   const { default: extraBoldFont } = await import("./fonts/JetBrainsMono_ExtraBold.ttf?arraybuffer");
   const jsx = <div>Hello, world!</div>;
   const svg = await satori(jsx, {
@@ -14,9 +16,35 @@ export const createOpenGraphImage = async (): Promise<string> => {
     height: 720,
     width: 1280,
   });
+
+  await initWasm(await fetch("https://unpkg.com/@resvg/resvg-wasm/index_bg.wasm"));
+
+  const resvg = new Resvg(svg);
+  const render = resvg.render();
+  const png = render.asPng();
+
+  return png;
+
   /*const resvg = new Resvg(svg);
   const render = resvg.render();
   const png = render.asPng();*/
 
-  return svg;
+  /*const canvas = new OffscreenCanvas(1280, 720);
+  const context = canvas.getContext("2d");
+
+  if (!context) {
+    return Buffer.alloc(0);
+  }
+
+  const data = Canvg.fromString(context, svg);
+
+  await data.render();
+
+  const blobImage = await canvas.convertToBlob({
+    type: "image/png",
+  });
+  const arrayBuffer = await blobImage.arrayBuffer();
+  const image = Buffer.from(arrayBuffer);
+
+  return image;*/
 };
