cc03f5806391360bc50fc94c132a97ab91359f8e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-04 14:59:55 +0200

    feat(docs): create dynamic og images

---
+++ apps/docs/app/routes/docs.$name.og.tsx
@@ -0,0 +1,11 @@
+import { createOpenGraphImage } from "#util/functions/createOpenGraphImage";
+
+export const loader = async () => {
+  const dynamicImage = await createOpenGraphImage();
+
+  return new Response(dynamicImage, {
+    headers: {
+      "Content-Type": "image/png",
+    },
+  });
+};
---
+++ apps/docs/util/functions/createOpenGraphImage.tsx
@@ -0,0 +1,22 @@
+import { Resvg } from "@resvg/resvg-js";
+import satori from "satori";
+
+export const createOpenGraphImage = async (): Promise<ArrayBuffer> => {
+  const { default: extraBoldFont } = await import("./fonts/JetBrainsMono_ExtraBold.ttf?arraybuffer");
+  const jsx = <div>Hello, world!</div>;
+  const svg = await satori(jsx, {
+    fonts: [
+      {
+        data: extraBoldFont,
+        name: "JetBrainsMono_ExtraBold",
+      },
+    ],
+    height: 720,
+    width: 1280,
+  });
+  const resvg = new Resvg(svg);
+  const render = resvg.render();
+  const png = render.asPng();
+
+  return png;
+};
