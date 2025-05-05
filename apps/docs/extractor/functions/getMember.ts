69f241d8d9940d43d069381cb4a9fb446b0982ca
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-05 19:48:57 +0200

    chore(docs): `getMember` does not longer require the kind argument

--- apps/docs/extractor/functions/getMember.ts
+++ apps/docs/extractor/functions/getMember.ts
@@ -1,9 +1,9 @@
 import type { AnyEntryPointMember } from "#extractor/types";
 import { getTypeMembers } from "#util/extractor";
 
-export const getMember = (name: string, kind: string): AnyEntryPointMember | null => {
+export const getMember = (name: string): AnyEntryPointMember | null => {
   const members = getTypeMembers();
-  const member = members.find((member) => member.kind === kind && member.name === name);
+  const member = members.find((member) => member.name === name);
 
   return member ?? null;
 };
