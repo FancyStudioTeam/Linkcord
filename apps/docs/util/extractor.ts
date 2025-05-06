8d3cb6f24dc38f2fe94cc4dcbd788676afd823af
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-06 23:34:57 +0200

    chore(docs): some things

--- apps/docs/util/extractor.ts
+++ apps/docs/util/extractor.ts
@@ -1,5 +1,23 @@
 import { cache } from "react";
 import { APIExtractor } from "#extractor/APIExtractor";
 
+export enum PackageName {
+  Gateway = "gateway",
+  Linkcord = "linkcord",
+  Rest = "rest",
+  Types = "types",
+  Utils = "utils",
+  Voice = "voice",
+}
+
+export const availablePackages: PackageName[] = [
+  PackageName.Gateway,
+  PackageName.Linkcord,
+  PackageName.Rest,
+  PackageName.Types,
+  PackageName.Utils,
+  PackageName.Voice,
+] as const;
+
 export const extractor = new APIExtractor();
 export const getTypeMembers = cache(extractor.getTypesPackageMembers);
