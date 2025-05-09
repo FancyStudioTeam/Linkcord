1719d43f1c8e1200de27cee5ef3f72d880e559bf
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-09 21:07:28 +0200

    chore(docs): add `ErrorBoundary`

--- apps/docs/app/root.tsx
+++ apps/docs/app/root.tsx
@@ -1,5 +1,5 @@
 import "./base.css";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse, useRouteError } from "@remix-run/react";
 import type { ReactNode } from "react";
 
 export const Layout = ({
@@ -23,3 +23,33 @@ export const Layout = ({
 );
 
 export default () => <Outlet />;
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+  const isRouteError = isRouteErrorResponse(error);
+
+  if (isRouteError) {
+    const { status, statusText } = error;
+
+    return (
+      <div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
+        <div className="flex w-full max-w-md flex-col gap-4">
+          <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
+            <h1 className="font-extrabold text-5xl">{status}</h1>
+            {statusText && <p className="text-sm text-zinc-400">{statusText}</p>}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
+      <div className="flex w-full max-w-md flex-col gap-4">
+        <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
+          <h1 className="font-extrabold text-5xl">Something went wrong...</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
