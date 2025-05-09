d42b36c51bf75e6e27627b42431aea69cd3967a2
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-09 19:41:22 +0200

    chore(docs): changes in `extractor`

--- apps/docs/util/extractor.ts
+++ apps/docs/util/extractor.ts
@@ -5,21 +5,19 @@ export enum PackageName {
   Gateway = "gateway",
   Linkcord = "linkcord",
   Types = "types",
-  Utils = "utils",
   Voice = "voice",
 }
 
-export const availablePackages = [
+export const availablePackages: PackageName[] = [
   PackageName.Gateway,
   PackageName.Linkcord,
   PackageName.Types,
-  PackageName.Utils,
   PackageName.Voice,
 ] as const;
 
 export const extractor = new APIExtractor();
 
-export const getGatewayMembers = cache(extractor.getGatewayPackageMembers);
-export const getLinkcordMembers = cache(extractor.getLinkcordPackageMembers);
-export const getTypeMembers = cache(extractor.getTypesPackageMembers);
-export const getVoiceMembers = cache(extractor.getVoicePackageMembers);
+export const getGatewayMembers = cache(extractor.getGatewayPackageMembers.bind(extractor));
+export const getLinkcordMembers = cache(extractor.getLinkcordPackageMembers.bind(extractor));
+export const getTypeMembers = cache(extractor.getTypesPackageMembers.bind(extractor));
+export const getVoiceMembers = cache(extractor.getVoicePackageMembers.bind(extractor));
