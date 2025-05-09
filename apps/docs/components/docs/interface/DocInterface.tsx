e2ffd461b3963fd928850182a09e37ef66ff3b75
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-09 21:16:53 +0200

    chore(docs): things

--- apps/docs/components/docs/interface/DocInterface.tsx
+++ apps/docs/components/docs/interface/DocInterface.tsx
@@ -1,12 +1,58 @@
-import type { APIInterface } from "#extractor/types";
+import { getMember, sortMembers } from "#extractor/functions";
+import type { APIInterface, AnyInterfaceMember } from "#extractor/types";
 import { DocInterfaceProperties } from "./DocInterfaceProperties.js";
+
+const GENERIC_TYPE_REGEX = /<.*>/g;
+
+const getExtendingReferences = (data: APIInterface) => {
+  const { excerptTokens, extendsTokenRanges } = data;
+  const extendingTypes: string[] = [];
+
+  extendsTokenRanges.map((tokenRange) => {
+    const { endIndex, startIndex } = tokenRange;
+    const slicedExcerptTokens = excerptTokens.slice(startIndex, endIndex).map(({ text }) => text);
+    const fullExcerptToken = slicedExcerptTokens.join("").replaceAll(GENERIC_TYPE_REGEX, "");
+
+    extendingTypes.push(fullExcerptToken);
+  });
+
+  return extendingTypes;
+};
+
+const getPropertiesFromInterface = (data: APIInterface): AnyInterfaceMember[] => {
+  const { members } = data;
+  const interfaceMembers: AnyInterfaceMember[] = [...members];
+  const extendingReferences = getExtendingReferences(data);
+  const isExtending = extendingReferences.length > 0;
+
+  console.log(extendingReferences);
+
+  if (isExtending) {
+    extendingReferences.map((extendingReference) => {
+      const originalReferenceMember = getMember(extendingReference) as APIInterface | null;
+
+      if (!originalReferenceMember) {
+        return;
+      }
+
+      const originalReferenceMembers = getPropertiesFromInterface(originalReferenceMember);
+
+      interfaceMembers.push(...originalReferenceMembers);
+    });
+  }
+
+  const sortedMembers = sortMembers(interfaceMembers);
+
+  return sortedMembers;
+};
 
 export const DocInterface = ({
   data,
 }: {
   data: APIInterface;
 }) => {
-  const { members } = data;
+  //const { members } = data;
+  const members = getPropertiesFromInterface(data);
 
   return <DocInterfaceProperties members={members} />;
 };
---
+++ apps/docs/components/docs/navbar/Navbar.tsx
@@ -0,0 +1,3 @@
+export const Navbar = () => {
+  return <div className="flex rounded-lg border border-zinc-700 bg-zinc-900 p-6 md:hidden">Linkcord</div>;
+};
