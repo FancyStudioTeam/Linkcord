164e205a04558dfe9f77fc98727b47799f3955d9
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-10 22:34:07 +0200

    chore(linkcord): export everything from other packages

--- packages/linkcord/src/index.ts
+++ packages/linkcord/src/index.ts
@@ -1,1 +1,4 @@
 export * from "./structures/index.js";
+export * from "@fancystudioteam/linkcord-gateway";
+export * from "@fancystudioteam/linkcord-types";
+export * from "@fancystudioteam/linkcord-voice";
