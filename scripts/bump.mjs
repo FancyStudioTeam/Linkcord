794e52d779fda00e21aa3ffa0af4013c6b2b7e6c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-10 21:35:36 +0200

    feat: add `bump` script

---
+++ scripts/bump.mjs
@@ -0,0 +1,59 @@
+// @ts-check
+
+import { execSync } from "node:child_process";
+import { existsSync, readFileSync, readdirSync, writeFileSync } from "node:fs";
+import { join } from "node:path";
+import semver from "semver";
+
+const { inc, RELEASE_TYPES, valid } = semver;
+const [, , ...args] = process.argv;
+const [semverOrVersion] = args;
+
+if (!semverOrVersion) {
+  // biome-ignore lint/suspicious/noConsole:
+  console.error("Missing semver or version");
+  process.exit(1);
+}
+
+const packagesPath = join(process.cwd(), "packages");
+const packages = readdirSync(packagesPath, {
+  withFileTypes: true,
+});
+
+for (const _package of packages) {
+  const { name, parentPath } = _package;
+  const packageJsonPath = join(parentPath, name, "package.json");
+  const existsPackageJson = existsSync(packageJsonPath);
+
+  if (!existsPackageJson) {
+    continue;
+  }
+
+  const packageJsonContent = readFileSync(packageJsonPath, "utf-8");
+  const parsedJsonContent = JSON.parse(packageJsonContent);
+  const currentVersion = parsedJsonContent.version;
+  let version = currentVersion;
+
+  // @ts-ignore
+  if (RELEASE_TYPES.includes(semverOrVersion.toLowerCase())) {
+    // @ts-ignore
+    version = inc(currentVersion, semverOrVersion.toLowerCase());
+  } else {
+    const isValidVersion = valid(semverOrVersion);
+
+    if (!isValidVersion) {
+      // biome-ignore lint/suspicious/noConsole:
+      console.error(`Invalid version: "${semverOrVersion}".`);
+      process.exit(1);
+    }
+
+    version = semverOrVersion;
+  }
+
+  parsedJsonContent.version = version;
+
+  const stringifiedJsonContent = JSON.stringify(parsedJsonContent, null, 2);
+
+  writeFileSync(packageJsonPath, `${stringifiedJsonContent}\n`, "utf-8");
+  execSync(`biome check --write ${packageJsonPath}`);
+}
