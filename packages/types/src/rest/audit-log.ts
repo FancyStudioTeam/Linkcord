76ab15dfd043fff18fa1b3cb3f8df748623fa01a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-11 20:19:38 +0200

    feat(types): add `audit-log` query string params

--- packages/types/src/rest/audit-log.ts
+++ packages/types/src/rest/audit-log.ts
@@ -1,4 +1,17 @@
-import type { APIAuditLog } from "#payloads";
+import type { APIAuditLog, AuditLogEvents } from "#payloads";
+import type { Snowflake } from "#shared";
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/resources/audit-log#get-guild-audit-log-query-string-params
+ */
+export interface RESTGetGuildAuditLogStringParams {
+  action_type?: AuditLogEvents;
+  after?: Snowflake;
+  before?: Snowflake;
+  limit?: number;
+  user_id?: Snowflake;
+}
 
 /**
  * @public
