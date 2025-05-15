1d19baba5c29162cce393b78cfd2125b4af399bf
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-15 23:43:20 +0200

    chore(voice): add udp socket and transformer

--- packages/voice/src/structures/VoiceConnection.ts
+++ packages/voice/src/structures/VoiceConnection.ts
@@ -1,13 +1,23 @@
-import { type Nullable, VoiceOpcodes } from "@fancystudioteam/linkcord-types";
-import { WebSocket } from "ws";
+import type { Readable } from "node:stream";
+import {
+  type Nullable,
+  type Snowflake,
+  SpeakingFlags,
+  type VoiceEvent,
+  type VoiceHeartbeatPayload,
+  type VoiceIdentifyPayload,
+  VoiceOpcodes,
+  type VoiceResumePayload,
+  type VoiceSelectProtocolPayload,
+  type VoiceSpeakingPayload,
+} from "@fancystudioteam/linkcord-types";
+import { type RawData, WebSocket } from "ws";
 import { VoiceConnectionError } from "#utils";
 import type { VoiceManager } from "./VoiceManager.js";
+import { VoiceStreamTransformer } from "./VoiceStreamTransformer.js";
+import { VoiceUDPSocket } from "./VoiceUDPSocket.js";
 
 const SENDABLE_VOICE_OPCODES = [
-  VoiceOpcodes.DaveMLSCommitWelcome,
-  VoiceOpcodes.DaveMLSInvalidCommitWelcome,
-  VoiceOpcodes.DaveMLSKeyPackage,
-  VoiceOpcodes.DaveTransitionReady,
   VoiceOpcodes.Heartbeat,
   VoiceOpcodes.Identify,
   VoiceOpcodes.Resume,
@@ -19,18 +29,50 @@ const SENDABLE_VOICE_OPCODES = [
  * @public
  */
 export class VoiceConnection {
+  private _address: Nullable<VoiceConnectionAddress> = null;
+  private _heartbeatAck = 0;
+  private _sequence = 0;
+
   readonly endpoint: URL;
+  readonly guildId: Snowflake;
   readonly manager: VoiceManager;
+  readonly sessionId: string;
   readonly token: string;
+  readonly userId: Snowflake;
 
+  sequence = 0;
   socket: Nullable<WebSocket> = null;
+  ssrc: Nullable<number> = null;
+  timestamp = 0;
+  udpSocket: Nullable<VoiceUDPSocket> = null;
 
-  constructor(manager: VoiceManager, endpoint: string, token: string) {
+  constructor(manager: VoiceManager, options: VoiceConnectionOptions) {
+    const { endpoint, guildId, sessionId, token, userId } = options;
+
     this.endpoint = new URL(`wss://${endpoint}`);
+    this.guildId = guildId;
     this.manager = manager;
+    this.sessionId = sessionId;
     this.token = token;
-
+    this.userId = userId;
     this._initializeSocket(this.endpoint);
+  }
+
+  private *_chunkBuffer(buffer: Buffer, chunkSize: number): Generator<Buffer> {
+    for (let offset = 0; offset < buffer.length; offset += chunkSize) {
+      yield buffer.slice(offset, offset + chunkSize);
+    }
+  }
+
+  private _heartbeat(): void {
+    const { _sequence, _heartbeatAck } = this;
+    const heartbeatPayload: VoiceHeartbeatPayload = {
+      // biome-ignore lint/style/useNamingConvention: Discord fields use snake case.
+      seq_ack: _sequence,
+      t: _heartbeatAck,
+    };
+
+    this.sendVoicePayload(VoiceOpcodes.Heartbeat, heartbeatPayload);
   }
 
   /**
@@ -46,6 +88,86 @@ export class VoiceConnection {
     const socket = new WebSocket(url);
 
     this.socket = socket;
+    this.socket.on("open", this._onOpen.bind(this));
+    this.socket.on("message", this._onMessage.bind(this));
+    this.socket.on("close", this._onClose.bind(this));
+  }
+
+  /**
+   * @internal
+   */
+  private _onClose(code: number, reason: Buffer): void {
+    console.log(code, reason.toString());
+  }
+
+  /**
+   * @internal
+   */
+  private _onMessage(rawData: RawData, _isBinary: boolean): void {
+    const stringifiedRawData = rawData.toString();
+    const message = JSON.parse(stringifiedRawData) as VoiceEvent;
+
+    if (message.seq) {
+      this._sequence = message.seq;
+    }
+
+    switch (message.op) {
+      case VoiceOpcodes.HeartbeatAck: {
+        const { t } = message.d;
+
+        this._heartbeatAck = t;
+
+        break;
+      }
+      case VoiceOpcodes.Hello: {
+        const { heartbeat_interval } = message.d;
+
+        setInterval(this._heartbeat.bind(this), heartbeat_interval);
+
+        break;
+      }
+      case VoiceOpcodes.Ready: {
+        const { ip, port, ssrc } = message.d;
+
+        this._address = {
+          ip,
+          port,
+        };
+        this.udpSocket = new VoiceUDPSocket(this, this._address);
+        this.ssrc = ssrc;
+
+        break;
+      }
+      case VoiceOpcodes.SessionDescription: {
+        const { secret_key } = message.d;
+        const udpSocket = this.getUDPSocket();
+
+        udpSocket.setSecretKey(secret_key);
+        udpSocket.sendSilenceFrames();
+
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  /**
+   * @internal
+   */
+  private _onOpen(): void {
+    const identifyPayload = {
+      // biome-ignore lint/style/useNamingConvention: Discord fields use snake case.
+      server_id: this.guildId,
+      // biome-ignore lint/style/useNamingConvention: Discord fields use snake case.
+      user_id: this.userId,
+      // biome-ignore lint/style/useNamingConvention: Discord fields use snake case.
+      session_id: this.sessionId,
+      token: this.token,
+    };
+
+    this.sendVoicePayload(VoiceOpcodes.Identify, identifyPayload);
   }
 
   getWebSocket(): WebSocket {
@@ -63,10 +185,39 @@ export class VoiceConnection {
     return socket;
   }
 
-  /*private sendPayload<Opcode extends AnySendableVoiceOpcode>(opcode: Opcode, payload: SendPayload[Opcode]): void {
+  getUDPSocket(): VoiceUDPSocket {
+    const socket = this.udpSocket;
+
+    if (!socket) {
+      const errorMessages = [
+        "The voice connection's udp socket has not been initialized yet.",
+        "Make sure to create a voice connection to initialize and open its udp socket.",
+      ];
+
+      throw new VoiceConnectionError(errorMessages.join("\n"));
+    }
+
+    return socket;
+  }
+
+  async playStream(stream: Readable): Promise<void> {
+    const transformer = new VoiceStreamTransformer();
+    const data: Readable = stream;
+    const opusData = await transformer.transformToOpus(data);
+    const udpSocket = this.getUDPSocket();
+
+    this.setSpeaking(SpeakingFlags.Microphone | SpeakingFlags.Soundshare);
+  }
+
+  sendVoicePayload<Opcode extends AnySendableVoiceOpcode>(opcode: Opcode, payload: SendVoicePayloadData[Opcode]): void {
     if (!SENDABLE_VOICE_OPCODES.includes(opcode)) {
       throw new VoiceConnectionError("Cannot send a non-sendable voice opcode to the gateway.");
     }
+
+    this.manager.emit(
+      "debug",
+      `Sending voice gateway opcode "${opcode}" with payload: ${JSON.stringify(payload, null, 2)}`,
+    );
 
     const socket = this.getWebSocket();
     const dataToSend = {
@@ -76,7 +227,50 @@ export class VoiceConnection {
     const stringifiedDataToSend = JSON.stringify(dataToSend);
 
     socket.send(stringifiedDataToSend);
-  }*/
+  }
+
+  setSpeaking(speaking: number): void {
+    const speakingPayload: VoiceSpeakingPayload = {
+      delay: 0,
+      speaking,
+      ssrc: this.ssrc ?? 0,
+    };
+
+    this.sendVoicePayload(VoiceOpcodes.Speaking, speakingPayload);
+  }
 }
 
+/**
+ * @public
+ */
+export interface SendVoicePayloadData {
+  [VoiceOpcodes.Heartbeat]: VoiceHeartbeatPayload;
+  [VoiceOpcodes.Identify]: VoiceIdentifyPayload;
+  [VoiceOpcodes.Resume]: VoiceResumePayload;
+  [VoiceOpcodes.SelectProtocol]: VoiceSelectProtocolPayload;
+  [VoiceOpcodes.Speaking]: VoiceSpeakingPayload;
+}
+
+/**
+ * @public
+ */
+export interface VoiceConnectionAddress {
+  ip: string;
+  port: number;
+}
+
+/**
+ * @public
+ */
+export interface VoiceConnectionOptions {
+  endpoint: string;
+  guildId: Snowflake;
+  sessionId: string;
+  token: string;
+  userId: Snowflake;
+}
+
+/**
+ * @public
+ */
 export type AnySendableVoiceOpcode = (typeof SENDABLE_VOICE_OPCODES)[number];
---
+++ packages/voice/src/structures/VoiceUDPSocket.ts
@@ -0,0 +1,167 @@
+import { createCipheriv } from "node:crypto";
+import { type Socket, type SocketType, createSocket } from "node:dgram";
+import { isIPv4 } from "node:net";
+import { type Nullable, VoiceOpcodes, type VoiceSelectProtocolPayload } from "@fancystudioteam/linkcord-types";
+import { VoiceConnectionError } from "#utils";
+import type { VoiceConnection, VoiceConnectionAddress } from "./VoiceConnection.js";
+
+const IP_DISCOVERY_PACKET_LENGTH = 74;
+const SILENCE_FRAMES = Buffer.from([0xf8, 0xff, 0xfe]);
+
+/**
+ * @public
+ */
+export class VoiceUDPSocket {
+  private _address: VoiceConnectionAddress;
+  private _localAddress: Nullable<VoiceConnectionUDPSocketAddress> = null;
+  private _secretKey: Buffer = Buffer.alloc(32);
+  private _socket: Nullable<Socket> = null;
+
+  voiceConnection: VoiceConnection;
+
+  constructor(voiceConnection: VoiceConnection, address: VoiceConnectionAddress) {
+    this._address = address;
+    this.voiceConnection = voiceConnection;
+    this.discovery();
+  }
+
+  /**
+   * @internal
+   */
+  private _createAudioPacket(audioFrame: Buffer): Buffer {
+    const rtpHeader = this._createRTPHeader();
+    const cipher = createCipheriv("aes-256-gcm", this._secretKey, rtpHeader);
+    const encryptedAudioFrame = Buffer.concat([cipher.update(audioFrame), cipher.final()]);
+    const tag = cipher.getAuthTag();
+    const audioPacket = Buffer.concat([rtpHeader, encryptedAudioFrame, tag]);
+
+    return audioPacket;
+  }
+
+  /**
+   * @internal
+   */
+  private _createRTPHeader(): Buffer {
+    const data = Buffer.alloc(12);
+    const { sequence, ssrc, timestamp } = this.voiceConnection;
+
+    data.writeUInt8(0x80, 0);
+    data.writeUInt8(0x78, 1);
+
+    data.writeUInt16BE(sequence, 2);
+    data.writeUInt32BE(timestamp, 4);
+    data.writeUInt32BE(ssrc ?? 0, 8);
+
+    return data;
+  }
+
+  /**
+   * @internal
+   */
+  private _onMessage(rawData: Buffer): void {
+    const addressOffset = 8;
+    const addressStart = addressOffset;
+    let addressEnd = addressOffset;
+
+    while (rawData[addressEnd] !== 0) {
+      addressEnd++;
+    }
+
+    const address = rawData.toString("ascii", addressStart, addressEnd);
+    const port = rawData.readUInt16BE(IP_DISCOVERY_PACKET_LENGTH - 2);
+
+    this._localAddress = {
+      ip: address,
+      port,
+    };
+
+    const selectProtocolPayload: VoiceSelectProtocolPayload = {
+      data: {
+        address,
+        mode: "aead_aes256_gcm_rtpsize",
+        port,
+      },
+      protocol: "udp",
+    };
+
+    this.voiceConnection.sendVoicePayload(VoiceOpcodes.SelectProtocol, selectProtocolPayload);
+  }
+
+  discovery(): void {
+    const { ip } = this._address;
+    const type: SocketType = isIPv4(ip) ? "udp4" : "udp6";
+
+    this._socket = createSocket(type);
+    this._socket.once("message", this._onMessage.bind(this));
+    this.sendDiscoveryPacket();
+  }
+
+  getUDPSocket(): Socket {
+    const socket = this._socket;
+
+    if (!socket) {
+      const errorMessages = [
+        "The voice connection's udp socket has not been initialized yet.",
+        "Make sure to create a voice connection to initialize and open its udp socket.",
+      ];
+
+      throw new VoiceConnectionError(errorMessages.join("\n"));
+    }
+
+    return socket;
+  }
+
+  sendAudioPacket(audioFrame: Buffer): void {
+    const { ip, port } = this._localAddress ?? {};
+
+    if (!(ip && port)) {
+      throw new VoiceConnectionError("Local address has not been set yet.");
+    }
+
+    const audioPacket = this._createAudioPacket(audioFrame);
+
+    this.sendUDPPacket(audioPacket, ip, port);
+  }
+
+  sendDiscoveryPacket(): void {
+    const { ip, port } = this._address;
+    const { ssrc } = this.voiceConnection;
+    const data = Buffer.allocUnsafe(IP_DISCOVERY_PACKET_LENGTH);
+
+    data.writeUInt16BE(0x1, 0);
+    data.writeUInt16BE(70, 2);
+    data.writeUInt32BE(ssrc ?? 0, 4);
+
+    this.sendUDPPacket(data, ip, port);
+  }
+
+  sendSilenceFrames(): void {
+    const { ip, port } = this._localAddress ?? {};
+
+    if (!(ip && port)) {
+      throw new VoiceConnectionError("Local address has not been set yet.");
+    }
+
+    this.sendUDPPacket(SILENCE_FRAMES, ip, port);
+  }
+
+  sendUDPPacket(data: Buffer, ip: string, port: number): void {
+    const socket = this.getUDPSocket();
+    const length = data.length;
+    const offset = 0;
+
+    socket.send(data, offset, length, port, ip);
+  }
+
+  setSecretKey(secretKey: number[]): void {
+    this._secretKey = Buffer.from(secretKey);
+  }
+}
+
+/**
+ * @public
+ */
+export interface VoiceConnectionUDPSocketAddress {
+  ip: string;
+  port: number;
+}
