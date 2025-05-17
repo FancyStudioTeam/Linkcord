5bcac7661f2979109ffc29e19f0082b776b12611
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-17 21:15:23 +0200

    chore(docs): use `Layout` component

--- apps/docs/app/root.tsx
+++ apps/docs/app/root.tsx
@@ -1,57 +1,38 @@
 import "./base.css";
-import { APIMemberKind } from "@fancystudioteam/api-extractor";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse, useRouteError } from "@remix-run/react";
+import { Outlet, isRouteErrorResponse, useRouteError } from "@remix-run/react";
 import type { ReactNode } from "react";
 
 export const Layout = ({
   children,
 }: {
   children: ReactNode;
-}) => (
-  <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <Meta />
-      <Links />
-    </head>
-    <body className="bg-zinc-950 font-mono text-white">
-      {APIMemberKind.Enum}
-      {children}
-      <ScrollRestoration />
-      <Scripts />
-    </body>
-  </html>
-);
-
-export default () => <Outlet />;
+}) => <Layout>{children}</Layout>;
 
 export const ErrorBoundary = () => {
   const error = useRouteError();
   const isRouteError = isRouteErrorResponse(error);
+  const node = isRouteError ? (
+    <>
+      <h1 className="font-extrabold text-5xl">{error.status}</h1>
+      {error.statusText && <p className="text-sm text-zinc-400">{error.statusText}</p>}
+    </>
+  ) : (
+    <>
+      <h1 className="font-extrabold text-5xl">Something went wrong...</h1>
+    </>
+  );
 
-  if (isRouteError) {
-    const { status, statusText } = error;
-
-    return (
+  return (
+    <Layout>
       <div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
         <div className="flex w-full max-w-md flex-col gap-4">
-          <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
-            <h1 className="font-extrabold text-5xl">{status}</h1>
-            {statusText && <p className="text-sm text-zinc-400">{statusText}</p>}
+          <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6 text-center">
+            {node}
           </div>
         </div>
       </div>
-    );
-  }
-
-  return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
-      <div className="flex w-full max-w-md flex-col gap-4">
-        <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
-          <h1 className="font-extrabold text-5xl">Something went wrong...</h1>
-        </div>
-      </div>
-    </div>
+    </Layout>
   );
 };
+
+export default () => <Outlet />;
---
+++ apps/docs/components/Layout.tsx
@@ -0,0 +1,22 @@
+import { Links, Meta, Scripts, ScrollRestoration } from "@remix-run/react";
+import type { ReactNode } from "react";
+
+export const Layout = ({
+  children,
+}: {
+  children: ReactNode;
+}) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Meta />
+      <Links />
+    </head>
+    <body className="bg-zinc-950 font-mono text-white">
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+    </body>
+  </html>
+);
