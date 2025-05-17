42910074721d8a45f97896ec1d5fa30a12866d9a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-17 21:45:41 +0200

    chore(docs): update main page

--- apps/docs/app/routes/docs._index.tsx
+++ apps/docs/app/routes/docs._index.tsx
@@ -1,6 +1,7 @@
 import { SiDiscord as Discord, SiGithub as GitHub } from "@icons-pack/react-simple-icons";
 import { Link } from "@remix-run/react";
 import { SquareArrowOutUpRight } from "lucide-react";
+import type { ReactNode } from "react";
 import { PACKAGE_ICONS, PACKAGE_NAMES } from "#util/data";
 import { type PackageName, availablePackages } from "#util/extractor";
 import { LINKS } from "#util/links";
@@ -27,26 +28,40 @@ const Package = ({
   );
 };
 
-export default () => {
-  return (
-    <div className="flex h-dvh w-full flex-col items-center justify-center">
-      <div className="flex w-full max-w-md flex-col gap-4">
-        <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
-          {availablePackages.map((packageName) => (
-            <Package key={packageName} packageName={packageName} />
-          ))}
-        </div>
-        <div className="flex w-full flex-col items-center justify-center gap-4 *:flex *:w-full *:items-center *:justify-center *:gap-2 *:rounded-md *:border *:border-zinc-700 *:bg-zinc-900 *:px-4 *:py-2 *:text-sm *:transition-colors *:hover:bg-zinc-800 sm:flex-row">
-          <Link target="_blank" to={`${LINKS.GitHub}/tree/main/packages`}>
-            <GitHub className="size-5 shrink-0" />
-            GitHub
-          </Link>
-          <Link target="_blank" to={LINKS.Discord}>
-            <Discord className="size-5 shrink-0" />
-            Discord
-          </Link>
-        </div>
+const SocialLink = ({
+  children,
+  to,
+}: {
+  children: ReactNode;
+  to: string;
+}) => (
+  <Link
+    className="flex w-full items-center justify-center gap-2 rounded-md border border-zinc-700 bg-zinc-900 px-4 py-2 text-sm transition-colors hover:bg-zinc-800"
+    target={"_blank"}
+    to={to}
+  >
+    {children}
+  </Link>
+);
+
+export default () => (
+  <div className="flex h-dvh w-full flex-col items-center justify-center">
+    <div className="flex w-full max-w-md flex-col gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 rounded-md border border-zinc-700 bg-zinc-900 p-6">
+        {availablePackages.map((packageName) => (
+          <Package key={packageName} packageName={packageName} />
+        ))}
+      </div>
+      <div className="flex w-full flex-col items-center justify-center gap-4 sm:flex-row">
+        <SocialLink to={LINKS.GitHub}>
+          <GitHub className="size-5 shrink-0" />
+          GitHub
+        </SocialLink>
+        <SocialLink to={LINKS.Discord}>
+          <Discord className="size-5 shrink-0" />
+          Discord
+        </SocialLink>
       </div>
     </div>
-  );
-};
+  </div>
+);
