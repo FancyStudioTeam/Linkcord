975ca8ae3abce74c215d3f8ca5b85e53b74a2c76
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-24 20:05:11 +0200

    feat(linkcord): add `BitFieldResolver`

---
+++ packages/linkcord/src/utils/structures/BitFieldResolver.ts
@@ -0,0 +1,46 @@
+/**
+ * @public
+ */
+export class BitFieldResolver {
+  bitField: number;
+
+  constructor(bitField: number) {
+    this.bitField = bitField;
+  }
+
+  /**
+   * Checks if the current instance is frozen.
+   * @returns A boolean indicating if the current instance is frozen.
+   */
+  get frozen(): boolean {
+    return Object.isFrozen(this);
+  }
+
+  /**
+   * Adds a bit to the bitfield.
+   * @param bit - The bit to add.
+   * @returns The bitfield with the added bit.
+   */
+  add(bit: number): number {
+    this.bitField |= bit;
+
+    return this.bitField;
+  }
+
+  /**
+   * Freezes the current instance.
+   * @returns The current frozen instance.
+   */
+  freeze(): Readonly<this> {
+    return Object.freeze(this);
+  }
+
+  /**
+   * Checks if the bit is set in the bitfield.
+   * @param bit - The bit to check.
+   * @returns A boolean indicating if the bit is set.
+   */
+  has(bit: number): boolean {
+    return (this.bitField & bit) === bit;
+  }
+}
---
+++ packages/linkcord/src/utils/structures/__tests__/BitFieldResolver.test.ts
@@ -0,0 +1,39 @@
+import { UserFlags } from "@fancystudioteam/linkcord-types";
+import { describe, expect, it } from "vitest";
+import { BitFieldResolver } from "../BitFieldResolver.js";
+
+describe("Class: BitFieldResolver", () => {
+  it("Returns an instance of the 'BitFieldResolver' class.", () => {
+    const bitFieldResolver = new BitFieldResolver(0);
+
+    expect(bitFieldResolver).toBeInstanceOf(BitFieldResolver);
+    expect(bitFieldResolver).toStrictEqual(new BitFieldResolver(0));
+    expect(bitFieldResolver.bitField).toBe(0);
+    expect(bitFieldResolver.frozen).toBe(false);
+  });
+
+  it("Adds the given bit to the bitfield and returns the updated value.", () => {
+    expect(new BitFieldResolver(0).add(0)).toBe(0);
+    expect(new BitFieldResolver(0).add(UserFlags.ActiveDeveloper)).toBe(UserFlags.ActiveDeveloper);
+    expect(new BitFieldResolver(UserFlags.ActiveDeveloper).add(UserFlags.Staff)).toBe(
+      UserFlags.ActiveDeveloper | UserFlags.Staff,
+    );
+  });
+
+  it("Checks if the given bit is set in the bitfield and returns a boolean depending on the result.", () => {
+    expect(new BitFieldResolver(0).has(0)).toBe(true);
+    expect(new BitFieldResolver(0).has(UserFlags.ActiveDeveloper)).toBe(false);
+    expect(new BitFieldResolver(UserFlags.ActiveDeveloper).has(UserFlags.Staff)).toBe(false);
+    expect(new BitFieldResolver(UserFlags.ActiveDeveloper).has(UserFlags.ActiveDeveloper)).toBe(true);
+    expect(new BitFieldResolver(UserFlags.ActiveDeveloper | UserFlags.Staff).has(UserFlags.Staff)).toBe(true);
+  });
+
+  it("Freezes the 'BitFieldResolver' instance and returns a readonly instance.", () => {
+    const frozenBitFieldResolver = new BitFieldResolver(0).freeze();
+
+    expect(frozenBitFieldResolver).toBeInstanceOf(BitFieldResolver);
+    expect(frozenBitFieldResolver).toStrictEqual(new BitFieldResolver(0));
+    expect(frozenBitFieldResolver.bitField).toBe(0);
+    expect(frozenBitFieldResolver.frozen).toBe(true);
+  });
+});
