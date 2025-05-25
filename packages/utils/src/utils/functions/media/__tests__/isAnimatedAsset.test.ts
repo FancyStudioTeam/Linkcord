ebd8022c7c81025541e3cd562f355eec7f8597e1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-25 22:03:56 +0200

    fix(tests): add `hash`

--- packages/utils/src/utils/functions/media/__tests__/isAnimatedAsset.test.ts
+++ packages/utils/src/utils/functions/media/__tests__/isAnimatedAsset.test.ts
@@ -5,7 +5,7 @@ const DYNAMIC_AVATAR_HASH = "a_685e98ef454175d0f2de0ce8d5db7650";
 const STATIC_AVATAR_HASH = "685e98ef454175d0f2de0ce8d5db7650";
 
 describe("Function: isAnimatedAsset", () =>
-  it("Returns a boolean indicating whether the asset is animated or not", () => {
+  it("Returns a boolean indicating whether the asset hash is animated or not", () => {
     expect(isAnimatedAsset(DYNAMIC_AVATAR_HASH)).toBe(true);
     expect(isAnimatedAsset(STATIC_AVATAR_HASH)).toBe(false);
     expect(isAnimatedAsset("")).toBe(false);
