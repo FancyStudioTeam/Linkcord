60c79cedf258b4f1a6d6d61bb4fae0742be52f40
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-26 22:57:36 +0200

    style: delete curly braces

--- packages/utils/src/utils/functions/__tests__/calculateShardIdFromGuild.test.ts
+++ packages/utils/src/utils/functions/__tests__/calculateShardIdFromGuild.test.ts
@@ -3,9 +3,8 @@ import { calculateShardIdFromGuildId } from "../calculateShardIdFromGuildId.js";
 
 const GUILD_ID = "613425648685547541";
 
-describe("Function: calculateShardIdFromGuildId", () => {
+describe("Function: calculateShardIdFromGuildId", () =>
   it("Returns the shard id from the guild id in a specific amount of shards.", () => {
     expect(calculateShardIdFromGuildId(1, GUILD_ID)).toBe(0);
     expect(calculateShardIdFromGuildId(15, GUILD_ID)).toBe(14);
-  });
-});
+  }));
