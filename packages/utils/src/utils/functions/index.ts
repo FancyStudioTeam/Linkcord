7e68e5cdececc85af3cac3c3b68de8828e4fc06c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-27 23:23:23 +0200

    chore(utils): update exports

--- packages/utils/src/utils/functions/index.ts
+++ packages/utils/src/utils/functions/index.ts
@@ -1,4 +1,3 @@
 export * from "./calculateShardIdFromGuildId.js";
 export * from "./fetchGatewayBot.js";
-export * from "./media/index.js";
 export * from "./replaceBotPrefix.js";
--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -1,2 +1,3 @@
 export * from "./constants.js";
 export * from "./functions/index.js";
+export * from "./structures/index.js";
