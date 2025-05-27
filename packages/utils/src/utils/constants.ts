7447cbd316d979d0e48192cb35bc506b6607a782
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-27 23:24:34 +0200

    chore(utils): use `CreateImageUrlOptions` for `ImageUrlOptions`

--- packages/utils/src/utils/constants.ts
+++ packages/utils/src/utils/constants.ts
@@ -1,3 +1,5 @@
+import type { CreateImageUrlOptions } from "./structures/ImageUtils.js";
+
 /**
  * @public
  */
@@ -11,28 +13,14 @@ export const ALLOWED_IMAGE_SIZES = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
 /**
  * @public
  */
-export interface ImageUrlOptions {
-  /**
-   * The extension to use for the asset.
-   */
-  extension?: ImageExtension;
-  /**
-   * Whether to force the use of the static version of the asset even if the
-   * asset hash is animated.
-   */
-  forceStatic?: boolean;
-  /**
-   * The size to use for the asset.
-   */
-  size?: ImageSize;
-}
-
-/**
- * @public
- */
 export type ImageExtension = (typeof ALLOWED_IMAGE_EXTENSIONS)[number];
 
 /**
  * @public
  */
 export type ImageSize = (typeof ALLOWED_IMAGE_SIZES)[number];
+
+/**
+ * @public
+ */
+export type ImageUrlOptions = CreateImageUrlOptions;
