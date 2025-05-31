76e5613e8f4e54d6611c377dcc241f7fb0084a78
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-05-31 22:46:21 +0200

    chore(linkcord): watch gateway events

--- packages/linkcord/src/structures/Client.ts
+++ packages/linkcord/src/structures/Client.ts
@@ -1,18 +1,21 @@
 import { EventEmitter } from "node:events";
-import { GatewayManager, type GatewayManagerOptions } from "@fancystudioteam/linkcord-gateway";
-import type { Snowflake } from "@fancystudioteam/linkcord-types";
+import { GatewayManager, type GatewayManagerOptions, type GatewayShard } from "@fancystudioteam/linkcord-gateway";
+import type { GatewayEvent, If, Snowflake } from "@fancystudioteam/linkcord-types";
 import { VoiceManager, type VoiceManagerOptions } from "@fancystudioteam/linkcord-voice";
+import type { User } from "../types/index.js";
+import { handlers } from "./handlers/handlers.js";
 
 /**
  * @public
  */
-export class Client<Ready extends boolean = false> extends EventEmitter<ClientEvents> {
+export class Client<Ready extends boolean = boolean> extends EventEmitter<ClientEvents> {
   readonly gateway: GatewayManager;
   readonly options: ClientOptions;
   readonly unavailableGuilds: Map<Snowflake, boolean> = new Map();
   readonly voice: VoiceManager;
 
   ready: Ready;
+  user: If<Ready, User, null> = null as If<Ready, User, null>;
 
   constructor(options: ClientOptions) {
     super();
@@ -30,6 +33,23 @@ export class Client<Ready extends boolean = false> extends EventEmitter<ClientEv
       ...voice,
       gatewayManager: this.gateway,
     });
+    this._watchGateway();
+  }
+
+  /**
+   * @internal
+   */
+  private _watchGateway(): void {
+    this.gateway.on("packet", this._handleOnGatewayPacket.bind(this));
+  }
+
+  /**
+   * @internal
+   */
+  private _handleOnGatewayPacket(packet: GatewayEvent, gatewayShard: GatewayShard): void {
+    const handler = handlers[packet.op];
+
+    handler?.(this, gatewayShard, packet as never);
   }
 
   async connect(): Promise<void> {
@@ -47,8 +67,7 @@ export class Client<Ready extends boolean = false> extends EventEmitter<ClientEv
  * @public
  */
 export interface ClientEvents {
-  debug: [message: string, shardId?: number];
-  hello: [heartbeatInterval: number, shardId: number];
+  debug: [message: string, shard?: GatewayShard];
 }
 
 /**
