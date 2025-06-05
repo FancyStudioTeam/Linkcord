168cc4bc8d6cd0f73524efb44121a9b1ef89538a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-05 22:32:42 +0200

    fix(gateway): expected 2 arguments

--- packages/gateway/src/structures/handlers/events/INVALID_SESSION.ts
+++ packages/gateway/src/structures/handlers/events/INVALID_SESSION.ts
@@ -6,20 +6,18 @@ import type { Handler } from "../handlers.js";
  */
 export const INVALID_SESSION: Handler<GatewayInvalidSession> = (gatewayShard, { d: isReconnectable }) => {
   if (isReconnectable) {
-    gatewayShard.emit(
-      "debug",
-      "The session was invalidated but it can be resumed. Should attempt to resume.",
+    gatewayShard.emit("debug", {
       gatewayShard,
-    );
+      message: "The session was invalidated and it can be resumed. Should attempt to resume.",
+    });
     // biome-ignore lint/complexity/useLiteralKeys:
     gatewayShard["_handleResume"]();
   }
 
-  gatewayShard.emit(
-    "debug",
-    "The session was invalidated but it cannot be resumed. Should attempt to request a new session.",
+  gatewayShard.emit("debug", {
     gatewayShard,
-  );
+    message: "The session was invalidated but it cannot be resumed. Should attempt to request a new session.",
+  });
   // biome-ignore lint/complexity/useLiteralKeys:
   gatewayShard["_handleConnect"]();
 };
