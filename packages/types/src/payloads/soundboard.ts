09dfe35285ce3833e68303e4350e6f4b63866e77
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-06 21:31:27 +0200

    chore(types): split default and guild soundboard sounds

--- packages/types/src/payloads/soundboard.ts
+++ packages/types/src/payloads/soundboard.ts
@@ -3,9 +3,9 @@ import type { APIUser } from "./user.js";
 
 /**
  * @public
- * @see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object-soundboard-sound-structure
+ * @see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object-example-default-soundboard-sound
  */
-export interface APISoundboardSound {
+export interface APIDefaultSoundboardSound {
   /**
    * @remarks
    * - This field may be `false` if the guild at which the soundboard sound
@@ -14,13 +14,27 @@ export interface APISoundboardSound {
   available: boolean;
   emoji_id: Snowflake | null;
   emoji_name: string | null;
-  guild_id?: Snowflake;
   name: string;
-  sound_id: Snowflake;
-  user?: APIUser;
+  sound_id: string;
   /**
    * @remarks
    * - This field is a double value between `0` and `1`. Example: `0.75`.
    */
   volume: number;
 }
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object-example-guild-soundboard-sound
+ */
+export interface APIGuildSoundboardSound extends Omit<APIDefaultSoundboardSound, "sound_id"> {
+  guild_id: Snowflake;
+  sound_id: Snowflake;
+  user?: APIUser;
+}
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/resources/soundboard#soundboard-sound-object-soundboard-sound-structure
+ */
+export type APISoundboardSound = APIDefaultSoundboardSound | APIGuildSoundboardSound;
