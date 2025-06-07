86424d64d0ecacb7322dc6ee0d43f791d0c3a37c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-07 23:19:12 +0200

    chore(rest): encode dynamic endpoints

--- packages/rest/src/utils/endpoints/CDNEndpoints.ts
+++ packages/rest/src/utils/endpoints/CDNEndpoints.ts
@@ -1,4 +1,5 @@
 import type { Snowflake } from "@fancystudioteam/linkcord-types";
+import { encodeEndpoint } from "@fancystudioteam/linkcord-utils";
 
 /**
  * @public
@@ -9,94 +10,96 @@ export class CDNEndpoints {
     achievementId: Snowflake,
     achievementIcon: string,
   ): string {
-    return `app-assets/${applicationId}/achievements/${achievementId}/icons/${achievementIcon}`;
+    return encodeEndpoint`app-assets/${applicationId}/achievements/${achievementId}/icons/${achievementIcon}`;
   }
 
   static applicationAssetStore(applicationId: Snowflake, assetId: Snowflake): string {
-    return `app-assets/${applicationId}/store/${assetId}`;
+    return encodeEndpoint`app-assets/${applicationId}/store/${assetId}`;
   }
 
   static applicationAsset(applicationId: Snowflake, assetId: Snowflake): string {
-    return `app-assets/${applicationId}/${assetId}`;
+    return encodeEndpoint`app-assets/${applicationId}/${assetId}`;
   }
 
   static applicationIcon(applicationId: Snowflake, applicationCoverOrIcon: string): string {
-    return `app-icons/${applicationId}/${applicationCoverOrIcon}`;
+    return encodeEndpoint`app-icons/${applicationId}/${applicationCoverOrIcon}`;
   }
 
   static attachment(channelId: Snowflake, messageId: Snowflake, attachmentName: string): string {
-    return `attachments/${channelId}/${messageId}/${attachmentName}`;
+    return encodeEndpoint`attachments/${channelId}/${messageId}/${attachmentName}`;
   }
 
   static avatarDecorationPreset(userAvatarDecorationAsset: string): string {
-    return `avatar-decorations/${userAvatarDecorationAsset}`;
+    return encodeEndpoint`avatar-decorations/${userAvatarDecorationAsset}`;
   }
 
   static avatar(userId: Snowflake, userAvatar: string): string {
-    return `avatars/${userId}/${userAvatar}`;
+    return encodeEndpoint`avatars/${userId}/${userAvatar}`;
   }
 
   static badgeIcon(badgeIcon: string): string {
-    return `badge-icons/${badgeIcon}`;
+    return encodeEndpoint`badge-icons/${badgeIcon}`;
   }
 
   static banner(guildOrUserId: Snowflake, guildOrUserBanner: string): string {
-    return `banners/${guildOrUserId}/${guildOrUserBanner}`;
+    return encodeEndpoint`banners/${guildOrUserId}/${guildOrUserBanner}`;
   }
 
   static clanBadge(guildId: Snowflake, clanBadge: string): string {
-    return `clan-badges/${guildId}/${clanBadge}`;
+    return encodeEndpoint`clan-badges/${guildId}/${clanBadge}`;
   }
 
   static clanBanner(guildId: Snowflake, clanBanner: string): string {
-    return `clan-banners/${guildId}/${clanBanner}`;
+    return encodeEndpoint`clan-banners/${guildId}/${clanBanner}`;
   }
 
   static discoverySplash(guildId: Snowflake, guildDiscoverySplash: string): string {
-    return `discovery-splashes/${guildId}/${guildDiscoverySplash}`;
+    return encodeEndpoint`discovery-splashes/${guildId}/${guildDiscoverySplash}`;
   }
 
   static embedAvatar(index: number): string {
-    return `embed/avatars/${index}`;
+    return encodeEndpoint`embed/avatars/${index}`;
   }
 
   static emoji(emojiId: Snowflake): string {
-    return `emojis/${emojiId}`;
+    return encodeEndpoint`emojis/${emojiId}`;
   }
 
   static guildScheduledEvent(guildId: Snowflake, scheduledEventId: Snowflake, scheduledEventCover: string): string {
-    return `guilds/${guildId}/scheduled-events/${scheduledEventId}/${scheduledEventCover}`;
+    return encodeEndpoint`guilds/${guildId}/scheduled-events/${scheduledEventId}/${scheduledEventCover}`;
   }
 
   static guildUserAvatar(guildId: Snowflake, userId: Snowflake, userAvatar: string): string {
-    return `guilds/${guildId}/users/${userId}/avatars/${userAvatar}`;
+    return encodeEndpoint`guilds/${guildId}/users/${userId}/avatars/${userAvatar}`;
   }
 
   static guildUserBanner(guildId: Snowflake, userId: Snowflake, userBanner: string): string {
-    return `guilds/${guildId}/users/${userId}/banners/${userBanner}`;
+    return encodeEndpoint`guilds/${guildId}/users/${userId}/banners/${userBanner}`;
   }
 
   static icon(guildId: Snowflake, guildIcon: string): string {
-    return `icons/${guildId}/${guildIcon}`;
+    return encodeEndpoint`icons/${guildId}/${guildIcon}`;
   }
 
   static roleIcon(roleId: Snowflake, roleIcon: string): string {
-    return `role-icons/${roleId}/${roleIcon}`;
+    return encodeEndpoint`role-icons/${roleId}/${roleIcon}`;
   }
 
   static soundboardSound(soundboardSoundId: Snowflake): string {
-    return `soundboard-sounds/${soundboardSoundId}`;
+    return encodeEndpoint`soundboard-sounds/${soundboardSoundId}`;
   }
 
   static splash(guildId: Snowflake, guildSplash: string): string {
-    return `splashes/${guildId}/${guildSplash}`;
+    return encodeEndpoint`splashes/${guildId}/${guildSplash}`;
   }
 
   static sticker(stickerId: Snowflake): string {
-    return `stickers/${stickerId}`;
+    return encodeEndpoint`stickers/${stickerId}`;
   }
 
   static teamIcon(teamId: Snowflake, teamIcon: string): string {
-    return `team-icons/${teamId}/${teamIcon}`;
+    return encodeEndpoint`team-icons/${teamId}/${teamIcon}`;
   }
 }
+
+Object.freeze(CDNEndpoints);
--- packages/rest/src/utils/endpoints/Endpoints.ts
+++ packages/rest/src/utils/endpoints/Endpoints.ts
@@ -1,67 +1,68 @@
 import type { Snowflake } from "@fancystudioteam/linkcord-types";
+import { encodeEndpoint } from "@fancystudioteam/linkcord-utils";
 
 /**
  * @public
  */
 export class Endpoints {
   static application(applicationId: "@me"): string {
-    return `applications/${applicationId}`;
+    return encodeEndpoint`applications/${applicationId}`;
   }
 
   static applicationActivityInstance(applicationId: Snowflake, activityInstance: string): string {
-    return `applications/${applicationId}/activity-instances/${activityInstance}`;
+    return encodeEndpoint`applications/${applicationId}/activity-instances/${activityInstance}`;
   }
 
   static applicationEmoji(applicationId: Snowflake, emojiId: Snowflake): string {
-    return `applications/${applicationId}/emojis/${emojiId}`;
+    return encodeEndpoint`applications/${applicationId}/emojis/${emojiId}`;
   }
 
   static applicationEmojis(applicationId: Snowflake): string {
-    return `applications/${applicationId}/emojis`;
+    return encodeEndpoint`applications/${applicationId}/emojis`;
   }
 
   static applicationEntitlement(applicationId: Snowflake, entitlementId: Snowflake): string {
-    return `applications/${applicationId}/entitlements/${entitlementId}`;
+    return encodeEndpoint`applications/${applicationId}/entitlements/${entitlementId}`;
   }
 
   static applicationEntitlementConsume(applicationId: Snowflake, entitlementId: Snowflake): string {
-    return `applications/${applicationId}/entitlements/${entitlementId}/consume`;
+    return encodeEndpoint`applications/${applicationId}/entitlements/${entitlementId}/consume`;
   }
 
   static applicationEntitlements(applicationId: Snowflake): string {
-    return `applications/${applicationId}/entitlements`;
+    return encodeEndpoint`applications/${applicationId}/entitlements`;
   }
 
   static applicationRoleConnectionMetadata(applicationId: Snowflake): string {
-    return `applications/${applicationId}/role-connections/metadata`;
+    return encodeEndpoint`applications/${applicationId}/role-connections/metadata`;
   }
 
   static applicationSkus(applicationId: Snowflake): string {
-    return `applications/${applicationId}/skus`;
+    return encodeEndpoint`applications/${applicationId}/skus`;
   }
 
   static channel(channelId: Snowflake): string {
-    return `channels/${channelId}`;
+    return encodeEndpoint`channels/${channelId}`;
   }
 
   static channelFollowers(channelId: Snowflake): string {
-    return `channels/${channelId}/followers`;
+    return encodeEndpoint`channels/${channelId}/followers`;
   }
 
   static channelInvites(channelId: Snowflake): string {
-    return `channels/${channelId}/invites`;
+    return encodeEndpoint`channels/${channelId}/invites`;
   }
 
   static channelMessage(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/messages/${messageId}`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}`;
   }
 
   static channelMessageCrosspost(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/messages/${messageId}/crosspost`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}/crosspost`;
   }
 
   static channelMessageReaction(channelId: Snowflake, messageId: Snowflake, emojiId: Snowflake): string {
-    return `channels/${channelId}/messages/${messageId}/reactions/${emojiId}`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}/reactions/${emojiId}`;
   }
 
   static channelMessageReactionUser(
@@ -70,247 +71,247 @@ export class Endpoints {
     emojiId: Snowflake,
     userId: Snowflake | "@me",
   ): string {
-    return `channels/${channelId}/messages/${messageId}/reactions/${emojiId}/${userId}`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}/reactions/${emojiId}/${userId}`;
   }
 
   static channelMessageReactions(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/messages/${messageId}/reactions`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}/reactions`;
   }
 
   static channelMessageThreads(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/messages/${messageId}/threads`;
+    return encodeEndpoint`channels/${channelId}/messages/${messageId}/threads`;
   }
 
   static channelMessages(channelId: Snowflake): string {
-    return `channels/${channelId}/messages`;
+    return encodeEndpoint`channels/${channelId}/messages`;
   }
 
   static channelMessagesBulkDelete(channelId: Snowflake): string {
-    return `channels/${channelId}/messages/bulk-delete`;
+    return encodeEndpoint`channels/${channelId}/messages/bulk-delete`;
   }
 
   static channelPermission(channelId: Snowflake, overwriteId: Snowflake): string {
-    return `channels/${channelId}/permissions/${overwriteId}`;
+    return encodeEndpoint`channels/${channelId}/permissions/${overwriteId}`;
   }
 
   static channelPin(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/pins/${messageId}`;
+    return encodeEndpoint`channels/${channelId}/pins/${messageId}`;
   }
 
   static channelPins(channelId: Snowflake): string {
-    return `channels/${channelId}/pins`;
+    return encodeEndpoint`channels/${channelId}/pins`;
   }
 
   static channelPollAnswer(channelId: Snowflake, messageId: Snowflake, answerId: number): string {
-    return `channels/${channelId}/polls/${messageId}/answers/${answerId}`;
+    return encodeEndpoint`channels/${channelId}/polls/${messageId}/answers/${answerId}`;
   }
 
   static channelPollExpire(channelId: Snowflake, messageId: Snowflake): string {
-    return `channels/${channelId}/polls/${messageId}/expire`;
+    return encodeEndpoint`channels/${channelId}/polls/${messageId}/expire`;
   }
 
   static channelRecipient(channelId: Snowflake, userId: Snowflake): string {
-    return `channels/${channelId}/recipients/${userId}`;
+    return encodeEndpoint`channels/${channelId}/recipients/${userId}`;
   }
 
   static channelSendSoundboardSound(channelId: Snowflake): string {
-    return `channels/${channelId}/send-soundboard-sound`;
+    return encodeEndpoint`channels/${channelId}/send-soundboard-sound`;
   }
 
   static channelThreadMember(channelId: Snowflake, userId: Snowflake | "@me"): string {
-    return `channels/${channelId}/thread-members/${userId}`;
+    return encodeEndpoint`channels/${channelId}/thread-members/${userId}`;
   }
 
   static channelThreadMembers(channelId: Snowflake): string {
-    return `channels/${channelId}/thread-members`;
+    return encodeEndpoint`channels/${channelId}/thread-members`;
   }
 
   static channelThreads(channelId: Snowflake): string {
-    return `channels/${channelId}/threads`;
+    return encodeEndpoint`channels/${channelId}/threads`;
   }
 
   static channelThreadsArchivedPrivate(channelId: Snowflake): string {
-    return `channels/${channelId}/threads/archived/private`;
+    return encodeEndpoint`channels/${channelId}/threads/archived/private`;
   }
 
   static channelThreadsArchivedPublic(channelId: Snowflake): string {
-    return `channels/${channelId}/threads/archived/public`;
+    return encodeEndpoint`channels/${channelId}/threads/archived/public`;
   }
 
   static channelTyping(channelId: Snowflake): string {
-    return `channels/${channelId}/typing`;
+    return encodeEndpoint`channels/${channelId}/typing`;
   }
 
   static channelUserThreadsArchivedPrivate(channelId: Snowflake, userId: "@me"): string {
-    return `channels/${channelId}/users/${userId}/threads/archived/private`;
+    return encodeEndpoint`channels/${channelId}/users/${userId}/threads/archived/private`;
   }
 
   static channelWebhooks(channelId: Snowflake): string {
-    return `channels/${channelId}/webhooks`;
+    return encodeEndpoint`channels/${channelId}/webhooks`;
   }
 
   static guild(guildId: Snowflake): string {
-    return `guilds/${guildId}`;
+    return encodeEndpoint`guilds/${guildId}`;
   }
 
   static guildAuditLogs(guildId: Snowflake): string {
-    return `guilds/${guildId}/audit-logs`;
+    return encodeEndpoint`guilds/${guildId}/audit-logs`;
   }
 
   static guildAutoModerationRule(guildId: Snowflake, autoModerationRuleId: Snowflake): string {
-    return `guilds/${guildId}/auto-moderation/rules/${autoModerationRuleId}`;
+    return encodeEndpoint`guilds/${guildId}/auto-moderation/rules/${autoModerationRuleId}`;
   }
 
   static guildAutoModerationRules(guildId: Snowflake): string {
-    return `guilds/${guildId}/auto-moderation/rules`;
+    return encodeEndpoint`guilds/${guildId}/auto-moderation/rules`;
   }
 
   static guildBan(guildId: Snowflake, userId: Snowflake): string {
-    return `guilds/${guildId}/bans/${userId}`;
+    return encodeEndpoint`guilds/${guildId}/bans/${userId}`;
   }
 
   static guildBans(guildId: Snowflake): string {
-    return `guilds/${guildId}/bans`;
+    return encodeEndpoint`guilds/${guildId}/bans`;
   }
 
   static guildBulkBan(guildId: Snowflake): string {
-    return `guilds/${guildId}/bulk-ban`;
+    return encodeEndpoint`guilds/${guildId}/bulk-ban`;
   }
 
   static guildChannels(guildId: Snowflake): string {
-    return `guilds/${guildId}/channels`;
+    return encodeEndpoint`guilds/${guildId}/channels`;
   }
 
   static guildEmoji(guildId: Snowflake, emojiId: Snowflake): string {
-    return `guilds/${guildId}/emojis/${emojiId}`;
+    return encodeEndpoint`guilds/${guildId}/emojis/${emojiId}`;
   }
 
   static guildEmojis(guildId: Snowflake): string {
-    return `guilds/${guildId}/emojis`;
+    return encodeEndpoint`guilds/${guildId}/emojis`;
   }
 
   static guildIncidentActions(guildId: Snowflake): string {
-    return `guilds/${guildId}/incident-actions`;
+    return encodeEndpoint`guilds/${guildId}/incident-actions`;
   }
 
   static guildIntegration(guildId: Snowflake, integrationId: Snowflake): string {
-    return `guilds/${guildId}/integrations/${integrationId}`;
+    return encodeEndpoint`guilds/${guildId}/integrations/${integrationId}`;
   }
 
   static guildIntegrations(guildId: Snowflake): string {
-    return `guilds/${guildId}/integrations`;
+    return encodeEndpoint`guilds/${guildId}/integrations`;
   }
 
   static guildInvites(guildId: Snowflake): string {
-    return `guilds/${guildId}/invites`;
+    return encodeEndpoint`guilds/${guildId}/invites`;
   }
 
   static guildMember(guildId: Snowflake, userId: Snowflake | "@me"): string {
-    return `guilds/${guildId}/members/${userId}`;
+    return encodeEndpoint`guilds/${guildId}/members/${userId}`;
   }
 
   static guildMemberRole(guildId: Snowflake, userId: Snowflake | "@me", roleId: Snowflake): string {
-    return `guilds/${guildId}/members/${userId}/roles/${roleId}`;
+    return encodeEndpoint`guilds/${guildId}/members/${userId}/roles/${roleId}`;
   }
 
   static guildMembers(guildId: Snowflake): string {
-    return `guilds/${guildId}/members`;
+    return encodeEndpoint`guilds/${guildId}/members`;
   }
 
   static guildMembersSearch(guildId: Snowflake): string {
-    return `guilds/${guildId}/members/search`;
+    return encodeEndpoint`guilds/${guildId}/members/search`;
   }
 
   static guildMfa(guildId: Snowflake): string {
-    return `guilds/${guildId}/mfa`;
+    return encodeEndpoint`guilds/${guildId}/mfa`;
   }
 
   static guildOnboarding(guildId: Snowflake): string {
-    return `guilds/${guildId}/onboarding`;
+    return encodeEndpoint`guilds/${guildId}/onboarding`;
   }
 
   static guildPreview(guildId: Snowflake): string {
-    return `guilds/${guildId}/preview`;
+    return encodeEndpoint`guilds/${guildId}/preview`;
   }
 
   static guildPrune(guildId: Snowflake): string {
-    return `guilds/${guildId}/prune`;
+    return encodeEndpoint`guilds/${guildId}/prune`;
   }
 
   static guildRegions(guildId: Snowflake): string {
-    return `guilds/${guildId}/regions`;
+    return encodeEndpoint`guilds/${guildId}/regions`;
   }
 
   static guildRole(guildId: Snowflake, roleId: Snowflake): string {
-    return `guilds/${guildId}/roles/${roleId}`;
+    return encodeEndpoint`guilds/${guildId}/roles/${roleId}`;
   }
 
   static guildRoles(guildId: Snowflake): string {
-    return `guilds/${guildId}/roles`;
+    return encodeEndpoint`guilds/${guildId}/roles`;
   }
 
   static guildScheduledEvent(guildId: Snowflake, scheduledEventId: Snowflake): string {
-    return `guilds/${guildId}/scheduled-events/${scheduledEventId}`;
+    return encodeEndpoint`guilds/${guildId}/scheduled-events/${scheduledEventId}`;
   }
 
   static guildScheduledEventUsers(guildId: Snowflake, scheduledEventId: Snowflake): string {
-    return `guilds/${guildId}/scheduled-events/${scheduledEventId}/users`;
+    return encodeEndpoint`guilds/${guildId}/scheduled-events/${scheduledEventId}/users`;
   }
 
   static guildScheduledEvents(guildId: Snowflake): string {
-    return `guilds/${guildId}/scheduled-events`;
+    return encodeEndpoint`guilds/${guildId}/scheduled-events`;
   }
 
   static guildSoundboardSound(guildId: Snowflake, soundboardSoundId: Snowflake): string {
-    return `guilds/${guildId}/soundboard-sounds/${soundboardSoundId}`;
+    return encodeEndpoint`guilds/${guildId}/soundboard-sounds/${soundboardSoundId}`;
   }
 
   static guildSoundboardSounds(guildId: Snowflake): string {
-    return `guilds/${guildId}/soundboard-sounds`;
+    return encodeEndpoint`guilds/${guildId}/soundboard-sounds`;
   }
 
   static guildSticker(guildId: Snowflake, stickerId: Snowflake): string {
-    return `guilds/${guildId}/stickers/${stickerId}`;
+    return encodeEndpoint`guilds/${guildId}/stickers/${stickerId}`;
   }
 
   static guildStickers(guildId: Snowflake): string {
-    return `guilds/${guildId}/stickers`;
+    return encodeEndpoint`guilds/${guildId}/stickers`;
   }
 
   static guildTemplate(guildId: Snowflake, templateCode: string): string {
-    return `guilds/${guildId}/templates/${templateCode}`;
+    return encodeEndpoint`guilds/${guildId}/templates/${templateCode}`;
   }
 
   static guildTemplates(guildId: Snowflake): string {
-    return `guilds/${guildId}/templates`;
+    return encodeEndpoint`guilds/${guildId}/templates`;
   }
 
   static guildThreadsActive(guildId: Snowflake): string {
-    return `guilds/${guildId}/threads/active`;
+    return encodeEndpoint`guilds/${guildId}/threads/active`;
   }
 
   static guildVanityUrl(guildId: Snowflake): string {
-    return `guilds/${guildId}/vanity-url`;
+    return encodeEndpoint`guilds/${guildId}/vanity-url`;
   }
 
   static guildVoiceState(guildId: Snowflake, userId: Snowflake | "@me"): string {
-    return `guilds/${guildId}/voice-states/${userId}`;
+    return encodeEndpoint`guilds/${guildId}/voice-states/${userId}`;
   }
 
   static guildWelcomeScreen(guildId: Snowflake): string {
-    return `guilds/${guildId}/welcome-screen`;
+    return encodeEndpoint`guilds/${guildId}/welcome-screen`;
   }
 
   static guildWidget(guildId: Snowflake): string {
-    return `guilds/${guildId}/widget`;
+    return encodeEndpoint`guilds/${guildId}/widget`;
   }
 
   static guildWidgetJSON(guildId: Snowflake): string {
-    return `guilds/${guildId}/widget.json`;
+    return encodeEndpoint`guilds/${guildId}/widget.json`;
   }
 
   static guildWidgetPNG(guildId: Snowflake): string {
-    return `guilds/${guildId}/widget.png`;
+    return encodeEndpoint`guilds/${guildId}/widget.png`;
   }
 
   static guilds(): string {
@@ -318,7 +319,7 @@ export class Endpoints {
   }
 
   static invite(inviteCode: string): string {
-    return `invites/${inviteCode}`;
+    return encodeEndpoint`invites/${inviteCode}`;
   }
 
   static lobbies(): string {
@@ -326,23 +327,23 @@ export class Endpoints {
   }
 
   static lobby(lobbyId: Snowflake): string {
-    return `lobbies/${lobbyId}`;
+    return encodeEndpoint`lobbies/${lobbyId}`;
   }
 
   static lobbyChannelLinking(lobbyId: Snowflake): string {
-    return `lobbies/${lobbyId}/channel-linking`;
+    return encodeEndpoint`lobbies/${lobbyId}/channel-linking`;
   }
 
   static lobbyMember(lobbyId: Snowflake, userId: Snowflake | "@me"): string {
-    return `lobbies/${lobbyId}/members/${userId}`;
+    return encodeEndpoint`lobbies/${lobbyId}/members/${userId}`;
   }
 
   static skuSubscription(skuId: Snowflake, subscriptionId: Snowflake): string {
-    return `skus/${skuId}/subscriptions/${subscriptionId}`;
+    return encodeEndpoint`skus/${skuId}/subscriptions/${subscriptionId}`;
   }
 
   static skuSubscriptions(skuId: Snowflake): string {
-    return `skus/${skuId}/subscriptions`;
+    return encodeEndpoint`skus/${skuId}/subscriptions`;
   }
 
   static soundboardDefaultSounds(): string {
@@ -350,7 +351,7 @@ export class Endpoints {
   }
 
   static stageInstance(stageInstanceId: Snowflake): string {
-    return `stage-instances/${stageInstanceId}`;
+    return encodeEndpoint`stage-instances/${stageInstanceId}`;
   }
 
   static stageInstances(): string {
@@ -358,11 +359,11 @@ export class Endpoints {
   }
 
   static sticker(stickerId: Snowflake): string {
-    return `stickers/${stickerId}`;
+    return encodeEndpoint`stickers/${stickerId}`;
   }
 
   static stickerPack(stickerPackId: Snowflake): string {
-    return `sticker-packs/${stickerPackId}`;
+    return encodeEndpoint`sticker-packs/${stickerPackId}`;
   }
 
   static stickerPacks(): string {
@@ -370,31 +371,31 @@ export class Endpoints {
   }
 
   static template(templateCode: string): string {
-    return `guilds/templates/${templateCode}`;
+    return encodeEndpoint`guilds/templates/${templateCode}`;
   }
 
   static user(userId: Snowflake | "@me"): string {
-    return `users/${userId}`;
+    return encodeEndpoint`users/${userId}`;
   }
 
   static userApplicationRoleConnection(userId: "@me", applicationId: Snowflake): string {
-    return `users/${userId}/applications/${applicationId}/role-connections`;
+    return encodeEndpoint`users/${userId}/applications/${applicationId}/role-connections`;
   }
 
   static userChannels(userId: "@me"): string {
-    return `users/${userId}/channels`;
+    return encodeEndpoint`users/${userId}/channels`;
   }
 
   static userConnections(userId: "@me"): string {
-    return `users/${userId}/connections`;
+    return encodeEndpoint`users/${userId}/connections`;
   }
 
   static userGuildMember(userId: "@me", guildId: Snowflake): string {
-    return `users/${userId}/guilds/${guildId}/member`;
+    return encodeEndpoint`users/${userId}/guilds/${guildId}/member`;
   }
 
   static userGuilds(userId: "@me"): string {
-    return `users/${userId}/guilds`;
+    return encodeEndpoint`users/${userId}/guilds`;
   }
 
   static voiceRegions(): string {
@@ -402,18 +403,20 @@ export class Endpoints {
   }
 
   static webhook(webhookId: Snowflake, webhookToken?: string): string {
-    return webhookToken ? `webhooks/${webhookId}/${webhookToken}` : `webhooks/${webhookId}`;
+    return webhookToken ? encodeEndpoint`webhooks/${webhookId}/${webhookToken}` : encodeEndpoint`webhooks/${webhookId}`;
   }
 
   static webhookGitHub(webhookId: Snowflake, webhookToken: string): string {
-    return `webhooks/${webhookId}/${webhookToken}/github`;
+    return encodeEndpoint`webhooks/${webhookId}/${webhookToken}/github`;
   }
 
   static webhookMessage(webhookId: Snowflake, webhookToken: string, messageId: Snowflake): string {
-    return `webhooks/${webhookId}/${webhookToken}/messages/${messageId}`;
+    return encodeEndpoint`webhooks/${webhookId}/${webhookToken}/messages/${messageId}`;
   }
 
   static webhookSlack(webhookId: Snowflake, webhookToken: string): string {
-    return `webhooks/${webhookId}/${webhookToken}/slack`;
+    return encodeEndpoint`webhooks/${webhookId}/${webhookToken}/slack`;
   }
 }
+
+Object.freeze(Endpoints);
