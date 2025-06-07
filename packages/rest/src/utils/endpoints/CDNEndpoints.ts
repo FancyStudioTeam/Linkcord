7ae4cef24f5c2f9b9db1a87826f5027430c83312
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-07 21:06:18 +0200

    chore(rest)!: singularize and add more cdn endpoints

--- packages/rest/src/utils/endpoints/CDNEndpoints.ts
+++ packages/rest/src/utils/endpoints/CDNEndpoints.ts
@@ -4,75 +4,99 @@ import type { Snowflake } from "@fancystudioteam/linkcord-types";
  * @public
  */
 export class CDNEndpoints {
-  static appAssetAchievementIcons(applicationId: Snowflake, achievementId: Snowflake, achievementIcon: string): string {
+  static applicationAssetAchievementIcon(
+    applicationId: Snowflake,
+    achievementId: Snowflake,
+    achievementIcon: string,
+  ): string {
     return `app-assets/${applicationId}/achievements/${achievementId}/icons/${achievementIcon}`;
   }
 
-  static appAssetStore(applicationId: Snowflake, assetId: Snowflake): string {
+  static applicationAssetStore(applicationId: Snowflake, assetId: Snowflake): string {
     return `app-assets/${applicationId}/store/${assetId}`;
   }
 
-  static appAssets(applicationId: Snowflake, assetId: Snowflake): string {
+  static applicationAsset(applicationId: Snowflake, assetId: Snowflake): string {
     return `app-assets/${applicationId}/${assetId}`;
   }
 
-  static appIcons(applicationId: Snowflake, applicationCoverOrIcon: string): string {
+  static applicationIcon(applicationId: Snowflake, applicationCoverOrIcon: string): string {
     return `app-icons/${applicationId}/${applicationCoverOrIcon}`;
   }
 
-  static avatarDecorationPresets(userAvatarDecorationAsset: string): string {
+  static attachment(channelId: Snowflake, messageId: Snowflake, attachmentName: string): string {
+    return `attachments/${channelId}/${messageId}/${attachmentName}`;
+  }
+
+  static avatarDecorationPreset(userAvatarDecorationAsset: string): string {
     return `avatar-decorations/${userAvatarDecorationAsset}`;
   }
 
-  static avatars(userId: Snowflake, userAvatar: string): string {
+  static avatar(userId: Snowflake, userAvatar: string): string {
     return `avatars/${userId}/${userAvatar}`;
   }
 
-  static banners(guildOrUserId: Snowflake, guildOrUserBanner: string): string {
+  static badgeIcon(badgeIcon: string): string {
+    return `badge-icons/${badgeIcon}`;
+  }
+
+  static banner(guildOrUserId: Snowflake, guildOrUserBanner: string): string {
     return `banners/${guildOrUserId}/${guildOrUserBanner}`;
   }
 
-  static discoverySplashes(guildId: Snowflake, guildDiscoverySplash: string): string {
+  static clanBadge(guildId: Snowflake, clanBadge: string): string {
+    return `clan-badges/${guildId}/${clanBadge}`;
+  }
+
+  static clanBanner(guildId: Snowflake, clanBanner: string): string {
+    return `clan-banners/${guildId}/${clanBanner}`;
+  }
+
+  static discoverySplash(guildId: Snowflake, guildDiscoverySplash: string): string {
     return `discovery-splashes/${guildId}/${guildDiscoverySplash}`;
   }
 
-  static embedAvatars(index: number): string {
+  static embedAvatar(index: number): string {
     return `embed/avatars/${index}`;
   }
 
-  static emojis(emojiId: Snowflake): string {
+  static emoji(emojiId: Snowflake): string {
     return `emojis/${emojiId}`;
   }
 
-  static guildEvents(guildId: Snowflake, scheduledEventId: Snowflake, scheduledEventCover: string): string {
+  static guildScheduledEvent(guildId: Snowflake, scheduledEventId: Snowflake, scheduledEventCover: string): string {
     return `guilds/${guildId}/scheduled-events/${scheduledEventId}/${scheduledEventCover}`;
   }
 
-  static guildUserAvatars(guildId: Snowflake, userId: Snowflake, userAvatar: string): string {
+  static guildUserAvatar(guildId: Snowflake, userId: Snowflake, userAvatar: string): string {
     return `guilds/${guildId}/users/${userId}/avatars/${userAvatar}`;
   }
 
-  static guildUserBanners(guildId: Snowflake, userId: Snowflake, userBanner: string): string {
+  static guildUserBanner(guildId: Snowflake, userId: Snowflake, userBanner: string): string {
     return `guilds/${guildId}/users/${userId}/banners/${userBanner}`;
   }
 
-  static icons(guildId: Snowflake, guildIcon: string): string {
+  static icon(guildId: Snowflake, guildIcon: string): string {
     return `icons/${guildId}/${guildIcon}`;
   }
 
-  static roleIcons(roleId: Snowflake, roleIcon: string): string {
+  static roleIcon(roleId: Snowflake, roleIcon: string): string {
     return `role-icons/${roleId}/${roleIcon}`;
   }
 
-  static splashes(guildId: Snowflake, guildSplash: string): string {
+  static soundboardSound(soundboardSoundId: Snowflake): string {
+    return `soundboard-sounds/${soundboardSoundId}`;
+  }
+
+  static splash(guildId: Snowflake, guildSplash: string): string {
     return `splashes/${guildId}/${guildSplash}`;
   }
 
-  static stickers(stickerId: Snowflake): string {
+  static sticker(stickerId: Snowflake): string {
     return `stickers/${stickerId}`;
   }
 
-  static teamIcons(teamId: Snowflake, teamIcon: string): string {
+  static teamIcon(teamId: Snowflake, teamIcon: string): string {
     return `team-icons/${teamId}/${teamIcon}`;
   }
 }
