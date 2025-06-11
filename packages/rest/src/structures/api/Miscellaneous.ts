745380d4c00ea0ac2e4e49d581f7c1e0b5f69099
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-12 00:21:14 +0200

    chore(rest): missing `getVoiceRegions`

--- packages/rest/src/structures/api/Miscellaneous.ts
+++ packages/rest/src/structures/api/Miscellaneous.ts
@@ -2,6 +2,7 @@ import type {
   RESTGetDefaultSoundboardSounds,
   RESTGetGateway,
   RESTGetGatewayBot,
+  RESTGetVoiceRegions,
 } from "@fancystudioteam/linkcord-types";
 import { Endpoints } from "../../utils/index.js";
 import { BaseAPI } from "./base/BaseAPI.js";
@@ -29,5 +30,12 @@ export class Miscellaneous extends BaseAPI {
    */
   async getGatewayBot<Result = RESTGetGatewayBot>(): Promise<Result> {
     return await super.get<Result>(Endpoints.gatewayBot());
+  }
+
+  /**
+   * @see https://discord.com/developers/docs/resources/voice#list-voice-regions
+   */
+  async getVoiceRegions<Result = RESTGetVoiceRegions>(): Promise<Result> {
+    return await super.get<Result>(Endpoints.voiceRegions());
   }
 }
