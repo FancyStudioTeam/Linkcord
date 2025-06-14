b718167c8844eb48d2c6b0bd363f413b804cf87a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-14 16:08:49 +0200

    chore(rest)!: delete old monetization apis

--- packages/rest/src/structures/api/monetization/MonetizationAPI.ts
+++
@@ -1,9 +0,0 @@
-import { Mixin } from "ts-mixer";
-import { Entitlement } from "./Entitlement.js";
-import { SKU } from "./SKU.js";
-import { Subscription } from "./Subscription.js";
-
-/**
- * @public
- */
-export class MonetizationAPI extends Mixin(Entitlement, SKU, Subscription) {}
