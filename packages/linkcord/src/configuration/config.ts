d4536975079406610d7dcf97d7fecb8a18618fed
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-19 22:56:03 +0200

    chore(linkcord)!: delete old `configuration` files

--- packages/linkcord/src/configuration/config.ts
+++
@@ -1,9 +0,0 @@
-import { options } from "./internal.js";
-import type { LinkcordOptions } from "./types.js";
-
-export const defineConfig = (config: LinkcordOptions): Readonly<LinkcordOptions> => {
-  Object.assign(options, config);
-  Object.freeze(options);
-
-  return options;
-};
