37cef8fbc987315d177da63934b84a1d6a75aaf8
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-20 21:11:36 +0200

    feat(linkcord): `createEvent`

---
+++ packages/linkcord/src/handlers/events/createEvent.ts
@@ -0,0 +1,22 @@
+import type { ClientEventsMap } from "../../client/types/Client.js";
+
+export const createEvent = <EventName extends ClientEventNames>(
+  options: CreateEventOptions<EventName, CreateEventOptionsData<EventName>>,
+) => options;
+
+export interface CreateEventOptions<
+  ClientEventName extends ClientEventNames,
+  ClientEventData extends CreateEventOptionsData<ClientEventName>,
+> {
+  data: ClientEventData;
+  run: (...data: ClientEventsMap[ClientEventName]) => void;
+}
+
+interface CreateEventOptionsData<EventName extends ClientEventNames> {
+  name: EventName;
+  once?: boolean;
+}
+
+export type ClientEventNames = keyof ClientEventsMap;
+
+export type EventData = ReturnType<typeof createEvent>;
