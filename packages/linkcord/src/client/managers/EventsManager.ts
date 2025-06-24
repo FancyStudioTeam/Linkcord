5757a839e79d5acd4bec40fb2be427b27ba9175a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-24 10:24:37 +0200

    feat(EventsManager): add `listeners`

--- packages/linkcord/src/client/managers/EventsManager.ts
+++ packages/linkcord/src/client/managers/EventsManager.ts
@@ -9,16 +9,24 @@ export class EventsManager {
   readonly client: BaseClient;
   readonly handlers: Map<ClientEventsString, EventData[]> = new Map();
 
+  listeners: Partial<EventListeners> = {};
+
   constructor(client: BaseClient) {
     this.client = client;
   }
 
   emit<ClientEvent extends ClientEventsString>(name: ClientEvent, ...data: ClientEventsMap[ClientEvent]): void {
-    const { handlers } = this;
+    const { handlers, listeners } = this;
     const handlersArray = handlers.get(name) ?? [];
 
     for (const handler of handlersArray) {
       handler.run(...data);
     }
+
+    listeners[name]?.(...data);
   }
 }
+
+type EventListeners = {
+  [Key in keyof ClientEventsMap]: (...data: ClientEventsMap[Key]) => unknown;
+};
