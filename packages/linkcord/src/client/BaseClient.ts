34dbeaca01e46b647dc8770516108e3bb4fafd97
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-24 10:25:26 +0200

    chore(BaseClient)!: remove `emit` method

--- packages/linkcord/src/client/BaseClient.ts
+++ packages/linkcord/src/client/BaseClient.ts
@@ -2,7 +2,6 @@ import { join } from "node:path";
 import { LinkcordConfiguration } from "../configuration/structures/LinkcordConfiguration.js";
 import { CommandsLoader } from "../handlers/commands/loaders/CommandsLoader.js";
 import { EventsLoader } from "../handlers/events/loaders/EventsLoader.js";
-import type { ClientEventsMap, ClientEventsString } from "./ClientEvents.js";
 import { EventsManager } from "./managers/EventsManager.js";
 
 /**
@@ -13,12 +12,6 @@ export class BaseClient {
 
   constructor() {
     (async () => await LinkcordConfiguration.loadConfigurationFile())();
-  }
-
-  emit<ClientEvent extends ClientEventsString>(name: ClientEvent, ...data: ClientEventsMap[ClientEvent]): void {
-    const { events } = this;
-
-    events.emit(name, ...data);
   }
 
   async init(): Promise<void> {
