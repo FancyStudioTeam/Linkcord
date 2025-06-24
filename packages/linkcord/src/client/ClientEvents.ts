bd6a9d50c9addd5ac40637c216cedd5154229ba6
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-24 13:06:41 +0200

    feat(events): add `debug` event

--- packages/linkcord/src/client/ClientEvents.ts
+++ packages/linkcord/src/client/ClientEvents.ts
@@ -7,6 +7,7 @@ import type { GatewayEvent } from "../types/raw/index.js";
  * @public
  */
 export interface ClientEventsMap {
+  debug: [message: string, shard?: GatewayShard];
   interactionCreate: [interaction: ChatInputCommandInteraction, shard: GatewayShard];
   ready: [user: User];
   shardPacket: [shard: GatewayShard, packet: GatewayEvent];
