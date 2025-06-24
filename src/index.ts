564b10f1ac059ce03bed11f4957c91449f871b97
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-24 20:27:15 +0200

    chore: export `rest`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./client/index.js";
 export * from "./configuration/index.js";
 export * from "./handlers/index.js";
+export * from "./rest/index.js";
 export * from "./structures/index.js";
 export * from "./types/index.js";
 export * from "./utils/index.js";
