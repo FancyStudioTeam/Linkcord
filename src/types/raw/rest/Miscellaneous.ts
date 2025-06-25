ec127a7c1541d25feb11039269acd1f1b58e713f
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-25 20:30:51 +0200

    chore: move gateway types into miscellaneous

--- src/types/raw/rest/Miscellaneous.ts
+++ src/types/raw/rest/Miscellaneous.ts
@@ -1,3 +1,4 @@
+import type { APIGateway, APIGatewayBot } from "../payloads/Gateway.js";
 import type { APIDefaultSoundboardSound } from "../payloads/Soundboards.js";
 import type { APIVoiceRegion } from "../payloads/Voice.js";
 
@@ -6,6 +7,18 @@ import type { APIVoiceRegion } from "../payloads/Voice.js";
  * @see https://discord.com/developers/docs/resources/soundboard#list-default-soundboard-sounds
  */
 export type RESTGetDefaultSoundboardSounds = APIDefaultSoundboardSound[];
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/events/gateway#get-gateway
+ */
+export type RESTGetGateway = APIGateway;
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/events/gateway#get-gateway-bot
+ */
+export type RESTGetGatewayBot = APIGatewayBot;
 
 /**
  * @public
