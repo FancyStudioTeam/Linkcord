5e4308119a1013cc104879442bc769aed1bde603
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-25 16:54:24 +0200

    chore: add files

---
+++ src/types/raw/payloads/Voice.ts
@@ -0,0 +1,34 @@
+import type { ISO8601Date, Snowflake } from "../shared/discord.js";
+import type { APIGuildMember } from "./Guilds.js";
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/resources/voice#voice-region-object-voice-region-structure
+ */
+export interface APIVoiceRegion {
+  custom: boolean;
+  deprecated: boolean;
+  id: string;
+  name: string;
+  optimal: boolean;
+}
+
+/**
+ * @public
+ * @see https://discord.com/developers/docs/resources/voice#voice-state-object-voice-state-structure
+ */
+export interface APIVoiceState {
+  channel_id: Snowflake | null;
+  deaf: boolean;
+  guild_id?: Snowflake;
+  member?: APIGuildMember;
+  mute: boolean;
+  request_to_speak_timestamp: ISO8601Date | null;
+  self_deaf: boolean;
+  self_mute: boolean;
+  self_stream?: boolean;
+  self_video: boolean;
+  session_id: string;
+  suppress: boolean;
+  user_id: Snowflake;
+}
