dfcdf8e856fa5d37a4e077a61228f6655d6eef3b
LonelyDeathVGX <99565725+LonelyDeathVGX@users.noreply.github.com>
2025-06-25 17:33:45 +0200

    chore: renames

--- src/types/raw/voice/events/Speaking.ts
+++ src/types/raw/voice/events/Speaking.ts
@@ -6,11 +6,6 @@ import type { VoiceOpcodes } from "../event.js";
  * @see https://discord.com/developers/docs/topics/voice-connections#speaking-example-speaking-payload
  */
 export interface VoiceSpeakingPayload {
-  /**
-   * @remarks
-   * - This field value should be set to `0` for bots that are using the voice
-   *   gateway.
-   */
   delay: number;
   speaking: number;
   ssrc: number;
