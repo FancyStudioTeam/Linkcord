9adcabdef175d669eabf8fe6140d74bcc8bc0e48
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-06-30 21:07:08 +0200

    chore: add `vitest-tsconfig-paths`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vitest-tsconfig-paths";
 
 export default defineConfig({
+    plugins: [
+        tsconfigPaths({
+            projects: ["tsconfig.json"],
+        }),
+    ],
     test: {
         reporters: ["verbose"],
     },
