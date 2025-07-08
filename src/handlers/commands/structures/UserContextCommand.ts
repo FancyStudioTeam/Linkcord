52561130a32db28d498cdd1a00d3bf0f5f3505ea
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-08 19:57:08 +0200

    chore: changes

--- src/handlers/commands/structures/UserContextCommand.ts
+++ src/handlers/commands/structures/UserContextCommand.ts
@@ -1,11 +1,24 @@
 import type { UserContextCommandOptions } from "#handlers/decorators/Declare.js";
 import { ApplicationCommandTypes } from "#types/index.js";
+import type { CreateUserContextApplicationCommandOptions } from "#types/parsed/ApplicationCommands.js";
 
+/**
+ * @public
+ */
 export abstract class UserContextCommand {
 	readonly declareOptions: UserContextCommandOptions | null = null;
 	readonly type = ApplicationCommandTypes.User;
 
-	/**
-	 * TODO: Implement `toJSON` method.
-	 */
+	toJSON(): CreateUserContextApplicationCommandOptions {
+		const { declareOptions, type } = this;
+
+		if (!declareOptions) {
+			throw new Error("Declaration is missing from the user context command instance.");
+		}
+
+		return {
+			...declareOptions,
+			type,
+		};
+	}
 }
--- src/handlers/commands/types.ts
+++ src/handlers/commands/types.ts
@@ -1,5 +1,11 @@
 import type { UserContextCommand } from "./structures/UserContextCommand.js";
 
+/**
+ * @internal
+ */
 type Instance<Class> = new (...args: unknown[]) => Class;
 
+/**
+ * @internal
+ */
 export type UserContextCommandInstance = Instance<UserContextCommand>;
