76db8cf91af525b4ceca888257dd69b90ae8aea6
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-20 15:42:52 +0200

    docs: add comments to `Uncached`

--- src/structures/Uncached.ts
+++ src/structures/Uncached.ts
@@ -1,12 +1,25 @@
 import type { Snowflake } from "#types/index.js";
 
 /**
+ * Represents an uncached entity.
+ *
  * @public
  */
 export class Uncached {
-	id: Snowflake;
-	uncached = true;
+	/**
+	 * The ID of the entity.
+	 */
+	readonly id: Snowflake;
+	/**
+	 * Whether the entity is uncached.
+	 */
+	readonly uncached = true;
 
+	/**
+	 * Creates a new {@link Uncached} instance.
+	 *
+	 * @param id - The ID of the entity.
+	 */
 	constructor(id: Snowflake) {
 		this.id = id;
 	}
