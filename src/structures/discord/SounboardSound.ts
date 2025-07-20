05b1b31e025493ee127090dd7d716e5510ff436b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-20 16:41:48 +0200

    chore: add some methods to `SoundboardSound`

--- src/structures/discord/SounboardSound.ts
+++ src/structures/discord/SounboardSound.ts
@@ -1,73 +1,171 @@
-import type { APIGuildSoundboardSound, Snowflake } from "#types/index.js";
+import type { Client } from "#client/Client.js";
+import type { APIGuildSoundboardSound, JSONSoundboardSound, Snowflake } from "#types/index.js";
+import type { MaybeUncached } from "#utils/types.js";
 import { Base } from "./base/Base.js";
 import { User } from "./User.js";
 
 /**
+ * Represents a Discord guild soundboard sound.
+ *
  * @public
  */
 export class SoundboardSound extends Base {
-	available: boolean;
-	emojiId: Snowflake | null;
-	emojiName: string | null;
-	guildId: Snowflake;
+	/**
+	 * Whether the soundboard sound is available to use.
+	 */
+	available!: boolean;
+	/**
+	 * The emoji of the soundboard sound.
+	 */
+	emojiId!: Snowflake | null;
+	/**
+	 * The emoji name of the soundboard sound.
+	 */
+	emojiName!: string | null;
+	/**
+	 * The ID of the guild at which the soundboard sound belongs.
+	 */
+	readonly guildId: Snowflake;
+	/**
+	 * The name of the soundboard sound.
+	 */
 	name: string;
-	soundId: Snowflake;
-	user: User | null;
+	/**
+	 * The ID of the soundboard sound.
+	 */
+	readonly soundId: Snowflake;
+	/**
+	 * The user who created the soundboard sound, if any.
+	 */
+	user!: User | null;
+	/**
+	 * The volume of the soundboard sound.
+	 */
 	volume: number;
 
-	constructor(id: Snowflake, data: APIGuildSoundboardSound) {
-		super(id);
+	/**
+	 * Creates a new {@link SoundboardSound | `SoundboardSound`} instance from
+	 * raw Discord API data.
+	 *
+	 * @param client - The client that instantiated the soundboard sound.
+	 * @param data - The raw Discord API soundboard sound data.
+	 */
+	constructor(client: Client, data: APIGuildSoundboardSound) {
+		super(client);
 
-		const { available, emoji_id, emoji_name, guild_id, name, sound_id, user, volume } = data;
+		const { guild_id, name, sound_id, volume } = data;
 
-		this.available = available;
-		this.emojiId = emoji_id;
-		this.emojiName = emoji_name;
 		this.guildId = guild_id;
 		this.name = name;
 		this.soundId = sound_id;
-		this.user = user ? new User(user.id, user) : null;
 		this.volume = volume;
-		this.patch(data);
+		this._patch(data);
 	}
 
 	/**
+	 * Patches the soundboard sound properties with the given data.
+	 *
+	 * @param data - The data to use when patching the soundboard sound
+	 * properties.
+	 *
 	 * @internal
 	 */
-	private patch(data: SoundboardSoundPatchData): void {
-		const { available, emoji_id, emoji_name, guild_id, name, sound_id, user, volume } = data;
+	protected _patch(data: SoundboardSoundPatchData = {}): void {
+		const { available, emoji_id, emoji_name, name, user, volume } = data;
 
 		if (available) {
 			this.available = available;
+		} else {
+			this.available ??= false;
 		}
 
 		if (emoji_id) {
 			this.emojiId = emoji_id;
+		} else {
+			this.emojiId ??= null;
 		}
 
 		if (emoji_name) {
 			this.emojiName = emoji_name;
-		}
-
-		if (guild_id) {
-			this.guildId = guild_id;
+		} else {
+			this.emojiName ??= null;
 		}
 
 		if (name) {
 			this.name = name;
 		}
 
-		if (sound_id) {
-			this.soundId = sound_id;
-		}
-
 		if (user) {
-			this.user = user ? new User(user.id, user) : null;
+			const { client } = this;
+
+			this.user = new User(client, user);
+		} else {
+			this.user ??= null;
 		}
 
 		if (volume) {
 			this.volume = volume;
 		}
+	}
+
+	// TODO: Delete the soundboard sound from the guild.
+	/**
+	 * Deletes the soundboard sound from the guild.
+	 *
+	 * @param reason - The reason for deleting the soundboard sound.
+	 *
+	 * @returns The deleted {@link SoundboardSound | `SoundboardSound`}, if
+	 * any.
+	 */
+	delete(_reason?: string): Promise<MaybeUncached<SoundboardSound>> {
+		return Promise.resolve(this);
+	}
+
+	// TODO: Update the soundboard sound.
+	/**
+	 * Updates the soundboard sound.
+	 *
+	 * @param options - The options to use when updating the soundboard sound.
+	 *
+	 * @returns The updated {@link SoundboardSound | `SoundboardSound`}
+	 * instance.
+	 */
+	edit(_options: unknown): Promise<SoundboardSound> {
+		return Promise.resolve(this);
+	}
+
+	// TODO: Send the soundboard sound to the user voice channel.
+	/**
+	 * Sends the soundboard sound to the user voice channel.
+	 *
+	 * @param channelId - The ID of the voice channel at which to send the
+	 * soundboard sound.
+	 */
+	send(_channelId: Snowflake): Promise<void> {
+		// const { guildId, rest, soundId } = this;
+
+		return Promise.resolve();
+	}
+
+	/**
+	 * Converts the {@link SoundboardSound | `SoundboardSound`} instance to a
+	 * JSON object.
+	 *
+	 * @returns The JSON soundboard sound data.
+	 */
+	toJSON(): JSONSoundboardSound {
+		const { available, emojiId, emojiName, guildId, name, soundId, user, volume } = this;
+
+		return {
+			available,
+			emojiId,
+			emojiName,
+			guildId,
+			name,
+			soundId,
+			user,
+			volume,
+		};
 	}
 }
 
