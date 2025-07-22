11ece324f84002bc6675b8b8296e9c23bffc7c23
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-22 20:57:13 +0200

    chore: omit `client` from `JSONProperties`

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,14 +3,19 @@ import type { Uncached } from "#structures/Uncached.js";
 /**
  * @public
  */
-export type JSONProperties<Instance extends Newable> = {
-	/**
-	 * biome-ignore lint/complexity/noBannedTypes: Expect any function.
-	 */
-	[Key in keyof InstanceType<Instance> as InstanceType<Instance>[Key] extends Function
-		? never
-		: Key]: InstanceType<Instance>[Key];
-};
+export type JSONProperties<Instance extends Newable> = Readonly<
+	Omit<
+		{
+			/**
+			 * biome-ignore lint/complexity/noBannedTypes: Expect any function.
+			 */
+			[Key in keyof InstanceType<Instance> as InstanceType<Instance>[Key] extends Function
+				? never
+				: Key]: InstanceType<Instance>[Key];
+		},
+		"client"
+	>
+>;
 
 /**
  * @public
@@ -19,6 +24,10 @@ export type MaybeUncached<Type> = Type | Uncached;
 
 /**
  * biome-ignore-start lint/suspicious/noExplicitAny: Expect anything.
+ *
+ * @remarks
+ * Use suppression ranges to prevent IntelliSense from inferring Biome
+ * comments.
  */
 /**
  * @internal
