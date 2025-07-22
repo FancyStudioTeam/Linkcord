c5ca82a95c802f4b96e9fba3c054477d317f054d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-22 23:35:24 +0200

    chore: add `_api` to `Base`

--- src/structures/discord/base/Base.ts
+++ src/structures/discord/base/Base.ts
@@ -1,5 +1,6 @@
 import type { Client } from "#client/Client.js";
 import type { RESTManager } from "#rest/index.js";
+import type { APIManager } from "#rest/structures/APIManager.js";
 
 /**
  * Represents a base class for most of the structures.
@@ -10,17 +11,34 @@ export abstract class Base {
 	/**
 	 * The client that instantiated the base.
 	 */
-	readonly client: Client;
+	protected readonly client: Client;
 	/**
 	 *  The REST manager to perform requests within the extended classes.
 	 */
 	protected readonly rest: RESTManager;
 
+	/**
+	 * Creates a new {@link Base | `Base`} instance.
+	 *
+	 * @param client - The client that instantiated the extended class.
+	 */
 	constructor(client: Client) {
 		const { rest } = client;
 
 		this.client = client;
 		this.rest = rest;
+	}
+
+	/**
+	 * Gets the API manager of the extended class.
+	 *
+	 * @internal
+	 */
+	protected get _api(): APIManager {
+		const { rest } = this;
+		const { api } = rest;
+
+		return api;
 	}
 
 	/**
