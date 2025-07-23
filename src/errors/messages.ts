1ab08c69048340026a68894df7e65d5ab1728cca
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-23 11:44:20 +0200

    chore: use functions

--- src/errors/messages.ts
+++ src/errors/messages.ts
@@ -1,9 +1,18 @@
+/**
+ * biome-ignore-all lint/style/useNamingConvention: Use screaming snake case
+ * for these messages.
+ */
+
 import { ListFormatter } from "./utils/index.js";
 
-export const METHOD_NOT_IMPLEMENTED = () => "Method not implemented.";
+export function METHOD_NOT_IMPLEMENTED(): string {
+	return "This method is not implemented yet";
+}
 
-export const MISSING_REQUIRED_FIELD_FROM_DATA = (field: string, objectDescription: string) =>
-	`Field "${field}" is missing from the ${objectDescription} object but should be always present in the data.`;
+export function MISSING_REQUIRED_FIELD_FROM_DATA(field: string, objectDescription: string): string {
+	return `Field "${field}" is missing from the "${objectDescription}" object but should be always present in the data.`;
+}
 
-export const MISSING_REQUIRED_FIELDS_FROM_DATA = (fields: string[], objectDescription: string) =>
-	`Fields "${ListFormatter.conjunction(fields)}" are missing from the ${objectDescription} object but should be always present in the data.`;
+export function MISSING_REQUIRED_FIELDS_FROM_DATA(fields: string[], objectDescription: string) {
+	return `Fields "${ListFormatter.conjunction(fields)}" are missing from the "${objectDescription}" object but should be always present in the data.`;
+}
