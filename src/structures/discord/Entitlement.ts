e754fe8096ecee084b54f16aceaa5bdc1244d266
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-24 19:56:57 +0200

    chore: update `Entitlement`

--- src/structures/discord/Entitlement.ts
+++ src/structures/discord/Entitlement.ts
@@ -1,98 +1,159 @@
-import type { APIEntitlement, EntitlementTypes, Snowflake } from "#types/index.js";
+import type { Client } from "#client/Client.js";
+import type { APIEntitlement, EntitlementTypes, JSONEntitlement, Snowflake } from "#types/index.js";
 import { Base } from "./base/Base.js";
 
 /**
+ * Represents a Discord entitlement.
+ *
  * @public
  */
 export class Entitlement extends Base {
-	applicationId: Snowflake;
-	consumed: boolean;
-	deleted: boolean;
-	endsAt: Date | null;
-	guildId: Snowflake | null;
-	skuId: Snowflake;
-	startsAt: Date | null;
-	type: EntitlementTypes;
-	userId: Snowflake | null;
+	/**
+	 * The ID of the application that the entitlement belongs.
+	 */
+	readonly applicationId: Snowflake;
+	/**
+	 * Whether the entitlement was consumed.
+	 */
+	consumed!: boolean;
+	/**
+	 * Whether the entitlement was deleted.
+	 */
+	deleted!: boolean;
+	/**
+	 * The timestamp at which the entitlement is no longer valid.
+	 */
+	endsTimestamp!: number | null;
+	/**
+	 * The ID of the guild at which the entitlement was granted.
+	 */
+	readonly guildId: Snowflake | null;
+	/**
+	 * The ID of the entitlement.
+	 */
+	readonly id: Snowflake;
+	/**
+	 * The ID of the SKU associated with the entitlement.
+	 */
+	readonly skuId: Snowflake;
+	/**
+	 * The timestamp at which the entitlement is valid.
+	 */
+	startsTimestamp!: number | null;
+	/**
+	 * The type of the entitlement.
+	 */
+	readonly type: EntitlementTypes;
+	/**
+	 * The ID of the user at which the entitlement was granted.
+	 */
+	readonly userId: Snowflake | null;
 
-	constructor(id: Snowflake, data: APIEntitlement) {
-		super(id);
+	/**
+	 * Creates a new {@link Entitlement | `Entitlement`} instance from raw
+	 * Discord API data.
+	 *
+	 * @param client - The client that instantiated the entitlement.
+	 * @param data - The raw Discord API entitlement data.
+	 */
+	constructor(client: Client, data: APIEntitlement) {
+		super(client);
 
-		const {
-			application_id,
-			consumed,
-			deleted,
-			ends_at,
-			guild_id,
-			sku_id,
-			starts_at,
-			type,
-			user_id,
-		} = data;
+		const { application_id, guild_id, sku_id, id, type, user_id } = data;
 
 		this.applicationId = application_id;
-		this.consumed = consumed ?? false;
-		this.deleted = deleted;
-		this.endsAt = ends_at ? new Date(ends_at) : null;
 		this.guildId = guild_id ?? null;
+		this.id = id;
 		this.skuId = sku_id;
-		this.startsAt = starts_at ? new Date(starts_at) : null;
 		this.type = type;
 		this.userId = user_id ?? null;
-		this.patch(data);
+		this._patch(data);
 	}
 
 	/**
 	 * @internal
 	 */
-	private patch(data: EntitlementData): void {
-		const {
-			application_id,
-			consumed,
-			deleted,
-			ends_at,
-			guild_id,
-			sku_id,
-			starts_at,
-			type,
-			user_id,
-		} = data;
-
-		if (application_id) {
-			this.applicationId = application_id;
-		}
+	protected _patch(data: EntitlementData): void {
+		const { consumed, deleted, ends_at, starts_at } = data;
 
 		if (consumed) {
 			this.consumed = consumed;
+		} else {
+			this.consumed ??= false;
 		}
 
 		if (deleted) {
 			this.deleted = deleted;
+		} else {
+			this.deleted ??= false;
 		}
 
 		if (ends_at) {
-			this.endsAt = ends_at ? new Date(ends_at) : null;
-		}
-
-		if (guild_id) {
-			this.guildId = guild_id;
-		}
-
-		if (sku_id) {
-			this.skuId = sku_id;
+			this.endsTimestamp = Date.parse(ends_at);
+		} else {
+			this.endsTimestamp ??= null;
 		}
 
 		if (starts_at) {
-			this.startsAt = starts_at ? new Date(starts_at) : null;
+			this.startsTimestamp = Date.parse(starts_at);
+		} else {
+			this.startsTimestamp ??= null;
 		}
+	}
 
-		if (type) {
-			this.type = type;
-		}
+	/**
+	 * The date at which the entitlement is no longer valid.
+	 */
+	get endsAt(): Date | null {
+		const { endsTimestamp } = this;
 
-		if (user_id) {
-			this.userId = user_id;
-		}
+		return endsTimestamp ? new Date(endsTimestamp) : null;
+	}
+
+	/**
+	 * The date at which the entitlement is valid.
+	 */
+	get startsAt(): Date | null {
+		const { startsTimestamp } = this;
+
+		return startsTimestamp ? new Date(startsTimestamp) : null;
+	}
+
+	/**
+	 * Converts the {@link Entitlement | `Entitlement`} instance to a JSON object.
+	 *
+	 * @returns The JSON entitlement data.
+	 */
+	toJSON(): JSONEntitlement {
+		const {
+			applicationId,
+			consumed,
+			deleted,
+			endsAt,
+			endsTimestamp,
+			guildId,
+			id,
+			skuId,
+			startsAt,
+			startsTimestamp,
+			type,
+			userId,
+		} = this;
+
+		return Object.freeze({
+			applicationId,
+			consumed,
+			deleted,
+			endsAt,
+			endsTimestamp,
+			guildId,
+			id,
+			skuId,
+			startsAt,
+			startsTimestamp,
+			type,
+			userId,
+		});
 	}
 }
 
