05dac75cfdf8b19ee04aa75d7904a480e05a45ff
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-25 01:08:28 +0200

    fix: use new base class

--- src/structures/discord/ChatInputCommandInteraction.ts
+++ src/structures/discord/ChatInputCommandInteraction.ts
@@ -1,15 +1,21 @@
-import {
-	type APIApplicationCommandInteraction,
-	ApplicationCommandTypes,
-	type Snowflake,
-} from "#types/index.js";
-import { CommandInteractionBase } from "./base/CommandInteractionBase.js";
+import type { Client } from "#client/Client.js";
+import { type APIApplicationCommandInteraction, ApplicationCommandTypes } from "#types/index.js";
+import { BaseCommandInteraction } from "./base/BaseCommandInteraction.js";
 
 /**
+ * Represents a Discord chat input command interaction.
+ *
  * @public
  */
-export class ChatInputCommandInteraction extends CommandInteractionBase<ApplicationCommandTypes.ChatInput> {
-	constructor(id: Snowflake, data: APIApplicationCommandInteraction) {
-		super(id, data, ApplicationCommandTypes.ChatInput);
+export class ChatInputCommandInteraction extends BaseCommandInteraction<ApplicationCommandTypes.ChatInput> {
+	/**
+	 * Creates a new
+	 * {@link ChatInputCommandInteraction | `ChatInputCommandInteraction`} instance.
+	 *
+	 * @param client - The client that instantiated the interaction.
+	 * @param data - The raw Discord API application command interaction data.
+	 */
+	constructor(client: Client, data: APIApplicationCommandInteraction) {
+		super(client, data, ApplicationCommandTypes.ChatInput);
 	}
 }
