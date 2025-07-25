363b9b1b8c6f670a9d4071b1b6a3fe9cd25d1b73
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-25 02:34:54 +0200

    chore: update guild role hooks

--- src/gateway/hooks/GUILD_ROLE_CREATE.ts
+++ src/gateway/hooks/GUILD_ROLE_CREATE.ts
@@ -1,9 +1,7 @@
 import type { Client } from "#client/Client.js";
 import type { GatewayShard } from "#gateway/structures/GatewayShard.js";
-import { type Guild, Role } from "#structures/index.js";
-import { Uncached } from "#structures/Uncached.js";
+import { Role } from "#structures/index.js";
 import type { GatewayDispatchGuildRoleCreatePayload } from "#types/index.js";
-import type { MaybeUncached } from "#utils/types.js";
 
 export const GUILD_ROLE_CREATE = (
 	client: Client,
@@ -13,34 +11,22 @@ export const GUILD_ROLE_CREATE = (
 	const { events, guilds } = client;
 	const { cache: guildsCache } = guilds;
 
-	const { id: roleId } = roleData;
-	const role = new Role(client, roleData);
-
-	/**
-	 * These variables will be assigned later by their cached instance or an
-	 * `Uncached` instance.
-	 */
-	let guild: MaybeUncached<Guild>;
-
 	const cachedGuild = guildsCache.get(guildId);
 
-	if (cachedGuild) {
-		guild = cachedGuild;
-
-		const { roles } = cachedGuild;
-
-		/**
-		 * biome-ignore lint/complexity/useLiteralKeys: Accessing private
-		 * members from the manager.
-		 */
-		roles["add"](roleId, role);
+	if (!cachedGuild) {
+		return;
 	}
 
-	/**
-	 * Assign an `Uncached` instance value to variables that have not been
-	 * assigned yet.
-	 */
-	guild ??= new Uncached(guildId);
+	const { roles } = cachedGuild;
+	const { id: roleId } = roleData;
 
-	events.emit("guildRoleCreate", role, guild);
+	const role = new Role(client, roleData, cachedGuild);
+
+	/**
+	 * biome-ignore lint/complexity/useLiteralKeys: Accessing private members
+	 * from the manager.
+	 */
+	roles["add"](roleId, role);
+
+	events.emit("guildRoleCreate", role, cachedGuild);
 };
--- src/gateway/hooks/GUILD_ROLE_UPDATE.ts
+++ src/gateway/hooks/GUILD_ROLE_UPDATE.ts
@@ -1,6 +1,6 @@
 import type { Client } from "#client/Client.js";
 import type { GatewayShard } from "#gateway/structures/GatewayShard.js";
-import { type Guild, Role } from "#structures/index.js";
+import { Role } from "#structures/index.js";
 import { Uncached } from "#structures/Uncached.js";
 import type { GatewayDispatchGuildRoleUpdatePayload } from "#types/index.js";
 import type { MaybeUncached } from "#utils/types.js";
@@ -13,44 +13,49 @@ export const GUILD_ROLE_UPDATE = (
 	const { events, guilds } = client;
 	const { cache: guildsCache } = guilds;
 
+	const cachedGuild = guildsCache.get(guildId);
+
+	if (!cachedGuild) {
+		return;
+	}
+
+	const { roles } = cachedGuild;
 	const { id: roleId } = roleData;
-	const role = new Role(client, roleData);
+
+	const role = new Role(client, roleData, cachedGuild);
+
+	/**
+	 * biome-ignore lint/complexity/useLiteralKeys: Accessing private
+	 * members from the manager.
+	 */
+	roles["patch"](roleId, roleData);
 
 	/**
 	 * These variables will be assigned later by their cached instance or an
 	 * `Uncached` instance.
 	 */
-	let guild: MaybeUncached<Guild>;
 	let oldRole: MaybeUncached<Role>;
 
-	const cachedGuild = guildsCache.get(guildId);
+	const { cache: rolesCache } = roles;
+	const cachedRole = rolesCache.get(roleId);
 
-	if (cachedGuild) {
-		guild = cachedGuild;
-
-		const { roles } = cachedGuild;
-		const { cache: rolesCache } = roles;
-		const cachedRole = rolesCache.get(roleId);
-
-		if (!cachedRole) {
-			return;
-		}
-
-		oldRole = structuredClone(cachedRole);
-
-		/**
-		 * biome-ignore lint/complexity/useLiteralKeys: Accessing private
-		 * members from the manager.
-		 */
-		roles["patch"](roleId, roleData);
+	if (!cachedRole) {
+		return;
 	}
+
+	oldRole = structuredClone(cachedRole);
+
+	/**
+	 * biome-ignore lint/complexity/useLiteralKeys: Accessing private
+	 * members from the manager.
+	 */
+	roles["patch"](roleId, roleData);
 
 	/**
 	 * Assign an `Uncached` instance value to variables that have not been
 	 * assigned yet.
 	 */
-	guild ??= new Uncached(guildId);
 	oldRole ??= new Uncached(roleId);
 
-	events.emit("guildRoleUpdate", role, oldRole, guild);
+	events.emit("guildRoleUpdate", role, oldRole, cachedGuild);
 };
