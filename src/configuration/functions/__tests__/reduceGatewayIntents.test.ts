44991fcacb0ae0c43ed528a3bfd2f61c3658cdd5
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-28 01:12:16 +0200

    test: return the given intents when they are already a number

--- src/configuration/functions/__tests__/reduceGatewayIntents.test.ts
+++ src/configuration/functions/__tests__/reduceGatewayIntents.test.ts
@@ -3,6 +3,12 @@ import { GatewayIntents } from "#types/index.js";
 import { reduceGatewayIntents } from "../reduceGatewayIntents.js";
 
 describe("Function: reduceGatewayIntents", () => {
+	it("Should return the given intents as a number if they are already a number.", () => {
+		const GatewayIntentsNumber = GatewayIntents.Guilds | GatewayIntents.GuildMessages;
+
+		expect(reduceGatewayIntents(GatewayIntentsNumber)).toEqual(GatewayIntentsNumber);
+	});
+
 	it("Should return the reduced intents as a number.", () => {
 		const GatewayIntentsArray = [
 			GatewayIntents.Guilds,
