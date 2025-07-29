7caa7ba319ca635425853870b96e775423fddb4e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-29 03:06:16 +0200

    rest: include `GuildsAPI` in `APIManager`

--- src/rest/structures/APIManager.ts
+++ src/rest/structures/APIManager.ts
@@ -1,8 +1,9 @@
 import { Mixin } from "ts-mixer";
 import { ChannelsAPI } from "#rest/api/ChannelsAPI.js";
+import { GuildsAPI } from "#rest/api/GuildsAPI.js";
 import { InteractionsAPI } from "#rest/api/InteractionsAPI.js";
 
 /**
  * @public
  */
-export class APIManager extends Mixin(ChannelsAPI, InteractionsAPI) {}
+export class APIManager extends Mixin(ChannelsAPI, GuildsAPI, InteractionsAPI) {}
