792efb0e3bc40769e53daded6f1af81b90df5d17
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-07-29 03:03:28 +0200

    chore: update enums for commit lint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,25 @@ import type { UserConfig } from "@commitlint/types";
 
 const configuration: UserConfig = {
 	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			[
+				"api-docs",
+				"chore",
+				"ci",
+				"docs",
+				"feat",
+				"gateway",
+				"refactor",
+				"rest",
+				"style",
+				"test",
+				"types",
+			],
+		],
+	},
 };
 
 export default configuration;
