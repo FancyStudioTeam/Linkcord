27c8b27a0d5c59fdf1a6935ce7d9cfab4a915149
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 00:07:22 +0200

    chore: update `BaseClient`

--- src/client/BaseClient.ts
+++ src/client/BaseClient.ts
@@ -1,35 +1,63 @@
 import { join } from "node:path";
-import { LinkcordConfiguration } from "../configuration/structures/LinkcordConfiguration.js";
-import { CommandsLoader } from "../handlers/commands/loaders/CommandsLoader.js";
+import { ConfigurationUtils } from "#configuration/utils/ConfigurationUtils.js";
 import { EventsLoader } from "../handlers/events/loaders/EventsLoader.js";
 import { EventsManager } from "./managers/EventsManager.js";
 
 /**
+ * Creates a path to a folder.
+ * @param root - The directory root where the folder should be located.
+ * @param folderName - The name of the folder.
+ * @returns The path to the folder.
+ * @internal
+ */
+function createFolderPath(root: string, folderName: string): string {
+	return join(process.cwd(), root, folderName);
+}
+
+/**
+ * Represents a base client.
  * @public
  */
 export class BaseClient {
+	/**
+	 * The events manager of the client.
+	 */
 	readonly events = new EventsManager();
 
+	/**
+	 * The intents of the client.
+	 */
+	get intents(): Readonly<number> {
+		return ConfigurationUtils.getIntents();
+	}
+
+	/**
+	 * The token of the client.
+	 */
+	get token(): Readonly<string> {
+		return ConfigurationUtils.getToken();
+	}
+
+	/**
+	 * Initializes the base client.
+	 */
 	async init(): Promise<void> {
-		const { intents, locations, token } = LinkcordConfiguration.getOptions();
+		await Promise.all([this.register()]);
+	}
 
-		if (!(token && intents)) {
-			throw new TypeError("Token or intents are missing from the configuration file.");
-		}
+	/**
+	 * Registers the client commands and events.
+	 */
+	async register(): Promise<void> {
+		const locations = ConfigurationUtils.getLocations();
+		const { events, root } = locations;
 
-		if (locations) {
-			const { commands, events, root } = locations;
-			const createPath = (folder: string) => join(process.cwd(), root, folder);
-			const locationPromises: Promise<unknown>[] = [
-				commands
-					? CommandsLoader.registerCommandsToClient(createPath(commands), this)
-					: Promise.resolve(false),
-				events
-					? EventsLoader.registerEventsToClient(createPath(events), this)
-					: Promise.resolve(false),
-			];
+		const eventsFolderPath = createFolderPath(root, events);
 
-			await Promise.all(locationPromises);
-		}
+		const locationPromises: Promise<unknown>[] = [
+			EventsLoader.registerEvents(eventsFolderPath, this),
+		];
+
+		await Promise.all(locationPromises);
 	}
 }
