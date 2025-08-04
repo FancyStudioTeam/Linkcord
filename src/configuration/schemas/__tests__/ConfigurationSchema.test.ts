8578203afa0d49028f6e4c6380a0248fba857238
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 06:24:31 +0200

    test: delete expected error message

---
+++ src/configuration/schemas/__tests__/ConfigurationSchema.test.ts
@@ -0,0 +1,21 @@
+import { parse } from "valibot";
+import { describe, expect, it } from "vitest";
+import { GatewayIntents } from "#types/index.js";
+import { ConfigurationSchema } from "../ConfigurationSchema.js";
+
+describe("Schema: ConfigurationSchema", () => {
+	it("Should throw an error if the input is not an object.", () => {
+		const InvalidInputType = null;
+
+		expect(() => parse(ConfigurationSchema, InvalidInputType)).toThrowError();
+	});
+
+	it("Should throw an error if one of the required fields is missing.", () => {
+		const InvalidInput = {
+			intents: [GatewayIntents.Guilds],
+			token: "ANY_DISCORD_BOT_TOKEN",
+		};
+
+		expect(() => parse(ConfigurationSchema, InvalidInput)).toThrowError();
+	});
+});
