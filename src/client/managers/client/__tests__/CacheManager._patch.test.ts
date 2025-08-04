2c565da17d741506387ce837772997ab907dedaa
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 04:43:56 +0200

    test: `CacheManager._patch`

---
+++ src/client/managers/client/__tests__/CacheManager._patch.test.ts
@@ -0,0 +1,48 @@
+// biome-ignore-all lint/complexity/useLiteralKeys: Private methods.
+
+import { describe, expect, it } from "vitest";
+import { CacheManager as CacheManagerClass } from "../CacheManager.js";
+
+class UserClass {
+	name: string;
+
+	constructor(name: string) {
+		this.name = name;
+	}
+
+	_patch(data: Partial<UserClassData> = {}): void {
+		const { name } = data;
+
+		if (name) {
+			this.name = name;
+		}
+	}
+}
+
+describe("Method: CacheManager._patch", () =>
+	it("Should patch the user with the given data.", () => {
+		const User = new UserClass("User 1");
+
+		const CacheManagerIterable: readonly [string, UserClass][] = [["user_1", User]];
+		// @ts-expect-error
+		const CacheManager = new CacheManagerClass(Infinity, CacheManagerIterable);
+
+		CacheManager["_patch"]("user_1", {
+			name: "User 1 (Patched)",
+		});
+
+		const { cache } = CacheManager;
+
+		const CachedValueAfterPatch = cache.get("user_1") as unknown as UserClass;
+		const { name: CachedValueAfterPatchName } = CachedValueAfterPatch;
+
+		expect(CachedValueAfterPatch).toBeInstanceOf(UserClass);
+		expect(CachedValueAfterPatchName).toBe("User 1 (Patched)");
+	}));
+
+/**
+ * @internal
+ */
+interface UserClassData {
+	name: string;
+}
