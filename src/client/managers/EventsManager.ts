6984764ba21ee8128aeb918b8023de0d5610ff90
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 02:50:03 +0200

    docs: update `EventsManager` comments

--- src/client/managers/EventsManager.ts
+++ src/client/managers/EventsManager.ts
@@ -1,14 +1,45 @@
 import type { ClientEventsMap, ClientEventsString } from "../ClientEvents.js";
 
 /**
+ * The events manager for the client.
  * @public
  */
 export class EventsManager {
 	/**
-	 * biome-ignore lint/complexity/noBannedTypes: (x)
+	 * The registered listeners of the events manager.
 	 */
+	// biome-ignore lint/complexity/noBannedTypes: Allow any function.
 	readonly listeners = new Map<ClientEventsString, Function[]>();
 
+	/**
+	 * Registers a listener for an event.
+	 * @param name - The name of the event to register.
+	 * @param listener - The listener to register.
+	 */
+	addListener<Event extends keyof ClientEventsMap>(
+		name: Event,
+		listener: (...data: ClientEventsMap[Event]) => unknown,
+	): void {
+		const { listeners } = this;
+		const existing = listeners.get(name);
+
+		// Check if there is no existing listeners for the event or if the
+		// existing listeners are not an array.
+		// Equivalent to `if (!existing || !Array.isArray(existing))`.
+		if (!(existing && Array.isArray(existing))) {
+			// Use `void` to avoid TypeScript complaining about the return
+			// type but still executing the following code.
+			return void listeners.set(name, [listener]);
+		}
+
+		existing.push(listener);
+	}
+
+	/**
+	 * Emits an event with the given event name and data.
+	 * @param name - The name of the event to emit.
+	 * @param data - The data to emit with the event.
+	 */
 	emit<Event extends ClientEventsString>(name: Event, ...data: ClientEventsMap[Event]): void {
 		const { listeners } = this;
 		const existing = listeners.get(name) ?? [];
@@ -16,21 +47,5 @@ export class EventsManager {
 		for (const listener of existing) {
 			listener(...data);
 		}
-	}
-
-	register<Event extends keyof ClientEventsMap>(
-		name: Event,
-		listener: (...data: ClientEventsMap[Event]) => unknown,
-	): void {
-		const { listeners } = this;
-		const existing = listeners.get(name);
-
-		if (!(existing && Array.isArray(existing))) {
-			listeners.set(name, [listener]);
-
-			return;
-		}
-
-		existing.push(listener);
 	}
 }
