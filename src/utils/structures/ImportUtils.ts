4248d9f2049bbf3a965c0354ffa4dcf155675284
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 02:34:55 +0200

    feat: `ImportUtils.import`

--- src/utils/structures/ImportUtils.ts
+++ src/utils/structures/ImportUtils.ts
@@ -1,7 +1,28 @@
 import { join, resolve } from "node:path";
 import { pathToFileURL } from "node:url";
+import { MISSING_DEFAULT_EXPORT_FROM_FILE_PATH } from "#errors/messages.js";
 
 const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
+
+/**
+ * Imports a file and returns the imported data.
+ * @param path - The path of the file to import.
+ * @param requiredDefaultExport - Whether the file must include a default
+ * export.
+ * @returns The imported data.
+ */
+async function _import<ImportData>(
+	path: string,
+	requiredDefaultExport?: boolean,
+): Promise<ImportData> {
+	const data = await import(path);
+
+	if (requiredDefaultExport && !("default" in data)) {
+		throw new Error(MISSING_DEFAULT_EXPORT_FROM_FILE_PATH(path));
+	}
+
+	return data;
+}
 
 /**
  * Resolves a path to a file for compatibility with both CommonJS and ESM.
@@ -26,5 +47,6 @@ function resolvePath(...fragments: string[]): string {
  * @internal
  */
 export const ImportUtils = {
+	import: _import,
 	resolvePath,
 };
