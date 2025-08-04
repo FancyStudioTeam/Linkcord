d9c93d45972ec73d5c3b2ecb43c03adab3b833e4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-04 05:30:32 +0200

    fix: use correct file name

--- src/configuration/functions/defineConfig.ts
+++ src/configuration/functions/defineConfig.ts
@@ -3,7 +3,7 @@ import {
 	type ConfigurationLocationsSchema,
 	ConfigurationSchema,
 } from "#configuration/schemas/ConfigurationSchema.js";
-import { INVALID_DEFINE_CONFIG_INPUT } from "#errors/messages/configuration.js";
+import { INVALID_DEFINE_CONFIG_INPUT } from "#errors/messages/Configuration.js";
 import type { GatewayIntents } from "#types/index.js";
 
 /**
