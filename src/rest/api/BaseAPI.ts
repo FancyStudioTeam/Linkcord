9f855ec9a85141983877bf7a945e203771240bfe
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-07 18:58:05 +0200

    chore: delete base folder in api

--- src/rest/api/BaseAPI.ts
+++ src/rest/api/BaseAPI.ts
@@ -1,4 +1,4 @@
-import type { Client } from "#client/Client.js";
+import type { Client } from "#client/index.js";
 import type {
 	RESTDeleteOptions,
 	RESTGetOptions,
@@ -10,7 +10,7 @@ import type {
 
 /**
  * Represents a base class for all API classes.
- *
+ * @group REST/API
  * @public
  */
 export class BaseAPI {
@@ -21,12 +21,85 @@ export class BaseAPI {
 
 	/**
 	 * Creates a new {@link BaseAPI | `BaseAPI`} instance.
-	 *
-	 * @param rest - The REST manager to perform requests within the API
-	 * classes.
+	 * @param rest - The REST manager that instantiated the API.
 	 */
 	constructor(rest: RESTManager) {
 		this.rest = rest;
+	}
+
+	/**
+	 * Performs a `DELETE` request to the Discord API.
+	 * @param endpoint - The endpoint where the request will be made.
+	 * @param options - The options to use when performing the request.
+	 * @internal
+	 */
+	protected __delete__<Result, QueryStringParams = never>(
+		endpoint: string,
+		options?: RESTDeleteOptions<QueryStringParams>,
+	): Promise<Result> {
+		const { rest } = this;
+
+		return rest.delete<Result, QueryStringParams>(endpoint, options);
+	}
+
+	/**
+	 * Performs a `GET` request to the Discord API.
+	 * @param endpoint - The endpoint where the request will be made.
+	 * @param options - The options to use when performing the request.
+	 * @internal
+	 */
+	protected async __get__<Result, QueryStringParams = never>(
+		endpoint: string,
+		options?: RESTGetOptions<QueryStringParams>,
+	): Promise<Result> {
+		const { rest } = this;
+
+		return await rest.get<Result, QueryStringParams>(endpoint, options);
+	}
+
+	/**
+	 * Performs a `PATCH` request to the Discord API.
+	 * @param endpoint - The endpoint where the request will be made.
+	 * @param options - The options to use when performing the request.
+	 * @internal
+	 */
+	protected async __patch__<Result, JSONParams = never, QueryStringParams = never>(
+		endpoint: string,
+		options?: RESTPatchOptions<JSONParams, QueryStringParams>,
+	): Promise<Result> {
+		const { rest } = this;
+
+		return await rest.patch<Result, JSONParams, QueryStringParams>(endpoint, options);
+	}
+
+	/**
+	 * Performs a `POST` request to the Discord API.
+	 * @param endpoint - The endpoint where the request will be made.
+	 * @param options - The options to use when performing the request.
+	 * @internal
+	 */
+	protected async __post__<Result, JSONParams = never, QueryStringParams = never>(
+		endpoint: string,
+		options?: RESTPostOptions<JSONParams, QueryStringParams>,
+	): Promise<Result> {
+		const { rest } = this;
+
+		return await rest.post<Result, JSONParams, QueryStringParams>(endpoint, options);
+	}
+
+	/**
+	 * Performs a `PUT` request to the Discord API.
+	 * @param endpoint - The endpoint where the request will be made.
+	 * @param options - The options to use when performing the request.
+	 * @internal
+	 */
+	protected async __put__<Result, JSONParams = never, QueryStringParams = never>(
+		endpoint: string,
+		options?: RESTPutOptions<JSONParams, QueryStringParams>,
+	): Promise<Result> {
+		const { rest } = this;
+
+		return await rest.put<Result, JSONParams, QueryStringParams>(endpoint, options);
 	}
 
 	/**
@@ -37,65 +110,5 @@ export class BaseAPI {
 		const { client } = rest;
 
 		return client;
-	}
-
-	/**
-	 * @internal
-	 */
-	protected delete<Result, QueryStringParams = never>(
-		endpoint: string,
-		options?: RESTDeleteOptions<QueryStringParams>,
-	): Promise<Result> {
-		const { rest } = this;
-
-		return rest.delete<Result, QueryStringParams>(endpoint, options);
-	}
-
-	/**
-	 * @internal
-	 */
-	protected async get<Result, QueryStringParams = never>(
-		endpoint: string,
-		options?: RESTGetOptions<QueryStringParams>,
-	): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.get<Result, QueryStringParams>(endpoint, options);
-	}
-
-	/**
-	 * @internal
-	 */
-	protected async patch<Result, JSONParams = never, QueryStringParams = never>(
-		endpoint: string,
-		options?: RESTPatchOptions<JSONParams, QueryStringParams>,
-	): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.patch<Result, JSONParams, QueryStringParams>(endpoint, options);
-	}
-
-	/**
-	 * @internal
-	 */
-	protected async post<Result, JSONParams = never, QueryStringParams = never>(
-		endpoint: string,
-		options?: RESTPostOptions<JSONParams, QueryStringParams>,
-	): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.post<Result, JSONParams, QueryStringParams>(endpoint, options);
-	}
-
-	/**
-	 * @internal
-	 */
-	protected async put<Result, JSONParams = never, QueryStringParams = never>(
-		endpoint: string,
-		options?: RESTPutOptions<JSONParams, QueryStringParams>,
-	): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.put<Result, JSONParams, QueryStringParams>(endpoint, options);
 	}
 }
