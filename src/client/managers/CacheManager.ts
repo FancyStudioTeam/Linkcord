8fd09c1822799d54264a4faa98fb9950b4f69a5e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-12 04:54:35 +0200

    client: update `CacheManager`

--- src/client/managers/CacheManager.ts
+++ src/client/managers/CacheManager.ts
@@ -4,6 +4,7 @@
  * destructuring them from "this".
  */
 
+import type { Iterable } from "#client/types/index.js";
 import type { Base } from "#structures/index.js";
 import type { Snowflake } from "#types/index.js";
 
@@ -13,43 +14,41 @@ import type { Snowflake } from "#types/index.js";
  * @public
  */
 export class CacheManager<Key extends string, Value extends Base> {
-	/**
-	 * The maximum number of cached values in the cache manager.
-	 */
+	/** The maximum number of cached values in the cache manager. */
 	private readonly __limit__: number;
-
-	/**
-	 * The cached values of the cache manager.
-	 */
+	/** The map where the cached values are stored. */
 	readonly cache: Map<Snowflake, Value>;
 
 	/**
 	 * Creates a new {@link CacheManager | `CacheManager`} instance.
 	 * @param limit - The maximum number of cached values in the cache
 	 * 	manager.
-	 * @param iterable - The initial iterable of key-value pairs for the
-	 * 	cache manager.
+	 * @param initialCachedValues - The initial values to cache when
+	 * 	instantiating the cache manager.
 	 */
-	constructor(limit: number = Infinity, iterable: Iterable<Key, Value> = []) {
+	constructor(limit: number = Infinity, initialCachedValues: Iterable<Key, Value> = []) {
 		this.__limit__ = limit;
-		this.cache = new Map(iterable);
+		this.cache = new Map(initialCachedValues);
 	}
 
 	/**
-	 * Adds a value to the cache manager.
+	 * Adds a value to the cache.
 	 * @param key - The key of the value to add.
 	 * @param value - The value to add.
-	 * @internal
 	 */
-	protected __add__(key: Snowflake, value: Value): void {
-		const { __limit__, cache } = this;
+	private __add__(key: Snowflake, value: Value): void {
+		const { __limit__: limit, cache } = this;
 		const { size: cacheSize } = cache;
 
-		if (cacheSize >= __limit__) {
-			return void process.emitWarning(
-				`The entry "${key}" has not been added due to the cache limit. (${cacheSize}/${__limit__} allowed entries)`,
-				"[CacheManager]",
-			);
+		// If the cache is full, remove the oldest value and add the new one.
+		// TODO: Test if oldest values are removed when the cache is full.
+		if (cacheSize >= limit) {
+			const entries = cache.entries();
+			const [firstCachedValueKey] = entries.next().value ?? [];
+
+			if (firstCachedValueKey) {
+				cache.delete(firstCachedValueKey);
+			}
 		}
 
 		cache.set(key, value);
@@ -57,33 +56,27 @@ export class CacheManager<Key extends string, Value extends Base> {
 
 	/**
 	 * Patches the data of a cached value with the given data, if exists.
-	 * @param key - The key of the value to patch.
+	 * @param key - The key of the cached value to patch.
 	 * @param data - The data to use to patch the cached value.
-	 * @internal
 	 */
-	protected __patch__(key: Snowflake, data: unknown): void {
+	private __patch__(key: Snowflake, data: unknown): void {
 		const { cache } = this;
 		const existing = cache.get(key);
 
-		// biome-ignore lint/complexity/useLiteralKeys: Private method.
-		existing?.["_patch"](data);
+		if (existing) {
+			// biome-ignore lint/complexity/useLiteralKeys: Private method.
+			existing["_patch"](data);
+		}
 	}
 
 	/**
-	 * Removes a value from the cache manager, if exists.
+	 * Removes a value from the cache, if exists.
 	 * @param key - The key of the value to remove.
 	 * @returns Whether the value was removed.
-	 * @internal
 	 */
-	protected __remove__(key: Snowflake): boolean {
+	private __remove__(key: Snowflake): boolean {
 		const { cache } = this;
 
 		return cache.delete(key);
 	}
 }
-
-/**
- * Represents an iterable of key-value pairs for the cache manager.
- * @internal
- */
-type Iterable<Key extends string, Value extends Base> = readonly [Key, Value][];
