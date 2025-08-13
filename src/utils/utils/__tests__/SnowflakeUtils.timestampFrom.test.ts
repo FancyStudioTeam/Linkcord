f5933c9c8a65303da7488f159532dbd323030f3e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-13 22:04:11 +0200

    chore: update `SnowflakeUtils` test

--- src/utils/utils/__tests__/SnowflakeUtils.timestampFrom.test.ts
+++ src/utils/utils/__tests__/SnowflakeUtils.timestampFrom.test.ts
@@ -7,22 +7,20 @@ import { describe, expect, it } from "vitest";
 import { SnowflakeUtils } from "../SnowflakeUtils.js";
 
 describe("Method: SnowflakeUtils.timestampFrom", () => {
-	describe("Method: timestampFrom", () => {
-		it("Should return the timestamp of the snowflake.", () => {
-			const UserIDString = "1346162378575970326";
-			const UserIDBigInt = BigInt(UserIDString);
-			const ExpectedTimestamp = 1741020521540;
+	it("Should return the timestamp of the snowflake.", () => {
+		const UserIDString = "1346162378575970326";
+		const UserIDBigInt = BigInt(UserIDString);
+		const ExpectedResult = 1741020521540;
 
-			expect(SnowflakeUtils.timestampFrom(UserIDString)).toBe(ExpectedTimestamp);
-			expect(SnowflakeUtils.timestampFrom(UserIDBigInt)).toBe(ExpectedTimestamp);
-		});
+		expect(SnowflakeUtils.timestampFrom(UserIDString)).toBe(ExpectedResult);
+		expect(SnowflakeUtils.timestampFrom(UserIDBigInt)).toBe(ExpectedResult);
+	});
 
-		it("Should throw an error when the snowflake is not valid number or string.", () => {
-			// @ts-expect-error
-			expect(() => SnowflakeUtils.timestampFrom(null)).toThrowError(TypeError);
-			expect(() => SnowflakeUtils.timestampFrom("NOT_A_VALID_SNOWFLAKE_STRING")).toThrowError(
-				TypeError,
-			);
-		});
+	it("Should throw an error when the snowflake is not valid number or string.", () => {
+		// @ts-expect-error
+		expect(() => SnowflakeUtils.timestampFrom(null)).toThrowError(TypeError);
+		expect(() => SnowflakeUtils.timestampFrom("NOT_A_VALID_SNOWFLAKE_STRING")).toThrowError(
+			TypeError,
+		);
 	});
 });
