f18439edccab467eb2d31742fe13a628350a4a28
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-13 06:29:02 +0200

    feat: handler types

---
+++ src/handlers/types/commands/structures/UserContextCommand.ts
@@ -0,0 +1,8 @@
+import type { UserContextCommand } from "#handlers/commands/index.js";
+import type { Newable } from "#utils/types.js";
+
+/**
+ * Represents a {@link UserContextCommand | `UserContextCommand`} instance.
+ * @public
+ */
+export type UserContextCommandInstance = Newable<UserContextCommand>;
