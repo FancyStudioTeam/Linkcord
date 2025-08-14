807fb22b6aa1ae651b767165e414d6a86dd767f5
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-14 05:43:44 +0200

    feat: `EmbedFieldJSX` component

---
+++ src/jsx/components/messages/EmbedFieldJSX.ts
@@ -0,0 +1,33 @@
+import { EmbedFieldBuilder } from "#builders/index.js";
+import type { EmbedFieldJSXProperties } from "#jsx/types/index.js";
+
+/**
+ * Creates an {@link EmbedFieldBuilder | `EmbedFieldBuilder`} instance using
+ * JSX components.
+ * @param properties - The properties of the component.
+ * @returns The created {@link EmbedFieldBuilder | `EmbedFieldBuilder`}
+ * instance.
+ * @public
+ */
+export function EmbedFieldJSX(properties: EmbedFieldJSXProperties): EmbedFieldBuilder {
+	const embedField = new EmbedFieldBuilder();
+	const { children, inline, name, value } = properties;
+
+	if (inline) {
+		embedField.setInline(inline);
+	}
+
+	if (name) {
+		embedField.setName(name);
+	}
+
+	if (value) {
+		embedField.setValue(value);
+	}
+
+	if (children && typeof children === "string") {
+		embedField.setValue(children);
+	}
+
+	return embedField;
+}
