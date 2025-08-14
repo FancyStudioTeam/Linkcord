b9918d262d77c21617a504e9ea1475cfb90f733b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-14 16:59:06 +0200

    feat(types): channel types

--- src/types/discord/payloads/Channels.ts
+++ src/types/discord/payloads/Channels.ts
@@ -219,26 +219,6 @@ export enum ChannelFlags {
 
 /**
  * @public
- * @see https://discord.com/developers/docs/resources/channel#channel-object-channel-types
- */
-export enum ChannelTypes {
-	AnnouncementThread = 10,
-	DirectMessage = 1,
-	GroupDM = 3,
-	GuildAnnouncement = 5,
-	GuildCategory = 4,
-	GuildDirectory = 14,
-	GuildForum = 15,
-	GuildMedia = 16,
-	GuildStageVoice = 13,
-	GuildText = 0,
-	GuildVoice = 2,
-	PrivateThread = 12,
-	PublicThread = 11,
-}
-
-/**
- * @public
  * @see https://discord.com/developers/docs/resources/channel#channel-object-forum-layout-types
  */
 export enum ForumLayoutTypes {
