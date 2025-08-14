07b134cdb3413a04091a7bc61b9b291a3595e4ae
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-14 04:02:27 +0200

    feat: add jsx types

---
+++ src/jsx/types/components/messages/EmbedJSX.ts
@@ -0,0 +1,42 @@
+import type {
+	AllowedEmbedAuthor,
+	AllowedEmbedColor,
+	AllowedEmbedField,
+	AllowedEmbedFooter,
+	AllowedEmbedImageURL,
+	AllowedEmbedThumbnailURL,
+	AllowedEmbedTimestamp,
+	AllowedEmbedURL,
+} from "#builders/index.js";
+import { EmbedJSX } from "#jsx/components/EmbedJSX.js";
+import type { ComponentProperties } from "#jsx/jsx-runtime";
+
+/**
+ * Represents the properties of the {@link EmbedJSX | `EmbedJSX`} component.
+ * @public
+ */
+export type EmbedJSXProperties = ComponentProperties<
+	{
+		/** The author of the embed. */
+		author?: AllowedEmbedAuthor;
+		/** The color of the embed. */
+		color?: AllowedEmbedColor;
+		/** The description of the embed. */
+		description?: string;
+		/** The fields of the embed. */
+		fields?: AllowedEmbedField[];
+		/** The footer of the embed. */
+		footer?: AllowedEmbedFooter;
+		/** The image of the embed. */
+		image?: AllowedEmbedImageURL;
+		/** The thumbnail of the embed. */
+		thumbnail?: AllowedEmbedThumbnailURL;
+		/** The timestamp of the embed. */
+		timestamp?: AllowedEmbedTimestamp;
+		/** The title of the embed. */
+		title?: string;
+		/** The URL of the embed. */
+		url?: AllowedEmbedURL;
+	},
+	unknown
+>;
