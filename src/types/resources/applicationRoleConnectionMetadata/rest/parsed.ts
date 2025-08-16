3208c1b3122d9db3997de78648ba4c276a453f92
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-16 23:16:16 +0200

    feat(types): application role connections

---
+++ src/types/resources/applicationRoleConnectionMetadata/rest/parsed.ts
@@ -0,0 +1,7 @@
+import type { ApplicationRoleConnectionMetadata } from "../structures/parsed.js";
+
+/**
+ * The options to use when updating an application role connection metadata.
+ * @see https://discord.com/developers/docs/resources/application-role-connection-metadata#update-application-role-connection-metadata-records
+ */
+export type UpdateApplicationRoleConnectionMetadata = ApplicationRoleConnectionMetadata[];
--- src/types/resources/index.ts
+++ src/types/resources/index.ts
@@ -2,5 +2,6 @@ export * from "./applicationCommands/index.js";
 export * from "./applications/index.js";
 export * from "./channels/index.js";
 export * from "./gateway/index.js";
+export * from "./guilds/index.js";
 export * from "./users/index.js";
 export * from "./voice/index.js";
