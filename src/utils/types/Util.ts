859aeadcc7d1ab04716a002581e16a71bd47da96
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-26 04:46:25 +0200

    feat(utils): `Brand`

--- src/utils/types/Util.ts
+++ src/utils/types/Util.ts
@@ -1,14 +1,15 @@
-/**
- * Represents the properties of an instance that can be serialized into a JSON object.
- * @public
- */
-export type JSONProperties<Instance extends Newable> = Readonly<
+declare const brand: unique symbol;
+
+/** Represents a branded type. */
+export type Brand<Type, Brand extends string> = Type & {
+	readonly [brand]: Brand;
+};
+
+/** Represents the properties of an instance that can be serialized into a JSON object. */
+export type JSONProperties<Instance extends Newable<unknown>> = Readonly<
 	Omit<
 		{
-			/*
-			 * biome-ignore lint/complexity/noBannedTypes: Exlude any method
-			 * from the instance.
-			 */
+			// biome-ignore lint/complexity/noBannedTypes: Exlude any method from the instance.
 			[Key in keyof InstanceType<Instance> as InstanceType<Instance>[Key] extends Function
 				? never
 				: Key]: InstanceType<Instance>[Key];
@@ -17,21 +18,13 @@ export type JSONProperties<Instance extends Newable> = Readonly<
 	>
 >;
 
-/**
- * Represents a conditional type that returns the `TrueResult` if the `Condition` is `true`.
- * Otherwise, it returns the `FalseResult`.
- * If `Condition` does not extend either `true` or `false`, it will return `never`.
- * @public
- */
+/** Represents a conditional type that returns the `TrueResult` if the `Condition` is `true`, or the `FalseResult` if the condition is `false`. */
 export type If<Condition, TrueResult, FalseResult = null> = Condition extends true
 	? TrueResult
 	: Condition extends false
 		? FalseResult
 		: never;
 
-/**
- * Represents a class that can be instantiated.
- * @public
- */
+/** Represents a class that can be instantiated. */
 // biome-ignore lint/suspicious/noExplicitAny: Expect anything.
-export type Newable<Class = unknown> = new (...args: any[]) => Class;
+export type Newable<Class> = new (...args: any[]) => Class;
