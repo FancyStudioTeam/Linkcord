caabb6f521f684feb4dc71d736b0def7defeab4c
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-26 20:43:41 +0200

    chore: delete `errors` folder

--- src/errors/messages/General.ts
+++
@@ -1,9 +0,0 @@
-//  biome-ignore-all lint/style/useNamingConvention: Upper snake case.
-
-/**
- * Shows a message indicating that the executed method is not available.
- * @returns The message indicating that the executed method is not available.
- */
-export function METHOD_NOT_IMPLEMENTED(): string {
-	return "This method has not been implemented yet or is not available.";
-}
