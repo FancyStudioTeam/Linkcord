3bdd0a8c58575baba03ff46ee3a8267f3b7222f1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-08-26 05:44:39 +0200

    builders: update `AllowedEmoji`

--- src/builders/types/mixins/EmojiMixin.ts
+++ src/builders/types/mixins/EmojiMixin.ts
@@ -1,4 +1,8 @@
-import type { PartialEmoji } from "#types/index.js";
+import type { PartialEmoji, Snowflake } from "#types/index.js";
 
-/** Represents an emoji that can be set to a component builder. */
-export type AllowedEmoji = PartialEmoji | string;
+/** Represents an emoji that can be set to a component. */
+export type AllowedEmoji =
+	| PartialEmoji
+	| string
+	| `<a:${string}:${Snowflake}>`
+	| `<:${string}:${Snowflake}>`;
