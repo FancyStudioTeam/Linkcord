290d4b88c28615e22df7aaae2f0a58d52a713d8b
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-01 22:30:53 +0200

    chore: delete commands temporally

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,3 @@
-export * from "./commands/index.js";
 export * from "./decorators/index.js";
 export * from "./events/index.js";
 export * from "./types/index.js";
--- src/handlers/types/index.ts
+++ src/handlers/types/index.ts
@@ -1,3 +1,2 @@
-export * from "./commands/index.js";
 export * from "./decorators/index.js";
 export * from "./events/index.js";
