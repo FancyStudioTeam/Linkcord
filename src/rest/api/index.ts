8595107c32f8a12149e8c5b1f0c9d148f576db9e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-01 20:41:45 +0200

    rest: delete `InteractionsAPI`

--- src/rest/api/index.ts
+++ src/rest/api/index.ts
@@ -1,3 +1,2 @@
 export * from "./BaseAPI.js";
 export * from "./GatewayAPI.js";
-export * from "./InteractionsAPI.js";
--- src/rest/structures/APIManager.ts
+++ src/rest/structures/APIManager.ts
@@ -1,7 +1,6 @@
 import { Mixin } from "ts-mixer";
 import { GatewayAPI } from "#rest/api/GatewayAPI.js";
-import { InteractionsAPI } from "#rest/api/InteractionsAPI.js";
 import { MiscellaneousAPI } from "#rest/api/MiscellaneousAPI.js";
 
 /** The API manager to perform requests within the Discord API. */
-export class APIManager extends Mixin(GatewayAPI, InteractionsAPI, MiscellaneousAPI) {}
+export class APIManager extends Mixin(GatewayAPI, MiscellaneousAPI) {}
