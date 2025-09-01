b688f752c7cc253a6863e6240f166e5caac2568f
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-01 16:16:38 +0200

    builders: export `SeparatorBuilder`

--- src/builders/structures/messages/index.ts
+++ src/builders/structures/messages/index.ts
@@ -2,3 +2,4 @@ export * from "./EmbedAuthorBuilder.js";
 export * from "./EmbedBuilder.js";
 export * from "./EmbedFieldBuilder.js";
 export * from "./EmbedFooterBuilder.js";
+export * from "./SeparatorBuilder.js";
