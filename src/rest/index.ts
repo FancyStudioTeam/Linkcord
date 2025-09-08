2282577d5e67f89e945a34023f5868511c01281d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-08 21:31:16 +0200

    fix: include all exports

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -1,4 +1,4 @@
 export * from "./api/index.js";
 export * from "./endpoints/index.js";
-export * from "./structures/RESTManager.js";
+export * from "./structures/index.js";
 export * from "./utils/index.js";
