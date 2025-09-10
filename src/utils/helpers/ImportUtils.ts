8a2e93886ccb12ee4324cce8dd5e2cbb02ec6bf3
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-10 18:07:41 +0200

    chore: use static class

--- src/utils/helpers/ImportUtils.ts
+++ src/utils/helpers/ImportUtils.ts
@@ -4,45 +4,45 @@ import { pathToFileURL } from "node:url";
 const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
 
 /**
- * Imports a file and returns the imported data.
- * @param path - The path of the file to import.
- * @param requiredDefaultExport - Whether the file must include a default
- * 	export.
- * @returns The imported data.
+ * Utility class for working with imports.
+ * @group Utils/Helpers
  */
-async function _import<ImportData>(path: string, requiredDefaultExport?: boolean): Promise<ImportData> {
-	const data = await import(path);
+export class ImportUtils {
+	/**
+	 * Imports a file and returns the imported data.
+	 *
+	 * @param path - The path of the file to import.
+	 * @param requiredDefaultExport - Whether the file must include a `default` export.
+	 * @returns The imported data from the file.
+	 *
+	 * @typeParam ImportData - The data of the imported file.
+	 */
+	static async import<ImportData>(path: string, requiredDefaultExport?: boolean): Promise<ImportData> {
+		const data = await import(path);
 
-	if (requiredDefaultExport && !("default" in data)) {
-		throw new Error(`File "${basename(path)}" must include a "default" export.`);
+		if (requiredDefaultExport && !("default" in data)) {
+			throw new TypeError(`File "${basename(path)}" must include a "default" export.`);
+		}
+
+		return data;
 	}
 
-	return data;
-}
+	/**
+	 * Resolves a path to a file for compatible with both CommonJS and ESM.
+	 *
+	 * @param fragments - The fragments of the path to resolve.
+	 * @returns The resolved path compatible with both CommonJS and ESM.
+	 */
+	static resolvePath(...fragments: string[]): string {
+		const path = join(...fragments);
+		const resolvedPath = resolve(path);
 
-/**
- * Resolves a path to a file for compatibility with both CommonJS and ESM.
- * @param fragments - The fragments of the path to resolve.
- * @returns The resolved path compatible with both CommonJS and ESM.
- */
-function resolvePath(...fragments: string[]): string {
-	const path = join(...fragments);
-	const resolvedPath = resolve(path);
+		if (IS_COMMON_JS) {
+			return resolvedPath;
+		}
 
-	if (IS_COMMON_JS) {
-		return resolvedPath;
+		const { href } = pathToFileURL(resolvedPath);
+
+		return href;
 	}
-
-	const { href } = pathToFileURL(resolvedPath);
-
-	return href;
 }
-
-/**
- * Utilities for importing files.
- * @internal
- */
-export const ImportUtils = Object.freeze({
-	import: _import,
-	resolvePath,
-});
