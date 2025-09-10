e22b12d59e577c8f018ed415f336fdded373b307
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-11 00:22:38 +0200

    feat(types): `MessageFlags`

--- src/types/resources/Messages/enums.ts
+++ src/types/resources/Messages/enums.ts
@@ -13,3 +13,25 @@ export enum EmbedTypes {
 	Rich = "rich",
 	Video = "video",
 }
+
+/**
+ * The flags of a message.
+ * @see https://discord.com/developers/docs/resources/message#message-object-message-flags
+ *
+ * @group Structures/Enums
+ */
+export enum MessageFlags {
+	Crossposted = 1 << 0,
+	Ephemeral = 1 << 6,
+	FailedToMentionSomeRolesInThread = 1 << 8,
+	HasSnapshot = 1 << 14,
+	HasThread = 1 << 5,
+	IsComponentsV2 = 1 << 15,
+	IsCrosspost = 1 << 1,
+	IsVoiceMessage = 1 << 13,
+	Loading = 1 << 7,
+	SourceMessageDeleted = 1 << 3,
+	SuppressEmbeds = 1 << 2,
+	SuppressNotifications = 1 << 12,
+	Urgent = 1 << 4,
+}
