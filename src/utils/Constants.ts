fcc7ba9005bb5ea3bda6f5ee20989907089a9c7e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-10 22:12:19 +0200

    docs: remove `Checks`

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -3,7 +3,7 @@ import { env } from "node:process";
 const { NODE_ENV } = env;
 
 /**
- * Checks whether the current environment is a production environment.
+ * Whether the current environment is a production environment.
  * @group Utils/Constants
  */
 export const IS_PRODUCTION_ENVIRONMENT = String(NODE_ENV).toLowerCase() === "production";
