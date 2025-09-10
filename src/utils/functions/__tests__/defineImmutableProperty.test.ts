d58fb4daa3ad456483994defc876a372549b3b1d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-10 21:19:41 +0200

    test: final change to test

--- src/utils/functions/__tests__/defineImmutableProperty.test.ts
+++ src/utils/functions/__tests__/defineImmutableProperty.test.ts
@@ -1,23 +1,31 @@
 import { defineImmutableProperty } from "../defineImmutableProperty.js";
 
+const DISCORD_BOT_TOKEN = "ANY_DISCORD_BOT_TOKEN";
+
 describe("Function: defineImmutableProperty", () => {
 	it("Should define 'token' property when using 'defineImmutableProperty' function.", () => {
-		const object = {};
-		const tokenValue = "ANY_DISCORD_BOT_TOKEN";
+		const object = {
+			foo: "bar",
+		};
 
+		// Property is not defined yet.
 		expect(Object.getOwnPropertyDescriptor(object, "token")).toBeUndefined();
 
-		defineImmutableProperty(object, "token", tokenValue);
+		defineImmutableProperty(object, "token", DISCORD_BOT_TOKEN);
 
-		const descriptorObject = Object.getOwnPropertyDescriptor(object, "token");
+		const descriptor = Object.getOwnPropertyDescriptor(object, "token");
 
-		const expectedTokenValueResult = tokenValue;
+		const expectedTokenValueResult = DISCORD_BOT_TOKEN;
 		const expectedTokenBooleanResult = false;
 
-		expect(descriptorObject?.value).toBe(expectedTokenValueResult);
+		// Check that all properties are defined and set to "false" if they are booleans.
+		expect(descriptor?.value).toBe(expectedTokenValueResult);
+		expect(descriptor?.configurable).toBe(expectedTokenBooleanResult);
+		expect(descriptor?.enumerable).toBe(expectedTokenBooleanResult);
+		expect(descriptor?.writable).toBe(expectedTokenBooleanResult);
 
-		expect(descriptorObject?.configurable).toBe(expectedTokenBooleanResult);
-		expect(descriptorObject?.enumerable).toBe(expectedTokenBooleanResult);
-		expect(descriptorObject?.writable).toBe(expectedTokenBooleanResult);
+		// If "enumerable" is "false", the property will not be listed in the "Object.keys" method.
+		// Since "object" already had a "foo" property, the length should be "1".
+		expect(Object.keys(object).length).toBe(1);
 	});
 });
