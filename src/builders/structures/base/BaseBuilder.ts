0820a9596d15aa85114eedf531c76738feefea77
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-20 23:40:33 +0200

    docs: update base builder comments

--- src/builders/structures/base/BaseBuilder.ts
+++ src/builders/structures/base/BaseBuilder.ts
@@ -1,4 +1,9 @@
-/** Represents a base class for all builder classes. */
+/**
+ * Represents a base class for all builder structures.
+ *
+ * @typeParam BuilderData - The type of the data of the builder.
+ * @group Builders/Structures
+ */
 export abstract class BaseBuilder<BuilderData> {
 	/** The object containing the data of the builder. */
 	// biome-ignore lint/style/useReadonlyClassProperties: The "data" property is being assigned in some builders.
--- src/builders/structures/base/ButtonBase.ts
+++ src/builders/structures/base/ButtonBase.ts
@@ -1,13 +1,18 @@
 import { type ButtonComponent, ComponentTypes } from "#types/index.js";
 import { BaseBuilder } from "./BaseBuilder.js";
 
-/** Represents a base class for all button builders. */
-export abstract class ButtonBase extends BaseBuilder<ButtonComponent> {
+/**
+ * Represents a base class for all button builder structures.
+ *
+ * @typeParam ButtonBuilderData - The type of the data of the button builder.
+ * @group Builders/Structures
+ */
+export abstract class ButtonBase<ButtonBuilderData extends ButtonComponent> extends BaseBuilder<ButtonBuilderData> {
 	/**
 	 * Creates a new {@link ButtonBase | `ButtonBase`} instance.
 	 * @param data - The data of the button to use.
 	 */
-	constructor(data: Partial<ButtonComponent> = {}) {
+	constructor(data: Partial<ButtonBuilderData> = {}) {
 		super();
 
 		this.data = {
