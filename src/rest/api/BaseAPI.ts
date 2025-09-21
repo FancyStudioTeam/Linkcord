1ae0005ffe518f6c0616e394c813e9e9489e31e3
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-09-21 18:26:21 +0200

    chore: use `makeRequest`

--- src/rest/api/BaseAPI.ts
+++ src/rest/api/BaseAPI.ts
@@ -1,12 +1,6 @@
 import type { Client } from "#client/index.js";
-import type {
-	RESTDeleteOptions,
-	RESTGetOptions,
-	RESTManager,
-	RESTPatchOptions,
-	RESTPostOptions,
-	RESTPutOptions,
-} from "#rest/structures/RESTManager.js";
+import type { RESTManager } from "#rest/structures/RESTManager.js";
+import { type MakeRequestOptions, RESTMethods } from "#rest/types/index.js";
 import { defineImmutableProperty } from "#utils/functions/defineImmutableProperty.js";
 
 /** Represents a base class for all API classes. */
@@ -32,71 +26,76 @@ export class BaseAPI {
 
 	/**
 	 * Performs a `DELETE` request to the Discord API.
+	 *
 	 * @param endpoint - The endpoint where the request will be made.
 	 * @param options - The options to use when performing the request.
 	 */
 	protected delete<Result, QueryStringParams = never>(
 		endpoint: string,
-		options?: RESTDeleteOptions<QueryStringParams>,
+		options?: MakeRequestOptions<never, QueryStringParams>,
 	): Promise<Result> {
 		const { rest } = this;
 
-		return rest.delete<Result, QueryStringParams>(endpoint, options);
+		return rest.makeRequest<Result, never, QueryStringParams>(RESTMethods.Delete, endpoint, options);
 	}
 
 	/**
 	 * Performs a `GET` request to the Discord API.
+	 *
 	 * @param endpoint - The endpoint where the request will be made.
 	 * @param options - The options to use when performing the request.
 	 */
 	protected async get<Result, QueryStringParams = never>(
 		endpoint: string,
-		options?: RESTGetOptions<QueryStringParams>,
+		options?: MakeRequestOptions<never, QueryStringParams>,
 	): Promise<Result> {
 		const { rest } = this;
 
-		return await rest.get<Result, QueryStringParams>(endpoint, options);
+		return await rest.makeRequest<Result, never, QueryStringParams>(RESTMethods.Get, endpoint, options);
 	}
 
 	/**
 	 * Performs a `PATCH` request to the Discord API.
+	 *
 	 * @param endpoint - The endpoint where the request will be made.
 	 * @param options - The options to use when performing the request.
 	 */
 	protected async patch<Result, JSONParams = never, QueryStringParams = never>(
 		endpoint: string,
-		options?: RESTPatchOptions<JSONParams, QueryStringParams>,
+		options?: MakeRequestOptions<JSONParams, QueryStringParams>,
 	): Promise<Result> {
 		const { rest } = this;
 
-		return await rest.patch<Result, JSONParams, QueryStringParams>(endpoint, options);
+		return await rest.makeRequest<Result, JSONParams, QueryStringParams>(RESTMethods.Patch, endpoint, options);
 	}
 
 	/**
 	 * Performs a `POST` request to the Discord API.
+	 *
 	 * @param endpoint - The endpoint where the request will be made.
 	 * @param options - The options to use when performing the request.
 	 */
 	protected async post<Result, JSONParams = never, QueryStringParams = never>(
 		endpoint: string,
-		options?: RESTPostOptions<JSONParams, QueryStringParams>,
+		options?: MakeRequestOptions<JSONParams, QueryStringParams>,
 	): Promise<Result> {
 		const { rest } = this;
 
-		return await rest.post<Result, JSONParams, QueryStringParams>(endpoint, options);
+		return await rest.makeRequest<Result, JSONParams, QueryStringParams>(RESTMethods.Post, endpoint, options);
 	}
 
 	/**
 	 * Performs a `PUT` request to the Discord API.
+	 *
 	 * @param endpoint - The endpoint where the request will be made.
 	 * @param options - The options to use when performing the request.
 	 */
 	protected async put<Result, JSONParams = never, QueryStringParams = never>(
 		endpoint: string,
-		options?: RESTPutOptions<JSONParams, QueryStringParams>,
+		options?: MakeRequestOptions<JSONParams, QueryStringParams>,
 	): Promise<Result> {
 		const { rest } = this;
 
-		return await rest.put<Result, JSONParams, QueryStringParams>(endpoint, options);
+		return await rest.makeRequest<Result, JSONParams, QueryStringParams>(RESTMethods.Put, endpoint, options);
 	}
 }
