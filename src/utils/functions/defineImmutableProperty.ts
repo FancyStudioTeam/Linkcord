f31ea16272a8b24bb70cb29029f423ced6f92b9d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-04 01:01:37 +0200

    docs: update typeParam

--- src/utils/functions/defineImmutableProperty.ts
+++ src/utils/functions/defineImmutableProperty.ts
@@ -5,8 +5,8 @@
  * @param propertyName - The name of the property to define.
  * @param value - The value of the property.
  *
- * @typeParam Object - The type inferred from the `object` parameter.
- * @typeParam Value - The type inferred from the `value` parameter.
+ * @typeParam Object - The inferred type from the `object` parameter.
+ * @typeParam Value - The inferred type from the `value` parameter.
  * @group Utils/Functions
  */
 export function defineImmutableProperty<Object, Value>(object: Object, propertyName: string, value: Value): void {
--- src/utils/functions/tryImport.ts
+++ src/utils/functions/tryImport.ts
@@ -4,7 +4,7 @@
  * @param packageName - The name of the package to import.
  * @returns The data of the package, or `null` if the package is not installed.
  *
- * @typeParam PackageData - The data of the imported package.
+ * @typeParam PackageData - The shape of the data of the package.
  * @group Utils/Functions
  */
 export async function tryImport<PackageData>(packageName: string): Promise<PackageData | null> {
