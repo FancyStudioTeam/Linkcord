d262cfdef4e4a3abac0f167d39a8ccdfc2216439
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-05 00:31:13 +0200

    style: missing new line

--- src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
+++ src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from "vitest";
 import { ConfigurationTokenSchema } from "../ConfigurationSchema.js";
 
 const DISCORD_TOKEN = "ODAzNTExMTAyMjQ2Nzg5MTI.XXXXXXXXXX";
+
 describe("Schema: ConfigurationTokenSchema", () => {
 	it("GIVEN a Zod schema WHEN validating valid input THEN does not throw", () => {
 		expect(() => ConfigurationTokenSchema.parse(DISCORD_TOKEN)).not.toThrow();
