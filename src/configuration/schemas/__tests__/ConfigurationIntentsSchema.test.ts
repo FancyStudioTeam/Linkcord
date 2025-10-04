67410c174349ab693a58ed35e811c357374073f4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-05 00:29:52 +0200

    test: check if schema throws or not

--- src/configuration/schemas/__tests__/ConfigurationIntentsSchema.test.ts
+++ src/configuration/schemas/__tests__/ConfigurationIntentsSchema.test.ts
@@ -1,11 +1,24 @@
-import { parse } from "valibot";
 import { describe, expect, it } from "vitest";
+import { GatewayIntents } from "#types/index.js";
 import { ConfigurationIntentsSchema } from "../ConfigurationSchema.js";
 
-describe("Schema: ConfigurationIntentsSchema", () => {
-	it("Should throw an error if the input is not a valid array.", () => {
-		const InvalidInputType = null;
+const GUILDS_INTENT_NUMBER = 1;
+const MESSAGE_CONTENT_INTENT_NUMBER = 32768;
 
-		expect(() => parse(ConfigurationIntentsSchema, InvalidInputType)).toThrowError();
+describe("Schema: ConfigurationIntentsSchema", () => {
+	it("GIVEN a Zod schema WHEN validating valid input THEN does not throw", () => {
+		expect(() =>
+			ConfigurationIntentsSchema.parse([GatewayIntents.Guilds, GatewayIntents.MessageContent]),
+		).not.toThrow();
+		expect(() =>
+			ConfigurationIntentsSchema.parse([GUILDS_INTENT_NUMBER, MESSAGE_CONTENT_INTENT_NUMBER]),
+		).not.toThrow();
+	});
+
+	it("GIVEN a Zod schema WHEN validating invalid input THEN does throw", () => {
+		expect(() => ConfigurationIntentsSchema.parse(null)).toThrow();
+		expect(() => ConfigurationIntentsSchema.parse([])).toThrow();
+		expect(() => ConfigurationIntentsSchema.parse([null])).toThrow();
+		expect(() => ConfigurationIntentsSchema.parse([0])).toThrow();
 	});
 });
--- src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
+++ src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
@@ -1,11 +1,13 @@
-import { parse } from "valibot";
 import { describe, expect, it } from "vitest";
 import { ConfigurationTokenSchema } from "../ConfigurationSchema.js";
 
+const DISCORD_TOKEN = "ODAzNTExMTAyMjQ2Nzg5MTI.XXXXXXXXXX";
 describe("Schema: ConfigurationTokenSchema", () => {
-	it("Should throw an error if the input is not a valid string.", () => {
-		const InvalidInputType = null;
+	it("GIVEN a Zod schema WHEN validating valid input THEN does not throw", () => {
+		expect(() => ConfigurationTokenSchema.parse(DISCORD_TOKEN)).not.toThrow();
+	});
 
-		expect(() => parse(ConfigurationTokenSchema, InvalidInputType)).toThrowError();
+	it("GIVEN a Zod schema WHEN validating invalid input THEN does throw", () => {
+		expect(() => ConfigurationTokenSchema.parse(null)).toThrow();
 	});
 });
