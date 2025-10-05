6859356e9404adda2aa9b5cc4c74dcae24657fa1
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-05 18:08:25 +0200

    chore: use `validation` for schemas

--- src/configuration/schemas/__tests__/ConfigurationIntentsSchema.test.ts
+++ src/configuration/schemas/__tests__/ConfigurationIntentsSchema.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { GatewayIntents } from "#types/index.js";
+import { validate } from "#utils/functions/validate.js";
+import { ValidationError } from "#utils/index.js";
 import { ConfigurationIntentsSchema } from "../ConfigurationSchema.js";
 
 const GUILDS_INTENT_NUMBER = 1;
@@ -8,17 +10,17 @@ const MESSAGE_CONTENT_INTENT_NUMBER = 32768;
 describe("Schema: ConfigurationIntentsSchema", () => {
 	it("GIVEN a Zod schema WHEN validating valid input THEN does not throw", () => {
 		expect(() =>
-			ConfigurationIntentsSchema.parse([GatewayIntents.Guilds, GatewayIntents.MessageContent]),
-		).not.toThrow();
+			validate(ConfigurationIntentsSchema, [GatewayIntents.Guilds, GatewayIntents.MessageContent]),
+		).not.toThrow(ValidationError);
 		expect(() =>
-			ConfigurationIntentsSchema.parse([GUILDS_INTENT_NUMBER, MESSAGE_CONTENT_INTENT_NUMBER]),
-		).not.toThrow();
+			validate(ConfigurationIntentsSchema, [GUILDS_INTENT_NUMBER, MESSAGE_CONTENT_INTENT_NUMBER]),
+		).not.toThrow(ValidationError);
 	});
 
 	it("GIVEN a Zod schema WHEN validating invalid input THEN does throw", () => {
-		expect(() => ConfigurationIntentsSchema.parse(null)).toThrow();
-		expect(() => ConfigurationIntentsSchema.parse([])).toThrow();
-		expect(() => ConfigurationIntentsSchema.parse([null])).toThrow();
-		expect(() => ConfigurationIntentsSchema.parse([0])).toThrow();
+		expect(() => validate(ConfigurationIntentsSchema, null)).toThrow(ValidationError);
+		expect(() => validate(ConfigurationIntentsSchema, [])).toThrow(ValidationError);
+		expect(() => validate(ConfigurationIntentsSchema, [null])).toThrow(ValidationError);
+		expect(() => validate(ConfigurationIntentsSchema, [0])).toThrow(ValidationError);
 	});
 });
--- src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
+++ src/configuration/schemas/__tests__/ConfigurationTokenSchema.test.ts
@@ -1,14 +1,16 @@
 import { describe, expect, it } from "vitest";
+import { validate } from "#utils/functions/validate.js";
+import { ValidationError } from "#utils/index.js";
 import { ConfigurationTokenSchema } from "../ConfigurationSchema.js";
 
 const DISCORD_TOKEN = "ODAzNTExMTAyMjQ2Nzg5MTI.XXXXXXXXXX";
 
 describe("Schema: ConfigurationTokenSchema", () => {
 	it("GIVEN a Zod schema WHEN validating valid input THEN does not throw", () => {
-		expect(() => ConfigurationTokenSchema.parse(DISCORD_TOKEN)).not.toThrow();
+		expect(() => validate(ConfigurationTokenSchema, DISCORD_TOKEN)).not.toThrow(ValidationError);
 	});
 
 	it("GIVEN a Zod schema WHEN validating invalid input THEN does throw", () => {
-		expect(() => ConfigurationTokenSchema.parse(null)).toThrow();
+		expect(() => validate(ConfigurationTokenSchema, null)).toThrow(ValidationError);
 	});
 });
