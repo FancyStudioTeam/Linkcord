c76d23b8a437893a25a0d426736b57e426961051
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-06 21:16:54 +0200

    chore: use shared schema

--- src/builders/schemas/messages/FileSchema.ts
+++ src/builders/schemas/messages/FileSchema.ts
@@ -1,14 +1,12 @@
 import { boolean, instanceof as instanceof_, literal, object, union } from "zod";
 import { FileBuilder } from "#builders/structures/messages/FileBuilder.js";
 import { ComponentTypes } from "#types/index.js";
-import { IDSchema, URLSchema } from "../Shared.js";
+import { IDSchema, UnfurledMediaItemSchema } from "../Shared.js";
 
 export const FileSpoilerSchema = boolean();
 export const FileIDSchema = IDSchema;
 export const FileTypeSchema = literal(ComponentTypes.File);
-export const FileURLSchema = URLSchema.transform((url) => ({
-	url,
-}));
+export const FileURLSchema = UnfurledMediaItemSchema;
 
 export const FileInstanceSchema = instanceof_(FileBuilder).transform((builder) => builder.toJSON());
 export const FileObjectSchema = object({
