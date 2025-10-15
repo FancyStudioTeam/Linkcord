83b58da2c83bca3bf3425fe958a2ee991785487e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-10-15 21:58:18 +0200

    chore: update implementation and update tests

--- src/utils/functions/__tests__/exception.test.ts
+++ src/utils/functions/__tests__/exception.test.ts
@@ -1,19 +1,32 @@
+// @ts-nocheck
+
 import { exception } from "../exception.js";
 
-const ERROR_MESSAGE = "This is an error message";
+const ERROR_MESSAGE = "Expected type to be a string";
 
 describe("Function: exception", () => {
-	it("Should throw the given error message using the default Error class", () => {
+	it("Should throw the given error message as an Error instance", () => {
 		const result = () => exception(ERROR_MESSAGE);
 		const expectedErrorResult = new Error(ERROR_MESSAGE);
 
 		expect(result).toThrow(expectedErrorResult);
 	});
 
-	it("Should throw the given error message using the TypeError class", () => {
+	it("Should throw the given error message as the given TypeError instance", () => {
 		const result = () => exception(TypeError, ERROR_MESSAGE);
 		const expectedErrorResult = new TypeError(ERROR_MESSAGE);
 
 		expect(result).toThrow(expectedErrorResult);
+	});
+
+	it("Should throw a TypeError if some of the given parameters are not valid functions or strings", () => {
+		const result1 = () => exception(null);
+		const result2 = () => exception(TypeError, null);
+
+		const expectedErrorResult1 = new TypeError("First parameter (error) from 'exception' must be a funcion");
+		const expectedErrorResult2 = new TypeError("Second parameter (message) from 'exception' must be a string");
+
+		expect(result1).toThrow(expectedErrorResult1);
+		expect(result2).toThrow(expectedErrorResult2);
 	});
 });
--- src/utils/functions/exception.ts
+++ src/utils/functions/exception.ts
@@ -1,7 +1,7 @@
 import type { Newable } from "#utils/types/Util.js";
 
 /**
- * Throws an exception in inline expressions.
+ * Throws an exception in an inline expression.
  * @param message - The message of the `Error` instance.
  *
  * @group Utils/Functions
@@ -9,28 +9,35 @@ import type { Newable } from "#utils/types/Util.js";
 export function exception(message: string): never;
 
 /**
- * Throws an exception in inline expressions.
- * @param error - The class to instantiate the error.
+ * Throws an exception in an inline expression.
+ *
+ * @param error - The class to use to instantiate the error.
  * @param message The message of the `Error` instance.
  *
  * @group Utils/Functions
  */
-export function exception<Instance extends Newable<Error>>(error: Instance, message: string): never;
+export function exception<Error extends Newable<Error>>(error: Error, message: string): never;
 
 /**
- * Throws an exception in inline expressions.
+ * Throws an exception in an inline expression.
  *
- * @param errorOrMessage - The class to instantiate the error or the message of the `Error` instance.
+ * @param errorOrMessage - The class to use to instantiate the error or the message of the `Error` instance.
  * @param possibleMessage - The message of the `Error` instance, if any.
+ *
+ * @group Utils/Functions
  */
 export function exception(errorOrMessage: Newable<Error> | string, possibleMessage?: string): never {
 	if (typeof errorOrMessage === "string") {
 		throw new Error(errorOrMessage);
-	}
+	} else {
+		if (typeof errorOrMessage !== "function") {
+			throw new TypeError("First parameter (error) from 'exception' must be a funcion");
+		}
 
-	if (!possibleMessage || typeof possibleMessage !== "string") {
-		throw new TypeError("Second parameter (message) from 'exception' must be present and be a string");
-	}
+		if (typeof possibleMessage !== "string") {
+			throw new TypeError("Second parameter (message) from 'exception' must be a string");
+		}
 
-	throw new errorOrMessage(possibleMessage);
+		throw new errorOrMessage(possibleMessage);
+	}
 }
