69ea43b85c5e80b229a2867352269e20db417d45
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-08 19:41:07 +0100

    docs: delete docs

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,10 +1,10 @@
 import { env } from "node:process";
 
-/** Whether the current environment is a CommonJS environment. */
-export const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
+///////////////////////////////////////////////////////////////////////////
 
-/** Whether the current environment is a production environment. */
+export const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
 export const IS_PRODUCTION_ENVIRONMENT = String(env.NODE_ENV).toLowerCase() === "production";
 
-/** The current version of Linkcord. */
+///////////////////////////////////////////////////////////////////////////
+
 export const LINKCORD_VERSION: string = "[VI]{{inject}}[/VI]";
--- src/utils/types/Util.ts
+++ src/utils/types/Util.ts
@@ -1,37 +1,22 @@
 declare const Brand: unique symbol;
 
-/**
- * Represents a type that can be awaited.
- *
- * @typeParam Type - The shape of the type that can be awaited.
- */
+///////////////////////////////////////////////////////////////////////////
+
 export type Awaitable<Type> = Promise<Type> | Type;
 
-/**
- * Represents a type that is marked with a brand.
- *
- * @typeParam Type - The shape of the type to mark as branded.
- * @typeParam Brand - The name of the brand to use.
- */
+///////////////////////////////////////////////////////////////////////////
+
 export type Brand<Type, Brand extends string> = Type & {
 	readonly [Brand]: Brand;
 };
 
-/**
- * Represents a type that is the constructor of a class.
- *
- * @typeParam Class - The shape of the class for the constructor.
- */
-// biome-ignore lint/suspicious/noExplicitAny: Expect anything in the constructor arguments.
+///////////////////////////////////////////////////////////////////////////
+
+// biome-ignore lint/suspicious/noExplicitAny: Expect anything in the constructor arguments
 export type Constructor<Class> = new (...args: any[]) => Class;
 
-/**
- * Represents a type that evaluates a boolean condition.
- *
- * @typeParam Condition - The boolean condition to evaluate.
- * @typeParam TrueResult - The shape of the result when the evaluation is `true`.
- * @typeParam FalseResult - The shape of the result when the evaluation is `false`.
- */
+///////////////////////////////////////////////////////////////////////////
+
 export type If<Condition, TrueResult, FalseResult = null> = Condition extends true
 	? TrueResult
 	: Condition extends false
