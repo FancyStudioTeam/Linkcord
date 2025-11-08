e1fbba01bb372191c1b160716d005c687a1b664a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-08 20:53:36 +0100

    chore: use `private`

--- src/utils/errors/ValidationError.ts
+++ src/utils/errors/ValidationError.ts
@@ -1,27 +1,34 @@
 import { styleText } from "node:util";
+import { AssertionUtils } from "#utils/helpers/AssertionUtils.js";
 import type { ValidationErrorIssue } from "#utils/types/index.js";
+
+/* --------------------------------------------------------------------------- */
+
+const { isSymbol } = AssertionUtils;
+
+/* --------------------------------------------------------------------------- */
 
 export class ValidationError extends Error {
 	constructor(issues: ValidationErrorIssue[]) {
 		super();
 
-		const prettifiedIssues = this.#prettifyIssues(issues);
+		const prettifiedIssues = this.prettifyIssues(issues);
 
 		this.message = `Validation has failed with the following issues:\n${prettifiedIssues}`;
 		this.name = "ValidationError";
 	}
 
-	#flattenIssuePath(path: PropertyKey[]): string {
+	private flattenIssuePath(path: PropertyKey[]): string {
 		const formattedPathCallback = (accumulator: string, currentItem: string | number): string =>
 			typeof currentItem === "number" ? `${accumulator}[${currentItem}]` : `${accumulator}.${currentItem}`;
 
-		const filteredPath = path.filter((item) => typeof item !== "symbol");
+		const filteredPath = path.filter((item) => !isSymbol(item));
 		const formattedPath = filteredPath.slice(1).reduce(formattedPathCallback, String(filteredPath[0]));
 
 		return formattedPath;
 	}
 
-	#prettifyIssue(issue: ValidationErrorIssue, isMainIssue = true, indentLevel = 1): string {
+	private prettifyIssue(issue: ValidationErrorIssue, isMainIssue = true, indentLevel = 1): string {
 		const { issues, message, path } = issue;
 		const { length: pathLength } = path;
 
@@ -31,7 +38,7 @@ export class ValidationError extends Error {
 		let prettifiedMessage = "";
 
 		if (isMainIssue && pathLength > 0) {
-			const flattenedPath = this.#flattenIssuePath(path);
+			const flattenedPath = this.flattenIssuePath(path);
 
 			prettifiedMessage = `${indent}${icon} ${flattenedPath}:\n`;
 			prettifiedMessage += `${indent}└── ${message}`;
@@ -41,15 +48,15 @@ export class ValidationError extends Error {
 
 		if (issues) {
 			for (const issue of issues) {
-				prettifiedMessage += `\n${this.#prettifyIssue(issue, false, indentLevel + 1)}`;
+				prettifiedMessage += `\n${this.prettifyIssue(issue, false, indentLevel + 1)}`;
 			}
 		}
 
 		return styleText(["bold", "red"], prettifiedMessage);
 	}
 
-	#prettifyIssues(issues: ValidationErrorIssue[]): string {
-		const prettifiedIssues = issues.map((issue) => this.#prettifyIssue(issue));
+	private prettifyIssues(issues: ValidationErrorIssue[]): string {
+		const prettifiedIssues = issues.map((issue) => this.prettifyIssue(issue));
 		const joinedIssueMessages = prettifiedIssues.join("\n\n");
 
 		return joinedIssueMessages;
