f8118d53ff51dd8187e5d44f890e29686c5b4ed3
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-16 12:24:27 +0100

    chore(Voice): update `Voice` types

--- src/types/resources/Voice/rest/raw.ts
+++ src/types/resources/Voice/rest/raw.ts
@@ -1,7 +1,44 @@
-import type { APIVoiceRegion } from "../structures/raw.js";
+import type { ISO8601Date, Snowflake } from "#types/miscellaneous/discord.js";
+import type { APIVoiceRegion, APIVoiceState } from "../structures/raw.js";
 
 /**
- * Represents the response of the {@link RESTGetAPIVoiceRegions | `GET /voice/regions`} endpoint.
+ * @see https://discord.com/developers/docs/resources/voice#modify-current-user-voice-state-json-params
+ */
+export interface RESTPatchAPICurrentVoiceStateJSONParams {
+	channel_id?: Snowflake;
+	request_to_speak_timestamp?: ISO8601Date | null;
+	supress?: boolean;
+}
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#modify-user-voice-state-json-params
+ */
+export interface RESTPatchAPIVoiceStateJSONParams {
+	channel_id?: Snowflake;
+	supress?: boolean;
+}
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#get-user-voice-state
+ */
+export type RESTGetAPICurrentVoiceState = APIVoiceState;
+
+/**
  * @see https://discord.com/developers/docs/resources/voice#list-voice-regions
  */
 export type RESTGetAPIVoiceRegions = APIVoiceRegion[];
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#get-user-voice-state
+ */
+export type RESTGetAPIVoiceState = APIVoiceState;
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#modify-current-user-voice-state
+ */
+export type RESTPatchAPICurrentVoiceState = APIVoiceState;
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#modify-user-voice-state
+ */
+export type RESTPatchAPIVoiceState = APIVoiceState;
--- src/types/resources/Voice/structures/parsed.ts
+++ src/types/resources/Voice/structures/parsed.ts
@@ -1,16 +1,10 @@
 /**
- * Represents a Discord voice region object.
  * @see https://discord.com/developers/docs/resources/voice#voice-region-object-voice-region-structure
  */
 export interface VoiceRegion {
-	/** Whether the voice region is custom. */
 	custom: boolean;
-	/** Whether the voice region is deprecated. */
 	deprecated: boolean;
-	/** The ID of the voice region. */
 	id: string;
-	/** The name of the voice region. */
 	name: string;
-	/** Whether the voice region is optimal. */
 	optimal: boolean;
 }
