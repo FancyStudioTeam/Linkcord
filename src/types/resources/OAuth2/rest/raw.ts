221690a0ea53ff3a5dc657e9af7d4687f5bc9750
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-21 20:32:20 +0100

    feat(types): add `rest` types for `OAuth2`

---
+++ src/types/resources/OAuth2/rest/raw.ts
@@ -0,0 +1,19 @@
+import type { ISO8601Date } from "#types/miscellaneous/discord.js";
+import type { APIApplication, APIPartialApplication } from "#types/resources/Application/index.js";
+import type { APIUser } from "#types/resources/Users/index.js";
+import type { OAuth2Scopes } from "../enums.js";
+
+/**
+ * @see https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information-response-structure
+ */
+export interface RESTGetAPICurrentAuthorizationInformation {
+	application: APIPartialApplication;
+	expires: ISO8601Date;
+	scopes: OAuth2Scopes[];
+	user?: APIUser;
+}
+
+/**
+ * @see https://discord.com/developers/docs/topics/oauth2#get-current-bot-application-information
+ */
+export type RESTGetAPICurrentBotApplication = APIApplication;
