34305b4ce795d80a3f1660c9936e34783d17cb00
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-23 20:24:29 +0100

    fix: entry

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["src/index.ts", "src/jsx/jsx-runtime.ts"],
+	entry: ["src/index.ts", "src/jsx-runtime.ts"],
 	esbuildPlugins: [esbuildPluginVersionInjector()],
 	format: "esm",
 });
