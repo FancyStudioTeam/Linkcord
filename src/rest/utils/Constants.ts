3db5b3e206eb66efd4e5ddceedfb75aeced3399e
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-23 16:27:28 +0100

    chore: delete version in name

--- src/rest/utils/Constants.ts
+++ src/rest/utils/Constants.ts
@@ -3,6 +3,5 @@ import { LINKCORD_VERSION } from "#utils/Constants.js";
 export const SAFE_CHARACTERS = new Set([":", "?", "@"]);
 
 export const REST_URL_BASE = "https://discord.com/api" as const;
-export const REST_USER_AGENT =
-	`Linkcord/${LINKCORD_VERSION} (https://github.com/FancyStudioTeam/Linkcord, v${LINKCORD_VERSION})` as const;
+export const REST_USER_AGENT = `Linkcord (https://github.com/FancyStudioTeam/Linkcord, v${LINKCORD_VERSION})` as const;
 export const REST_VERSION = 10 as const;
