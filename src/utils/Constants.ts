aef68c64c06aad8e8530d3bdb0a7e12631735912
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-23 19:14:25 +0100

    fix: use `as const`

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -15,7 +15,7 @@ const { version } = packageJSONParsedContent;
 export const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
 export const IS_PRODUCTION_ENVIRONMENT = String(env.NODE_ENV).toLowerCase() === "production";
 
+export const LINKCORD_AGENT = `Linkcord (https://github.com/FancyStudioTeam/Linkcord)` as const;
 export const LINKCORD_VERSION: string = "[VI]{{inject}}[/VI]";
-export const LINKCORD_AGENT = `Linkcord (https://github.com/FancyStudioTeam/Linkcord)`;
 
 export const ONE_SECOND_MILLISECONDS = 1_000;
