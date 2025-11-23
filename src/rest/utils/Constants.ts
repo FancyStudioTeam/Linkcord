c3fe5426aba0236aa590713b2c8e8839abf5b4bb
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-23 19:13:50 +0100

    fix: use `LINKCORD_AGENT`

--- src/rest/utils/Constants.ts
+++ src/rest/utils/Constants.ts
@@ -1,7 +1,7 @@
-import { LINKCORD_VERSION } from "#utils/Constants.js";
-
-export const SAFE_CHARACTERS = new Set([":", "?", "@"]);
+import { LINKCORD_AGENT } from "#utils/Constants.js";
 
 export const REST_URL_BASE = "https://discord.com/api" as const;
-export const REST_USER_AGENT = `Linkcord (https://github.com/FancyStudioTeam/Linkcord, v${LINKCORD_VERSION})` as const;
+export const REST_USER_AGENT = LINKCORD_AGENT;
 export const REST_VERSION = 10 as const;
+
+export const SAFE_CHARACTERS = new Set([":", "?", "@"]);
