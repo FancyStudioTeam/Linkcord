185ce1e2dc401d9057fb95ff406580d1854b6bae
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-28 22:05:55 +0100

    feat(builders): add `EmbedField` type

---
+++ src/builders/types/structures/messages/EmbedField.ts
@@ -0,0 +1,4 @@
+import type { EmbedFieldBuilder } from "#builders/structures/index.js";
+import type { EmbedField } from "#types/index.js";
+
+export type AllowedEmbedField = EmbedFieldBuilder | EmbedField;
