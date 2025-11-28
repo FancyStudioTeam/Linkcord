5cd1dabb831885a5b76eea3cd295ef9bc4dbf9da
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-28 21:15:18 +0100

    chore: changes in shared schemas

--- src/builders/schemas/Shared.ts
+++ src/builders/schemas/Shared.ts
@@ -1,21 +1,28 @@
-import { hex, instanceof as instanceof_, number, string, union, url } from "zod";
+import { hex, instanceof as instanceof_, int, string, union, url } from "zod";
 
 const MAXIMUM_32_BIT_LENGTH = 2_147_483_647;
 const MAXIMUM_CUSTOM_ID_LENGTH = 100;
+const MAXIMUM_HEX_CODE_DECIMAL_LENGTH = 16_777_215;
 
-const URLInstanceSchema = instanceof_(URL, {
-	error: "Expected input to be an instance of URL",
-}).transform((url) => url.toString());
-const URLStringSchema = url();
-
-export const ColorNumberSchema = hex().transform((hex) => Number(`0x${hex.replace("#", "").toLowerCase()}`));
-export const ColorStringSchema = hex();
-export const ColorSchema = union([ColorNumberSchema, ColorStringSchema]);
+export const ColorNumberSchema = int().min(0).max(MAXIMUM_HEX_CODE_DECIMAL_LENGTH);
+export const ColorStringSchema = hex().transform((hex) => Number(`0x${hex.replace("#", "")}`));
+export const ColorSchema = union([
+	ColorNumberSchema,
+	ColorStringSchema,
+]);
 
 export const CustomIDSchema = string().min(1).max(MAXIMUM_CUSTOM_ID_LENGTH);
-export const IDSchema = number().int().min(0).max(MAXIMUM_32_BIT_LENGTH);
+export const IDSchema = int().min(0).max(MAXIMUM_32_BIT_LENGTH);
 
-export const URLSchema = union([URLInstanceSchema, URLStringSchema]);
+export const URLInstanceSchema = instanceof_(URL, {
+	error: "Expected input to be an instance of URL",
+}).transform((url) => url.toString());
+export const URLStringSchema = url();
+export const URLSchema = union([
+	URLInstanceSchema,
+	URLStringSchema,
+]);
+
 export const UnfurledMediaItemSchema = URLSchema.transform((url) => ({
 	url,
 }));
