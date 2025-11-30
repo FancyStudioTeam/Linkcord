909bdfb1ce5e1d1ebc4b97bfad6a110987a3ecf4
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-11-30 20:01:36 +0100

    types(OAuth2): remove type that requires access token

--- src/types/resources/OAuth2/rest/raw.ts
+++ src/types/resources/OAuth2/rest/raw.ts
@@ -1,17 +1,4 @@
-import type { ISO8601Date } from "#types/miscellaneous/discord.js";
-import type { APIApplication, APIPartialApplication } from "#types/resources/Applications/index.js";
-import type { APIUser } from "#types/resources/Users/index.js";
-import type { OAuth2Scopes } from "../enums.js";
-
-/**
- * @see https://discord.com/developers/docs/topics/oauth2#get-current-authorization-information-response-structure
- */
-export interface RESTGetAPICurrentAuthorizationInformation {
-	application: APIPartialApplication;
-	expires: ISO8601Date;
-	scopes: OAuth2Scopes[];
-	user?: APIUser;
-}
+import type { APIApplication } from "#types/resources/Applications/index.js";
 
 /**
  * @see https://discord.com/developers/docs/topics/oauth2#get-current-bot-application-information
