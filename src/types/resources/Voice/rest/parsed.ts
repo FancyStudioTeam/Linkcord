8f6d40088f62a71b1cbcdb5c8cfe390cb7c97457
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-03 20:59:08 +0100

    chore: things

---
+++ src/types/resources/Voice/rest/parsed.ts
@@ -0,0 +1,18 @@
+import type { ISO8601Date, Snowflake } from "#types/miscellaneous/discord.js";
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#modify-current-user-voice-state-json-params
+ */
+export interface EditCurrentVoiceStateOptions {
+	channelId?: Snowflake;
+	requestToSpeakTimestamp?: ISO8601Date | null;
+	supress?: boolean;
+}
+
+/**
+ * @see https://discord.com/developers/docs/resources/voice#modify-user-voice-state-json-params
+ */
+export interface EditVoiceStateOptions {
+	channelId?: Snowflake;
+	supress?: boolean;
+}
