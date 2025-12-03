e7455a93ab79b5cd25e4e392f1d51950c578ea30
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-03 19:00:44 +0100

    feat: add `extractor:check-release-tags`

---
+++ scripts/checkReleaseTags.ts
@@ -0,0 +1,8 @@
+import { join } from "node:path";
+import { cwd } from "node:process";
+import { Extractor, ExtractorConfig } from "@microsoft/api-extractor";
+
+const apiExtractorJsonPath = join(cwd(), "api-extractor.json");
+const config = ExtractorConfig.loadFileAndPrepare(apiExtractorJsonPath);
+
+Extractor.invoke(config);
