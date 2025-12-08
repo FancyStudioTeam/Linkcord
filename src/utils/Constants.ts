9430b8fdb02433cdfedd61edba4e50a855ccbebe
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-08 14:49:55 +0100

    fix: `Constants.ts`

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -1,9 +1,6 @@
 import { env } from "node:process";
-import { AssertionUtils } from "./helpers/AssertionUtils.js";
 
-const { isUndefined } = AssertionUtils;
-
-export const IS_COMMON_JS = !(isUndefined(require) || isUndefined(module));
+export const IS_COMMON_JS = typeof require !== "undefined" && typeof module !== "undefined";
 export const IS_PRODUCTION_ENVIRONMENT = String(env.NODE_ENV).toLowerCase() === "production";
 
 export const LINKCORD_AGENT = `Linkcord (https://github.com/FancyStudioTeam/Linkcord)` as const;
