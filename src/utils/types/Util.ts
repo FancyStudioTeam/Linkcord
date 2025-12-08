9fd9f5dfcb32142bf5cfa9129984f2532451155d
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-08 15:50:44 +0100

    feat(Utils): add `Any`

--- src/utils/types/Util.ts
+++ src/utils/types/Util.ts
@@ -1,12 +1,18 @@
 declare const Brand: unique symbol;
 
+/*
+ * biome-ignore lint/suspicious/noExplicitAny: This utility type intentionally exposes `any` as `Any`.
+ * This avoids globally disabling or repeatedly suppressing Biome's `noExplicitAny` rule.
+ */
+export type Any = any;
+
 export type Awaitable<Type> = Promise<Type> | Type;
 
 export type Brand<Type, Brand extends string> = Type & {
 	readonly [Brand]: Brand;
 };
 
-export type Constructor<Class> = new (...args: unknown[]) => Class;
+export type Constructor<Class> = new (...args: Any[]) => Class;
 
 export type If<Condition, TrueResult, FalseResult = null> = Condition extends true
 	? TrueResult
