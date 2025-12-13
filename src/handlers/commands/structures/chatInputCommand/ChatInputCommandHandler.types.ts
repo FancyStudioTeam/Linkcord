1bd26c51c08526f1b239c58ba08412d4b104f916
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-13 20:43:46 +0100

    fix(types): exported instances are non abstract

--- src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.types.ts
+++ src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.types.ts
@@ -1,6 +1,6 @@
 import type { CreateApplicationCommandOptions } from "#types/index.js";
-import type { Constructor } from "#utils/index.js";
+import type { NonAbstractConstructor } from "#utils/index.js";
 import type { ChatInputCommandHandler } from "./ChatInputCommandHandler.js";
 
-export type ChatInputCommandHandlerConstructor = Constructor<ChatInputCommandHandler>;
+export type ChatInputCommandHandlerConstructor = NonAbstractConstructor<ChatInputCommandHandler>;
 export type ChatInputCommandHandlerDeclareOptions = Omit<CreateApplicationCommandOptions, "options" | "type">;
