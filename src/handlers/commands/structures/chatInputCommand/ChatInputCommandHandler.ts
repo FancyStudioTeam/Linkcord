3bfec1f8005181cffaebe76d40b275a3b0135d26
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-13 15:33:09 +0100

    feat(ChatInputCommandHandler): add `toJSON`

--- src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.ts
+++ src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.ts
@@ -1,5 +1,15 @@
+import { ApplicationCommandType, type CreateChatInputApplicationCommandOptions } from "#types/index.js";
 import type { ChatInputCommandHandlerDeclareOptions } from "./ChatInputCommandHandler.types.js";
 
 export abstract class ChatInputCommandHandler {
 	declare declareOptions: ChatInputCommandHandlerDeclareOptions;
+
+	toJSON(): CreateChatInputApplicationCommandOptions {
+		const { declareOptions } = this;
+
+		return {
+			...declareOptions,
+			type: ApplicationCommandType.ChatInput,
+		};
+	}
 }
