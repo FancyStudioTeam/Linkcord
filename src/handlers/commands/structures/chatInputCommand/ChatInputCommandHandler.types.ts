0c06fc6992236cdcbf69db0948a372f587dcf460
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-13 22:13:44 +0100

    fix(types): use `CreateChatInputApplicationCommandOptions`

--- src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.types.ts
+++ src/handlers/commands/structures/chatInputCommand/ChatInputCommandHandler.types.ts
@@ -1,6 +1,6 @@
-import type { CreateApplicationCommandOptions } from "#types/index.js";
+import type { CreateChatInputApplicationCommandOptions } from "#types/index.js";
 import type { NonAbstractConstructor } from "#utils/index.js";
 import type { ChatInputCommandHandler } from "./ChatInputCommandHandler.js";
 
 export type ChatInputCommandHandlerConstructor = NonAbstractConstructor<ChatInputCommandHandler>;
-export type ChatInputCommandHandlerDeclareOptions = Omit<CreateApplicationCommandOptions, "options" | "type">;
+export type ChatInputCommandHandlerDeclareOptions = Omit<CreateChatInputApplicationCommandOptions, "options" | "type">;
