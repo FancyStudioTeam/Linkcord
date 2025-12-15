70e85425f6229cae2e50f584cb74f2f8eae77319
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-15 20:14:57 +0100

    build: add `// @ts-expect-error`

--- src/handlers/decorators/Declare.ts
+++ src/handlers/decorators/Declare.ts
@@ -3,6 +3,7 @@ import type { DeclarableConstructor, DeclareOptions } from "./Declare.types.js";
 
 export function Declare<Target extends DeclarableConstructor>(declareOptions: DeclareOptions<Target>) {
 	return (target: DeclarableConstructor) => {
+		// @ts-expect-error
 		return class extends target {
 			constructor(...args: unknown[]) {
 				super(...args);
