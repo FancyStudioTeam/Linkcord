f51d1f33350066090369c53c390631e8dc35270f
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-15 20:34:26 +0100

    feat(Collection): add `size`

--- src/utils/structures/Collection.ts
+++ src/utils/structures/Collection.ts
@@ -6,9 +6,6 @@ export class Collection<Key, Value> {
 		this.#limit = limit;
 	}
 
-	/**
-	 * Indicates whether the collection exceeds its limit.
-	 */
 	get #isLimitExceeded(): boolean {
 		const entries = this.#entries;
 		const limit = this.#limit;
@@ -19,12 +16,13 @@ export class Collection<Key, Value> {
 		return isLimitExceeded;
 	}
 
-	/**
-	 * Deletes the oldest value in the collection.
-	 *
-	 * @remarks
-	 * - If the collection is empty, this method does nothing.
-	 */
+	get size() {
+		const entries = this.#entries;
+		const { size } = entries;
+
+		return size;
+	}
+
 	#deleteOldestEntry(): void {
 		const entries = this.#entries;
 		const keys = entries.keys();
@@ -37,13 +35,6 @@ export class Collection<Key, Value> {
 		}
 	}
 
-	/**
-	 * Deletes an entry from the collection.
-	 *
-	 * @param key - The key associated with the entry to delete.
-	 *
-	 * @returns `true` if the entry was deleted, `false` otherwise.
-	 */
 	delete(key: Key): boolean {
 		const entries = this.#entries;
 		const isDeleted = entries.delete(key);
@@ -51,13 +42,6 @@ export class Collection<Key, Value> {
 		return isDeleted;
 	}
 
-	/**
-	 * Gets a value from the collection by its key.
-	 *
-	 * @param key - The key associated with the entry to get.
-	 *
-	 * @returns The value associated with the key if it exists, `undefined` otherwise.
-	 */
 	get(key: Key): Value | undefined {
 		const entries = this.#entries;
 		const entry = entries.get(key);
@@ -65,13 +49,6 @@ export class Collection<Key, Value> {
 		return entry;
 	}
 
-	/**
-	 * Checks whether the collection contains an entry with the specified key.
-	 *
-	 * @param key - The key associated with the entry to check.
-	 *
-	 * @returns `true` if the entry exists, `false` otherwise.
-	 */
 	has(key: Key): boolean {
 		const entries = this.#entries;
 		const existsEntry = entries.has(key);
@@ -80,17 +57,9 @@ export class Collection<Key, Value> {
 	}
 
 	/**
-	 * Sets a key-value entry to the collection.
-	 *
-	 * @param key - The key associated with the value.
-	 * @param value - The value to store.
-	 *
 	 * @remarks
 	 * - If the collection exceeds its limit, the oldest entry will be removed
 	 * before setting the new entry.
-	 *
-	 * - If an entry with the same key exists, its value will be overwritten
-	 * with the new value.
 	 */
 	set(key: Key, value: Value): void {
 		const isLimitExceeded = this.#isLimitExceeded;
@@ -104,11 +73,6 @@ export class Collection<Key, Value> {
 		entries.set(key, value);
 	}
 
-	/**
-	 * Converts the collection into an array containing all stored values.
-	 *
-	 * @returns An array of the stored values.
-	 */
 	toArray(): Value[] {
 		const entries = this.#entries;
 
