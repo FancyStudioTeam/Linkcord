9387e8c36182a2a3557c1a9de86260481833c4fc
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-18 13:20:54 +0100

    feat(rest)!: add `RESTManager.types.ts`

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -2,5 +2,4 @@ export * from "./api/index.js";
 export * from "./endpoints/index.js";
 export * from "./errors/index.js";
 export * from "./structures/index.js";
-export * from "./types/index.js";
 export * from "./utils/index.js";
--- src/rest/structures/index.ts
+++ src/rest/structures/index.ts
@@ -1,1 +1,2 @@
 export * from "./RESTManager.js";
+export * from "./RESTManager.types.js";
