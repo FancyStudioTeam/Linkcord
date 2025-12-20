2e2b285459729db66f2d21e5bc0ea3de551ffcc2
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-20 14:43:51 +0100

    feat(client): add `EnsureInitialized` decorator

---
+++ src/client/decorators/EnsureInitialized.ts
@@ -0,0 +1,19 @@
+import { isFunction } from "#utils/helpers/AssertionUtils.js";
+
+export function EnsureInitialized() {
+	return (_target: object, _propertyKey: string, propertyDescriptor: PropertyDescriptor) => {
+		const { get } = propertyDescriptor;
+		const originalGetter = get;
+
+		if (!isFunction(originalGetter)) {
+			throw new TypeError(`'${originalGetter}' is not a getter method`);
+		}
+
+		propertyDescriptor.get = function () {
+			// @ts-expect-error
+			this.checkIsInitialized();
+
+			return originalGetter.call(this);
+		};
+	};
+}
