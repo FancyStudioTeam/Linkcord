fcae9cf41c91e2822fa9bbd018a6885ffe62e8f2
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-20 14:47:57 +0100

    chore: new files for `Client`

---
+++ src/client/structures/Client/Client.ts
@@ -0,0 +1,52 @@
+import { CacheManager, CommandManager, EventManager } from "#client/managers/index.js";
+import { GatewayManager } from "#gateway/index.js";
+import { RESTManager } from "#rest/index.js";
+import { defineImmutableProperty } from "#utils/functions/defineImmutableProperty.js";
+import { isUndefined } from "#utils/helpers/AssertionUtils.js";
+import { ClientBase } from "../ClientBase.js";
+import { type ClientDebugOptions, ClientEvents } from "./Client.types.js";
+
+const BRACKETS_REGEX = /^\[*(.*?)\]*$/;
+
+export class Client extends ClientBase {
+	declare readonly cache: CacheManager;
+	declare readonly commands: CommandManager;
+	declare readonly events: EventManager;
+	declare readonly gateway: GatewayManager;
+	declare readonly rest: RESTManager;
+
+	constructor() {
+		super();
+
+		defineImmutableProperty(this, "cache", new CacheManager());
+		defineImmutableProperty(this, "commands", new CommandManager());
+		defineImmutableProperty(this, "events", new EventManager());
+		defineImmutableProperty(this, "gateway", new GatewayManager(this));
+		defineImmutableProperty(this, "rest", new RESTManager(this));
+	}
+
+	#normalizeLabelBrackets(label: string): `[${string}]` {
+		const [_, content] = label.match(BRACKETS_REGEX) ?? [];
+
+		return !isUndefined(content) ? `[${content}]` : `[Unknown]`;
+	}
+
+	debug(message: string, options?: ClientDebugOptions): void {
+		const { label = "Client" } = options ?? {};
+		const { events } = this;
+
+		const normalizedLabel = this.#normalizeLabelBrackets(label);
+		const debugMessage = `${normalizedLabel} ${message}`;
+
+		events.emit(ClientEvents.Debug, debugMessage);
+	}
+
+	async init(): Promise<void> {
+		const { gateway } = this;
+
+		this.debug("Initializing the client and its dependencies...");
+
+		await super.init(this);
+		await gateway.init();
+	}
+}
