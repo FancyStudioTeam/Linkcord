b4506720f2f78147b9c437519c5a40504bbeea5f
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-24 00:02:03 +0100

    refactor(BaseAPI): delete methods

--- src/rest/api/BaseAPI.ts
+++ src/rest/api/BaseAPI.ts
@@ -1,13 +1,5 @@
 import type { Client } from "#client/index.js";
 import type { RESTManager } from "#rest/structures/RESTManager.js";
-import {
-	type MakeDeleteRequestOptions,
-	type MakeGetRequestOptions,
-	type MakePatchRequestOptions,
-	type MakePostRequestOptions,
-	type MakePutRequestOptions,
-	RESTMethod,
-} from "#rest/structures/RESTManager.types.js";
 import { defineReadonlyProperty } from "#utils/functions/defineReadonlyProperty.js";
 
 export class BaseAPI {
@@ -17,50 +9,5 @@ export class BaseAPI {
 	constructor(rest: RESTManager, client: Client) {
 		defineReadonlyProperty(this, "client", client);
 		defineReadonlyProperty(this, "rest", rest);
-	}
-
-	protected delete<Result>(endpoint: string, options?: Omit<MakeDeleteRequestOptions, "method">): Promise<Result> {
-		const { rest } = this;
-
-		return rest.makeRequest<Result>(endpoint, {
-			...options,
-			method: RESTMethod.Delete,
-		});
-	}
-
-	protected async get<Result>(endpoint: string, options?: Omit<MakeGetRequestOptions, "method">): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.makeRequest<Result>(endpoint, {
-			...options,
-			method: RESTMethod.Get,
-		});
-	}
-
-	protected async patch<Result>(endpoint: string, options?: Omit<MakePatchRequestOptions, "method">): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.makeRequest<Result>(endpoint, {
-			...options,
-			method: RESTMethod.Patch,
-		});
-	}
-
-	protected async post<Result>(endpoint: string, options?: Omit<MakePostRequestOptions, "method">): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.makeRequest<Result>(endpoint, {
-			...options,
-			method: RESTMethod.Post,
-		});
-	}
-
-	protected async put<Result>(endpoint: string, options?: Omit<MakePutRequestOptions, "method">): Promise<Result> {
-		const { rest } = this;
-
-		return await rest.makeRequest<Result>(endpoint, {
-			...options,
-			method: RESTMethod.Put,
-		});
 	}
 }
