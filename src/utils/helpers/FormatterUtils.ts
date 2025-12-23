aede49b4690d66b744e1985dffff7403b9104495
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-23 23:18:36 +0100

    chore: things

--- src/utils/helpers/FormatterUtils.ts
+++ src/utils/helpers/FormatterUtils.ts
@@ -1,7 +1,13 @@
 import type { Snowflake } from "#types/index.js";
 import { ONE_SECOND_MILLISECONDS } from "#utils/Constants.js";
 import { isArray, isEnum, isInstanceOf, isNumber, isString } from "./AssertionUtils.js";
-import { CodeBlockLanguage, HeadingLevel, type HeadingLevelsMap, type RecursiveArray, TimestampStyle } from "./FormatterUtils.types.js";
+import {
+	type CodeBlockLanguage,
+	HeadingLevel,
+	type HeadingLevelsMap,
+	type RecursiveArray,
+	TimestampStyle,
+} from "./FormatterUtils.types.js";
 
 const HEX_COLOR_DIGITS = 6;
 const HEXADECIMAL_BASE = 16;
@@ -9,6 +15,9 @@ const HEXADECIMAL_BASE = 16;
 const MAXIMUM_TUPLE_LENGTH = 3;
 const MINIMUM_TUPLE_LENGTH = 2;
 
+/**
+ * @internal
+ */
 function isChatInputCommandTuple(input: unknown): input is string[] {
 	if (!isArray(input)) return false;
 
@@ -20,12 +29,16 @@ function isChatInputCommandTuple(input: unknown): input is string[] {
 	return isValidLength && areAllStrings;
 }
 
+/**
+ * @internal
+ */
 function isHeadingLevel(input: unknown): input is HeadingLevel {
-	if (!isNumber(input)) return false;
-
-	return HeadingLevel[input] !== undefined;
+	return isNumber(input) && HeadingLevel[input] !== undefined;
 }
 
+/**
+ * @internal
+ */
 function listCallback(items: RecursiveArray<string>, startNumber = 1, isMainList = true, indentLevel = 0): string {
 	const indent = " ".repeat(indentLevel);
 	const mark = startNumber ? `${startNumber}.` : "-";
@@ -40,31 +53,30 @@ function listCallback(items: RecursiveArray<string>, startNumber = 1, isMainList
 	return `${indent}${mark} ${items}`;
 }
 
+/**
+ * @internal
+ */
 function normalizeChatInputCommandName(commandName: string | string[]): string {
-	if (isChatInputCommandTuple(commandName)) {
-		return commandName.join(" ");
-	}
-
-	return commandName;
+	return isChatInputCommandTuple(commandName) ? commandName.join(" ") : commandName;
 }
 
-function blockQuote<Content extends string>(content: Content) {
+export function blockQuote<Content extends string>(content: Content) {
 	return `>>> ${content}` as const;
 }
 
-function bold<Content extends string>(content: Content) {
+export function bold<Content extends string>(content: Content) {
 	return `**${content}**` as const;
 }
 
-function channelMention<ChannelId extends Snowflake>(channelId: ChannelId) {
+export function channelMention<ChannelId extends Snowflake>(channelId: ChannelId) {
 	return `<#${channelId}>` as const;
 }
 
-function chatInputCommandMention<CommandName extends Lowercase<string>, CommandId extends Snowflake>(
+export function chatInputCommandMention<CommandName extends Lowercase<string>, CommandId extends Snowflake>(
 	commandName: CommandName,
 	commandId: CommandId,
 ): `</${CommandName}:${CommandId}>`;
-function chatInputCommandMention<
+export function chatInputCommandMention<
 	CommandName extends Lowercase<string>,
 	SubcommandName extends Lowercase<string>,
 	CommandId extends Snowflake,
@@ -75,7 +87,7 @@ function chatInputCommandMention<
 	],
 	commandId: CommandId,
 ): `</${CommandName} ${SubcommandName}:${CommandId}>`;
-function chatInputCommandMention<
+export function chatInputCommandMention<
 	CommandName extends Lowercase<string>,
 	SubcommandGroupName extends Lowercase<string>,
 	SubcommandName extends Lowercase<string>,
@@ -89,7 +101,7 @@ function chatInputCommandMention<
 	commandId: CommandId,
 ): `</${CommandName} ${SubcommandGroupName} ${SubcommandName}:${CommandId}>`;
 
-function chatInputCommandMention(commandName: string | string[], commandId: Snowflake): string {
+export function chatInputCommandMention(commandName: string | string[], commandId: Snowflake): string {
 	const normalizedCommandName = normalizeChatInputCommandName(commandName);
 
 	const commandMention = `</${normalizedCommandName}:${commandId}>`;
@@ -98,32 +110,28 @@ function chatInputCommandMention(commandName: string | string[], commandId: Snow
 	return lowercasedCommandMention;
 }
 
-function codeBlock<Content extends string>(content: Content): `\`\`\`\n${Content}\n\`\`\``;
-function codeBlock<Language extends CodeBlockLanguage, Content extends string>(
+export function codeBlock<Content extends string>(content: Content): `\`\`\`\n${Content}\n\`\`\``;
+export function codeBlock<Language extends CodeBlockLanguage, Content extends string>(
 	language: Language,
 	content: Content,
 ): `\`\`\`${Language}\n${Content}\n\`\`\``;
 
-function codeBlock(languageOrContent: CodeBlockLanguage | string, possibleContent?: string): string {
+export function codeBlock(languageOrContent: CodeBlockLanguage | string, possibleContent?: string): string {
 	if (possibleContent) {
-		if (!isEnum(languageOrContent, CodeBlockLanguage)) {
-			throw new TypeError("First parameter (language) from 'FormatterUtils.codeBlock' must be an enum");
-		}
-
 		return `\`\`\`${languageOrContent}\n${possibleContent}\n\`\`\``;
 	}
 
 	return `\`\`\`\n${languageOrContent}\n\`\`\``;
 }
 
-function email<Username extends string, Domain extends string>(username: Username, domain: Domain): `<${Username}@${Domain}>`;
-function email<Username extends string, Domain extends string>(
+export function email<Username extends string, Domain extends string>(username: Username, domain: Domain): `<${Username}@${Domain}>`;
+export function email<Username extends string, Domain extends string>(
 	username: Username,
 	domain: Domain,
 	headersInit: HeadersInit,
 ): `<${Username}@${Domain}?${string}>`;
 
-function email(username: string, domain: string, headersInit?: HeadersInit): string {
+export function email(username: string, domain: string, headersInit?: HeadersInit): string {
 	const emailBase = `${username}@${domain}`;
 
 	if (headersInit) {
@@ -141,58 +149,57 @@ function email(username: string, domain: string, headersInit?: HeadersInit): str
 	return `<${emailBase}>`;
 }
 
-function everyone() {
+export function everyone() {
 	return "@everyone" as const;
 }
 
-function header<Content extends string>(content: Content): `# ${Content}`;
-function header<Level extends HeadingLevel, Content extends string>(level: Level, content: Content): HeadingLevelsMap<Content>[Level];
+export function header<Content extends string>(content: Content): `# ${Content}`;
+export function header<Level extends HeadingLevel, Content extends string>(
+	level: Level,
+	content: Content,
+): HeadingLevelsMap<Content>[Level];
 
-function header(levelOrContent: HeadingLevel | string, possibleContent?: string): string {
-	if (possibleContent) {
-		if (!isHeadingLevel(levelOrContent)) {
-			throw new TypeError("First parameter (level) from 'FormatterUtils.header' must be an enum");
-		}
-
+export function header(levelOrContent: HeadingLevel | string, possibleContent?: string): string {
+	if (isHeadingLevel(levelOrContent)) {
 		return `${"#".repeat(levelOrContent)} ${possibleContent}`;
 	}
 
 	return `# ${levelOrContent}`;
 }
 
-function here() {
+export function here() {
 	return "@here" as const;
 }
 
-function hexColor(color: number) {
+export function hexColor(color: number) {
 	const hexadecimal = color.toString(HEXADECIMAL_BASE);
 	const hexColor = `#${hexadecimal.padEnd(HEX_COLOR_DIGITS, "0")}` as const;
 
 	return hexColor;
 }
 
-function hideEmbedLink(url: URL): `<${string}>`;
-function hideEmbedLink<Url extends string>(url: Url): `<${Url}>`;
+export function hideEmbedLink(url: URL): `<${string}>`;
+export function hideEmbedLink<Url extends string>(url: Url): `<${Url}>`;
 
-function hideEmbedLink(url: URL | string): string {
-	return `<${url.toString()}>`;
+export function hideEmbedLink(url: URL | string): string {
+	return `<${String(url)}>`;
 }
 
-function hyperlink<Content extends string>(content: Content, url: URL): `[${Content}](${string})`;
-function hyperlink<Content extends string, Url extends string>(content: Content, url: Url): `[${Content}](${Url})`;
-function hyperlink<Content extends string, Title extends string>(
+export function hyperlink<Content extends string>(content: Content, url: URL): `[${Content}](${string})`;
+export function hyperlink<Content extends string, Url extends string>(content: Content, url: Url): `[${Content}](${Url})`;
+export function hyperlink<Content extends string, Title extends string>(
 	content: Content,
 	url: URL,
 	title: Title,
 ): `[${Content}](${string} "${Title}")`;
-function hyperlink<Content extends string, Url extends string, Title extends string>(
+export function hyperlink<Content extends string, Url extends string, Title extends string>(
 	content: Content,
 	url: Url,
 	title: Title,
 ): `[${Content}](${Url} "${Title}")`;
 
-function hyperlink(content: string, url: URL | string, possibleTitle?: string): string {
-	const urlString = url.toString();
+export function hyperlink(content: string, url: URL | string, possibleTitle?: string): string {
+	const urlString = String(url);
 
 	if (isString(possibleTitle)) {
 		return `[${content}](${urlString} "${possibleTitle}")`;
@@ -201,29 +208,29 @@ function hyperlink(content: string, url: URL | string, possibleTitle?: string): 
 	return `[${content}](${urlString})`;
 }
 
-function inlineCode<Content extends string>(content: Content) {
+export function inlineCode<Content extends string>(content: Content) {
 	return `\`${content}\`` as const;
 }
 
-function italic<Content extends string>(content: Content) {
+export function italic<Content extends string>(content: Content) {
 	return `*${content}*` as const;
 }
 
-function linkedRoleMention<LinkedRoleId extends Snowflake>(linkedRoleId: LinkedRoleId) {
+export function linkedRoleMention<LinkedRoleId extends Snowflake>(linkedRoleId: LinkedRoleId) {
 	return `<id:linked-roles:${linkedRoleId}>` as const;
 }
 
-function messageLink<ChannelId extends Snowflake, MessageId extends Snowflake>(
+export function messageLink<ChannelId extends Snowflake, MessageId extends Snowflake>(
 	channelId: ChannelId,
 	messageId: MessageId,
 ): `https://discord.com/channels/@me/${ChannelId}/${MessageId}`;
-function messageLink<GuildId extends Snowflake, ChannelId extends Snowflake, MessageId extends Snowflake>(
+export function messageLink<GuildId extends Snowflake, ChannelId extends Snowflake, MessageId extends Snowflake>(
 	guildId: GuildId,
 	channelId: ChannelId,
 	messageId: MessageId,
 ): `https://discord.com/channels/${GuildId}/${ChannelId}/${MessageId}`;
 
-function messageLink(channelOrGuildId: Snowflake, channelOrMessageId: Snowflake, possibleMessageId?: Snowflake): string {
+export function messageLink(channelOrGuildId: Snowflake, channelOrMessageId: Snowflake, possibleMessageId?: Snowflake): string {
 	if (possibleMessageId) {
 		return `https://discord.com/channels/${channelOrGuildId}/${channelOrMessageId}/${possibleMessageId}`;
 	}
@@ -231,115 +238,93 @@ function messageLink(channelOrGuildId: Snowflake, channelOrMessageId: Snowflake,
 	return `https://discord.com/channels/@me/${channelOrGuildId}/${channelOrMessageId}`;
 }
 
-function orderedList(items: RecursiveArray<string>, startNumber = 1): string {
+export function orderedList(items: RecursiveArray<string>, startNumber = 1): string {
 	return listCallback(items, Math.max(startNumber, 1));
 }
 
-function phoneNumber<Number extends `+${string}`>(number: Number) {
+export function phoneNumber<Number extends `+${string}`>(number: Number) {
 	if (!number.startsWith("+")) {
-		throw new TypeError("First parameter (number) from 'FormatterUtils.phoneNumber' must start with '+'");
+		throw new TypeError("First parameter (number) from phoneNumber must start with '+'");
 	}
 
 	return `<${number}>` as const;
 }
 
-function quote<Content extends string>(content: Content) {
+export function quote<Content extends string>(content: Content) {
 	return `> ${content}` as const;
 }
 
-function roleMention<RoleId extends Snowflake>(roleId: RoleId) {
+export function roleMention<RoleId extends Snowflake>(roleId: RoleId) {
 	return `<@&${roleId}>` as const;
 }
 
-function shrug() {
+export function shrug() {
 	return "¯\\_(ツ)_/¯" as const;
 }
 
-function spoiler<Content extends string>(content: Content) {
+export function spoiler<Content extends string>(content: Content) {
 	return `||${content}||` as const;
 }
 
-function strikethrough<Content extends string>(content: Content) {
+export function strikethrough<Content extends string>(content: Content) {
 	return `~~${content}~~` as const;
 }
 
-function subtext<Content extends string>(content: Content) {
+export function subtext<Content extends string>(content: Content) {
 	return `-# ${content}` as const;
 }
 
-function tableFlip() {
+export function tableFlip() {
 	return "(╯°□°)╯︵ ┻━┻" as const;
 }
 
-function underline<Content extends string>(content: Content) {
+export function underline<Content extends string>(content: Content) {
 	return `__${content}__` as const;
 }
 
-function unflip() {
+export function unflip() {
 	return "┬─┬ノ( º _ ºノ)" as const;
 }
 
-function unixTimestamp(date?: Date): `<t:${string}>`;
-function unixTimestamp<Style extends TimestampStyle>(date: Date, style: Style): `<t:${string}:${Style}>`;
-function unixTimestamp<Seconds extends number>(seconds: Seconds): `<t:${Seconds}>`;
-function unixTimestamp<Seconds extends number, Style extends TimestampStyle>(seconds: Seconds, style: Style): `<t:${Seconds}:${Style}>`;
+export function unixTimestamp(date?: Date): `<t:${string}>`;
+export function unixTimestamp<Style extends TimestampStyle>(date: Date, style: Style): `<t:${string}:${Style}>`;
+export function unixTimestamp<Seconds extends number>(seconds: Seconds): `<t:${Seconds}>`;
+export function unixTimestamp<Seconds extends number, Style extends TimestampStyle>(
+	seconds: Seconds,
+	style: Style,
+): `<t:${Seconds}:${Style}>`;
 
-function unixTimestamp(dateOrSeconds?: Date | number, possibleStyle?: TimestampStyle): string {
+export function unixTimestamp(dateOrSeconds?: Date | number, possibleStyle?: TimestampStyle): string {
 	if (isInstanceOf(dateOrSeconds, Date)) {
 		const dateTime = dateOrSeconds.getTime();
 		const unixTimestamp = Math.floor(dateTime / ONE_SECOND_MILLISECONDS);
 
-		if (isEnum(possibleStyle, TimestampStyle)) {
+		if (possibleStyle) {
+			if (!isEnum(possibleStyle, TimestampStyle)) {
+				throw new TypeError("Second parameter (style) from unixTimestamp must be an enum");
+			}
+
 			return `<t:${unixTimestamp}:${possibleStyle}>`;
 		}
 
 		return `<t:${unixTimestamp}>`;
 	}
 
-	if (isEnum(possibleStyle, TimestampStyle)) {
+	if (possibleStyle) {
+		if (!isEnum(possibleStyle, TimestampStyle)) {
+			throw new TypeError("Second parameter (style) from unixTimestamp must be an enum");
+		}
+
 		return `<t:${dateOrSeconds}:${possibleStyle}>`;
 	}
 
 	return `<t:${dateOrSeconds}>`;
 }
 
-function unorderedList(items: RecursiveArray<string>): string {
+export function unorderedList(items: RecursiveArray<string>): string {
 	return listCallback(items);
 }
 
-function userMention<UserId extends Snowflake>(userId: UserId) {
+export function userMention<UserId extends Snowflake>(userId: UserId) {
 	return `<@${userId}>` as const;
 }
-
-export const FormatterUtils = Object.freeze({
-	blockQuote,
-	bold,
-	channelMention,
-	chatInputCommandMention,
-	codeBlock,
-	email,
-	everyone,
-	header,
-	here,
-	hexColor,
-	hideEmbedLink,
-	hyperlink,
-	inlineCode,
-	italic,
-	linkedRoleMention,
-	messageLink,
-	orderedList,
-	phoneNumber,
-	quote,
-	roleMention,
-	shrug,
-	spoiler,
-	strikethrough,
-	subtext,
-	tableFlip,
-	underline,
-	unflip,
-	unixTimestamp,
-	unorderedList,
-	userMention,
-});
