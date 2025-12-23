cbb560bab3d997f5ea4b0affc3ae817e4c56a06a
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-23 16:12:26 +0100

    test: update test for `validate`

--- src/utils/functions/__tests__/validate.test.ts
+++ src/utils/functions/__tests__/validate.test.ts
@@ -1,32 +1,8 @@
-import { array, string } from "zod";
-import { ValidationError } from "#utils/errors/ValidationError.js";
 import { validate } from "../validate.js";
 
-const ARRAY_SCHEMA = array(string()).min(1);
-
-describe("Function: validate", () => {
-	describe("GIVEN valid parameters", () => {
-		describe("WHEN calling validate", () => {
-			it("THEN should validate the provided data with the specified schema", () => {
-				expect(() => validate(ARRAY_SCHEMA, null)).toThrow(ValidationError);
-				expect(() => validate(ARRAY_SCHEMA, [])).toThrow(ValidationError);
-				expect(
-					validate(ARRAY_SCHEMA, [
-						"Hello",
-					]),
-				).toStrictEqual([
-					"Hello",
-				]);
-			});
-		});
-	});
-
-	describe("GIVEN invalid parameters", () => {
-		describe("WHEN calling validate", () => {
-			it("THEN a TypeError is thrown", () => {
-				// @ts-expect-error
-				expect(() => validate(null)).toThrow("First parameter (schema) from 'validate' must be an instance of 'ZodType'");
-			});
-		});
+describe("validate", () => {
+	it("Should throw an error if the provided parameters are invalid", () => {
+		// @ts-expect-error
+		expect(() => validate(null)).toThrow(/must be an instance of/);
 	});
 });
