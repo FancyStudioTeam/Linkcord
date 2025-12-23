98958f221ad2dcc48f172a602d2c9d74cbe01ef5
LonelyDeathVGX <lonelydeathvgx@gmail.com>
2025-12-23 17:01:11 +0100

    test(getCurrentVersion): use `SEMVER_REGEX`

--- src/utils/functions/__tests__/getCurrentVersion.spec.ts
+++ src/utils/functions/__tests__/getCurrentVersion.spec.ts
@@ -1,9 +1,13 @@
 import { getCurrentVersion } from "../getCurrentVersion.js";
 
-const VERSION_REGEX = /^\d+\.\d+\.\d+$/;
+/**
+ * @see https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+ */
+const SEMVER_REGEX =
+	/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 
 describe("getCurrentVersion", () => {
-	it("Should return the current version matching the version regex", () => {
-		expect(getCurrentVersion()).toMatch(VERSION_REGEX);
+	it("Should return the current version matching the Semantic Versioning regex", () => {
+		expect(getCurrentVersion()).toMatch(SEMVER_REGEX);
 	});
 });
