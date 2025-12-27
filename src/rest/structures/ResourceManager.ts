52f26d8be1ffcfcc791df4eded8ea03539b7377d
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-27 16:48:45 +0100

    feat(ResourceManager): add `guilds`

--- src/rest/structures/ResourceManager.ts
+++ src/rest/structures/ResourceManager.ts
@@ -2,6 +2,7 @@ import type { Client } from '#client/index.js';
 import { ApplicationsResource } from '#rest/resources/ApplicationsResource.js';
 import { ChannelsResource } from '#rest/resources/ChannelsResource.js';
 import { GatewayResource } from '#rest/resources/GatewayResource.js';
+import { GuildsResource } from '#rest/resources/GuildsResource.js';
 import { VoiceResource } from '#rest/resources/VoiceResource.js';
 import { defineReadonlyProperty } from '#utils/functions/defineReadonlyProperty.js';
 import type { RESTManager } from './RESTManager.js';
@@ -10,12 +11,14 @@ export class ResourceManager {
 	declare readonly applications: ApplicationsResource;
 	declare readonly channels: ChannelsResource;
 	declare readonly gateway: GatewayResource;
+	declare readonly guilds: GuildsResource;
 	declare readonly voice: VoiceResource;
 
 	constructor(rest: RESTManager, client: Client) {
 		defineReadonlyProperty(this, 'applications', new ApplicationsResource(rest, client));
 		defineReadonlyProperty(this, 'channels', new ChannelsResource(rest, client));
 		defineReadonlyProperty(this, 'gateway', new GatewayResource(rest, client));
+		defineReadonlyProperty(this, 'guilds', new GuildsResource(rest, client));
 		defineReadonlyProperty(this, 'voice', new VoiceResource(rest, client));
 	}
 }
