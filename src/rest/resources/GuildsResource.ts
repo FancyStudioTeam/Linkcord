4ea2570f33f8af30ce98bbd8d99b216212e67500
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-27 19:24:08 +0100

    feat(GuildsResource): add `deleteGuildMemberRole` method

--- src/rest/resources/GuildsResource.ts
+++ src/rest/resources/GuildsResource.ts
@@ -5,6 +5,19 @@ import { ResourceBase } from './ResourceBase.js';
 
 export class GuildsResource extends ResourceBase {
 	/**
+	 * @see https://discord.com/developers/docs/resources/guild#remove-guild-member-role
+	 */
+	async deleteGuildMemberRole(guildId: Snowflake, memberId: Snowflake, roleId: Snowflake, reason?: string): Promise<void> {
+		const { rest } = this;
+		const guildMemberRoleResponseData = await rest.makeRequest<void>(GUILD_MEMBER_ROLE(guildId, memberId, roleId), {
+			method: RESTMethod.Delete,
+			reason,
+		});
+
+		return guildMemberRoleResponseData;
+	}
+
+	/**
 	 * @see https://discord.com/developers/docs/resources/guild#add-guild-member-role
 	 */
 	async putGuildMemberRole(guildId: Snowflake, memberId: Snowflake, roleId: Snowflake, reason?: string): Promise<void> {
