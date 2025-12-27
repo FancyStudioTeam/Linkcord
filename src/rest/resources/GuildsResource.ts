ae503434c41bbda793109eb92279c7add190646b
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-27 16:40:43 +0100

    feat(GuildsResource): add `reason`

--- src/rest/resources/GuildsResource.ts
+++ src/rest/resources/GuildsResource.ts
@@ -7,11 +7,11 @@ export class GuildsResource extends ResourceBase {
 	/**
 	 * @see https://discord.com/developers/docs/resources/guild#add-guild-member-role
 	 */
-	async putGuildMemberRole(guildId: Snowflake, memberId: Snowflake, roleId: Snowflake): Promise<void> {
+	async putGuildMemberRole(guildId: Snowflake, memberId: Snowflake, roleId: Snowflake, reason?: string): Promise<void> {
 		const { rest } = this;
 		const guildMemberRoleResponseData = await rest.makeRequest<void>(GUILD_MEMBER_ROLE(guildId, memberId, roleId), {
-			body: undefined,
 			method: RESTMethod.Put,
+			reason,
 		});
 
 		return guildMemberRoleResponseData;
