db40b8932afc4da303eff3db6dd890eb6b1fd3e1
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-30 15:54:35 +0100

    refactor(Teams)!: use `Raw` prefix instead of `API`

--- src/types/resources/Teams/structures/raw.ts
+++ src/types/resources/Teams/structures/raw.ts
@@ -5,10 +5,10 @@ import type { TeamMemberRoleType, TeamMembershipState } from '../enums.js';
 /**
  * @see https://discord.com/developers/docs/topics/teams#data-models-team-object
  */
-export interface APITeam {
+export interface RawTeam {
 	icon: string | null;
 	id: Snowflake;
-	members: APITeamMember[];
+	members: RawTeamMember[];
 	name: string;
 	owner_user_id: Snowflake;
 }
@@ -16,7 +16,7 @@ export interface APITeam {
 /**
  * @see https://discord.com/developers/docs/topics/teams#data-models-team-member-object
  */
-export interface APITeamMember {
+export interface RawTeamMember {
 	membership_state: TeamMembershipState;
 	role: TeamMemberRoleType;
 	team_id: Snowflake;
