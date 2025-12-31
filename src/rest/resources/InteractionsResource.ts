df4e3d6183378f0d676afd88f057a0843b51f264
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2025-12-31 17:50:13 +0100

    feat(InteractionsResource)!: `createInteractionResponse`

--- src/rest/resources/InteractionsResource.ts
+++ src/rest/resources/InteractionsResource.ts
@@ -1,24 +1,40 @@
-// import type { CreateInteractionResponseOptions, Snowflake } from '#types/index.js';
+import { INTERACTION_CALLBACK } from '#rest/endpoints/Endpoints.js';
+import { RESTMethod } from '#rest/structures/RESTManager.types.js';
+import { serializeCreateInteractionResponseOptions } from '#transformers/Interactions/REST.js';
+import type { CreateInteractionResponseOptions, Snowflake } from '#types/index.js';
 import { ResourceBase } from './ResourceBase.js';
 
 export class InteractionsResource extends ResourceBase {
 	/**
 	 * @see https://discord.com/developers/docs/interactions/receiving-and-responding#create-interaction-response
 	 */
-	/*async createInteractionResponse(
-		_interactionId: Snowflake,
-		_interactionToken: string,
+	async createInteractionResponse(
+		interactionId: Snowflake,
+		interactionToken: string,
 		options: CreateInteractionResponseOptions,
-	): Promise<undefined> {
+	): Promise<void> {
 		const { rest } = this;
-		const { type, withResponse } = options;
+		const { withResponse } = options;
 
 		const urlSearchParams = new URLSearchParams();
+		const { size: urlSearchParamsSize } = urlSearchParams;
 
 		if (withResponse) {
 			urlSearchParams.append('with_response', String(withResponse));
 		}
 
-		return;
-	}*/
+		const serializedOptions = serializeCreateInteractionResponseOptions(options);
+		let endpoint = INTERACTION_CALLBACK(interactionId, interactionToken);
+
+		if (urlSearchParamsSize > 0) {
+			endpoint += `/?${urlSearchParams.toString()}`;
+		}
+
+		const interactionCallbackResponseData = await rest.makeRequest<void>(endpoint, {
+			body: JSON.stringify(serializedOptions),
+			method: RESTMethod.Post,
+		});
+
+		return interactionCallbackResponseData;
+	}
 }
--- src/transformers/Interactions/REST.ts
+++ src/transformers/Interactions/REST.ts
@@ -1,1 +1,60 @@
-// export function serializeCreate
+import { serializeMessageComponentsArray } from '#transformers/Components/Serializer.js';
+import { normalizeMessageFlags } from '#transformers/Messages/REST.js';
+import { serializeEmbedsArray } from '#transformers/Messages/Serializer.js';
+import {
+	type CreateInteractionResponseOptions,
+	type CreateMessageInteractionResponseOptions,
+	InteractionCallbackType,
+	type RawCreateInteractionResponseOptions,
+	type RawCreateMessageInteractionResponseOptions,
+} from '#types/index.js';
+import { isUndefined } from '#utils/helpers/AssertionUtils.js';
+
+export function serializeCreateInteractionResponseOptions(
+	createInteractionResponseOptions: CreateInteractionResponseOptions,
+): RawCreateInteractionResponseOptions {
+	const { type } = createInteractionResponseOptions;
+
+	switch (type) {
+		case InteractionCallbackType.ApplicationCommandAutocompleteResult:
+		case InteractionCallbackType.LaunchActivity:
+		case InteractionCallbackType.Modal:
+		case InteractionCallbackType.Pong:
+			throw new Error('Interaction callback type not handled');
+		default:
+			return serializeCreateMessageInteractionResponseOptions(createInteractionResponseOptions);
+	}
+}
+
+export function serializeCreateMessageInteractionResponseOptions(
+	createMessageInteractionResponseOptions: CreateMessageInteractionResponseOptions,
+): RawCreateMessageInteractionResponseOptions {
+	const { data, type } = createMessageInteractionResponseOptions;
+	const { components, content, embeds, flags, tts } = data;
+	const rawCreateMessageInteractionResponseOptions: RawCreateMessageInteractionResponseOptions = {
+		data: {},
+		type,
+	};
+
+	if (!isUndefined(components)) {
+		rawCreateMessageInteractionResponseOptions.data.components = serializeMessageComponentsArray(components);
+	}
+
+	if (!isUndefined(content)) {
+		rawCreateMessageInteractionResponseOptions.data.content = content;
+	}
+
+	if (!isUndefined(embeds)) {
+		rawCreateMessageInteractionResponseOptions.data.embeds = serializeEmbedsArray(embeds);
+	}
+
+	if (!isUndefined(flags)) {
+		rawCreateMessageInteractionResponseOptions.data.flags = normalizeMessageFlags(flags);
+	}
+
+	if (!isUndefined(tts)) {
+		rawCreateMessageInteractionResponseOptions.data.tts = tts;
+	}
+
+	return rawCreateMessageInteractionResponseOptions;
+}
