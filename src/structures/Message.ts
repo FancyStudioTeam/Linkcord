d9994fee04c63b930cc33af66267ed54275b785f
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-02 23:15:35 +0100

    refactor(Message)!: refactor `Message` structure

--- src/structures/Message.ts
+++ src/structures/Message.ts
@@ -6,11 +6,14 @@ import { isUndefined } from '#utils/helpers/AssertionUtils.js';
 import { type If, messageLink } from '#utils/index.js';
 import { Base } from './Base.js';
 import type { Guild } from './Guild.js';
+import { User } from './User.js';
 
 /**
  * @see https://discord.com/developers/docs/resources/message#message-object-message-structure
  */
-export class Message<InGuild extends boolean = boolean> extends Base {
+export class Message<InGuild extends boolean = false> extends Base {
+	/** The author of the message. */
+	readonly author: User;
 	/** The ID of the channel where the message was sent. */
 	readonly channelId: Snowflake;
 	/** The ID of the message. */
@@ -22,47 +25,37 @@ export class Message<InGuild extends boolean = boolean> extends Base {
 	content: string;
 	/** The embeds of the message. */
 	embeds: Embed[];
-	/** The ID of the guild, if any. */
-	guildId: If<InGuild, Snowflake>;
+	/** The ID of the guild where the message was sent. */
+	guildId: GuildIdCondition<InGuild>;
 
-	constructor(client: Client, data: APIMessage) {
+	constructor(client: Client, rawMessage: APIMessage) {
 		super(client);
 
-		const { channel_id, components, content, embeds, id } = data;
+		const { author, channel_id, components, content, embeds, id } = rawMessage;
 
+		this.author = new User(client, author);
 		this.channelId = channel_id;
 		this.components = deserializeMessageComponentsArray(components);
 		this.content = content;
 		this.embeds = deserializeEmbedsArray(embeds);
-		this.guildId = null as If<InGuild, Snowflake>;
+		this.guildId = null as GuildIdCondition<InGuild>;
 		this.id = id;
-		this.patch(data);
+		this.patch(rawMessage);
 	}
 
-	/** The guild where the message was sent, if any. */
-	get guild(): If<InGuild, Guild> {
-		const { client, guildId } = this;
-		const {
-			cache: { guilds },
-		} = client;
-
-		// @ts-expect-error
-		const cachedGuild = guilds.get(String(guildId));
-
-		if (!cachedGuild) {
-			throw new TypeError(`Unable to get 'Guild' in message '${this.id}'. Guild is not cached.`);
-		}
-
-		return cachedGuild as If<InGuild, Guild>;
-	}
-
-	/** The formatted link to the message. */
+	/**
+	 * The formatted link to the message.
+	 */
 	get messageLink() {
 		return messageLink(this.channelId, this.id);
 	}
 
-	protected patch(data?: Partial<APIMessage & GatewayDispatchMessageCreateEventPayload>): void {
-		const { components, content, embeds, guild_id } = data ?? {};
+	/**
+	 * Patches the current {@link Message} instance with the provided
+	 * {@link RawMessage} structure.
+	 */
+	protected patch(rawMessage: Partial<APIMessage & GatewayDispatchMessageCreateEventPayload>): void {
+		const { components, content, embeds, guild_id } = rawMessage;
 
 		if (!isUndefined(components)) {
 			this.components = deserializeMessageComponentsArray(components);
@@ -77,11 +70,53 @@ export class Message<InGuild extends boolean = boolean> extends Base {
 		}
 
 		if (!isUndefined(guild_id)) {
-			this.guildId = guild_id as If<InGuild, Snowflake>;
+			this.guildId = guild_id as GuildIdCondition<InGuild>;
 		}
 	}
 
+	/**
+	 * Gets the {@link Guild} instance associated with the message.
+	 *
+	 * @param required - Whether the {@link Guild} instance must be present.
+	 */
+	async guild(required?: false): Promise<GuildCondition<InGuild> | null>;
+
+	/**
+	 * Gets the {@link Guild} instance associated with the message.
+	 *
+	 * @param required - Whether the {@link Guild} instance must be present.
+	 *
+	 * @remarks
+	 * This method throws an error if {@link Guild} is not cached.
+	 */
+	async guild(required: true): Promise<GuildCondition<InGuild>>;
+
+	// biome-ignore lint/suspicious/useAwait: (x)
+	async guild(required?: boolean): Promise<Guild | null> {
+		const { client, guildId } = this;
+		const { cache } = client;
+		const { guilds } = cache;
+
+		if (!guildId) {
+			throw new TypeError(`Unable to get 'Guild' without 'guildId'`);
+		}
+
+		const cachedGuild = guilds.get(guildId);
+
+		if (!cachedGuild && required) {
+			throw new TypeError(`Unable to get 'Guild' in message '${this.id}'. Guild is not cached.`);
+		}
+
+		return cachedGuild ?? null;
+	}
+
+	/**
+	 * Checks whether the message was sent in a guild.
+	 */
 	inGuild(): this is Message<true> {
 		return Boolean(this.guildId);
 	}
 }
+
+export type GuildCondition<InGuild extends boolean> = If<InGuild, Guild, null>;
+export type GuildIdCondition<InGuild extends boolean> = If<InGuild, Snowflake, null>;
