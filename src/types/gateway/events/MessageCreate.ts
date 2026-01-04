88a6da421e76fa261b40d80be976e5c71a792196
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-04 22:25:22 +0100

    fix(types): use `RawMessage`

--- src/types/gateway/events/MessageCreate.ts
+++ src/types/gateway/events/MessageCreate.ts
@@ -1,12 +1,12 @@
 import type { Snowflake } from '#types/miscellaneous/discord.js';
-import type { APIMessage } from '#types/resources/index.js';
+import type { RawMessage } from '#types/resources/index.js';
 import type { GatewayDispatchEvents } from '../enums.js';
 import type { GatewayDispatchEventBase } from './Dispatch.js';
 
 /**
  * @see https://discord.com/developers/docs/events/gateway-events#message-create-message-create-extra-fields
  */
-export interface GatewayDispatchMessageCreateEventPayload extends APIMessage {
+export interface GatewayDispatchMessageCreateEventPayload extends RawMessage {
 	guild_id?: Snowflake;
 }
 
