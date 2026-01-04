ea8ffd23c7b325d9937e5f839abe8ff19ebfef4a
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-04 23:27:42 +0100

    chore: emit a warning when calling `patch` from `ChatInputApplicationCommandInteraction`

--- src/structures/ChatInputApplicationCommandInteraction.ts
+++ src/structures/ChatInputApplicationCommandInteraction.ts
@@ -1,8 +1,20 @@
+import { emitWarning } from 'node:process';
 import { ApplicationCommandInteractionBase } from './ApplicationCommandInteractionBase.js';
 
 /**
  * @see https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-object-interaction-structure
  */
 export class ChatInputApplicationCommandInteraction extends ApplicationCommandInteractionBase {
-	protected patch(): void {}
+	protected patch(): void {
+		// biome-ignore lint/suspicious/noShadowRestrictedNames: (x)
+		const { constructor } = this;
+		const { name } = constructor;
+
+		const warningMessage = `All properties of '${name}' are readonly. Calling '${name}.patch()' will have no effect.`;
+
+		emitWarning(warningMessage, {
+			code: 'PATCH_METHOD',
+			type: 'Patch Method Warning',
+		});
+	}
 }
