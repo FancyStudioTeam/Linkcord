6abeb35ba892ff651e719ad4827cdd574785b31f
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-04 22:15:12 +0100

    feat(Resolvables): add `Shared`

--- src/types/resolvables/Messages.ts
+++ src/types/resolvables/Messages.ts
@@ -1,9 +1,14 @@
+import type { File } from '#rest/index.js';
+import type { Message } from '#structures/Message.js';
 import type { Snowflake } from '#types/miscellaneous/discord.js';
 import type { Embed, MessageComponents, MessageFlags, MessagePoll } from '#types/resources/index.js';
 import type { BitField } from '#utils/index.js';
 
+export type BulkMessageResolvable = Message | Snowflake;
+
 export type MessageComponentResolvable = MessageComponents;
 export type MessageEmbedResolvable = Embed;
+export type MessageFileResolvable = File;
 export type MessageFlagsResolvable = BitField | MessageFlags | MessageFlags[];
 export type MessagePollResolvable = MessagePoll;
 export type MessageStickerResolvable = Snowflake;
---
+++ src/types/resolvables/Shared.ts
@@ -0,0 +1,3 @@
+import type { ISO8601Date } from '#types/miscellaneous/discord.js';
+
+export type DateResolvable = Date | ISO8601Date | number;
