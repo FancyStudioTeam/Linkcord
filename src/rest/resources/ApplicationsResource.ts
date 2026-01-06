e55801419efefe934783ba91e32102c0759b3cec
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-06 03:59:10 +0100

    fix: use correct types

--- src/rest/resources/ApplicationsResource.ts
+++ src/rest/resources/ApplicationsResource.ts
@@ -1,13 +1,13 @@
 import { APPLICATION_COMMANDS_ENDPOINT } from '#rest/endpoints/Endpoints.js';
 import { RESTContentType, RESTMethod } from '#rest/structures/RESTManager.types.js';
-import type { CreateApplicationCommandOptions, RESTPutAPIApplicationCommandsJSONParams, Snowflake } from '#types/index.js';
+import type { CreateApplicationCommand, RawApplicationCommand, Snowflake } from '#types/index.js';
 import { ResourceBase } from './ResourceBase.js';
 
 export class ApplicationsResource extends ResourceBase {
 	/**
 	 * @see https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
 	 */
-	async bulkOverwriteApplicationCommands(applicationId: Snowflake, options: CreateApplicationCommandOptions[]): Promise<unknown[]> {
+	async bulkOverwriteApplicationCommands(applicationId: Snowflake, options: CreateApplicationCommand[]): Promise<unknown[]> {
 		const { rest } = this;
 		const body: unknown[] = [];
 
@@ -20,7 +20,7 @@ export class ApplicationsResource extends ResourceBase {
 			});
 		}
 
-		await rest.makeRequest<RESTPutAPIApplicationCommandsJSONParams>(APPLICATION_COMMANDS_ENDPOINT(applicationId), {
+		await rest.makeRequest<RawApplicationCommand[]>(APPLICATION_COMMANDS_ENDPOINT(applicationId), {
 			body: JSON.stringify(body),
 			contentType: RESTContentType.ApplicationJSON,
 			method: RESTMethod.Put,
