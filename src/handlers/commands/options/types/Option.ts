1c70ff8f95fb0fff5309f17dae40b29b7d3ac227
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-13 02:42:29 +0100

    refactor!: parse options

--- src/handlers/commands/options/types/Option.ts
+++ src/handlers/commands/options/types/Option.ts
@@ -1,13 +1,17 @@
+import type { User } from '#structures/User.js';
 import { ApplicationCommandOptionType } from '#types/index.js';
 import type { If } from '#utils/index.js';
-import type { createStringOption } from '../functions/createStringOption.js';
+import type { StringOptionData } from '../functions/createStringOption.types.js';
+import type { UserOptionData } from '../functions/createUserOption.types.js';
 
-export type OptionsReducer<Input extends Record<string, OptionData>> = {
-	readonly [Key in keyof Input]: OptionsDataMap<Input[Key]['required']>[ApplicationCommandOptionType.String];
+export type ParsedOptions<Input extends Record<string, OptionData>> = {
+	readonly [Key in keyof Input]: OptionDataMap<Input[Key]['required']>[Input[Key]['type']];
 };
 
-export type OptionData = ReturnType<typeof createStringOption>;
+export type OptionData = StringOptionData<boolean> | UserOptionData<boolean>;
 
-export type OptionsDataMap<Required extends boolean> = {
+export type OptionDataMap<Required extends boolean> = {
+	[ApplicationCommandOptionType.Boolean]: If<Required, boolean, boolean | undefined>;
 	[ApplicationCommandOptionType.String]: If<Required, string, string | undefined>;
+	[ApplicationCommandOptionType.User]: If<Required, User, User | undefined>;
 };
