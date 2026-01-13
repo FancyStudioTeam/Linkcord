1ff36cdbbdf77e1f1e038d3ead11fe2536bb5151
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-13 02:33:37 +0100

    feat: use parsed options type

--- src/handlers/commands/structures/ChatInputCommandHandler/ChatInputCommandHandler.types.ts
+++ src/handlers/commands/structures/ChatInputCommandHandler/ChatInputCommandHandler.types.ts
@@ -1,24 +1,23 @@
 import type { Client } from '#client/index.js';
 import type { GatewayShard } from '#gateway/index.js';
+import type { OptionData, ParsedOptions } from '#handlers/commands/options/index.js';
 import type { ChatInputApplicationCommandInteraction } from '#structures/ChatInputApplicationCommandInteraction.js';
 import type { CreateChatInputApplicationCommand } from '#types/index.js';
 import type { NonAbstractConstructor } from '#utils/index.js';
 import type { ChatInputCommandHandler } from './ChatInputCommandHandler.js';
 
-export interface ChatInputCommandContext<
-	Data extends ChatInputCommandContextData = {
-		options: Record<string, never>;
-	},
-> {
+export interface ChatInputCommandContext<Data extends ChatInputCommandContextData> {
 	client: Client;
 	gatewayShard: GatewayShard;
-	options: Data['options'];
+	options: ParsedOptions<NormalizeOptions<Data['options']>>;
 	interaction: ChatInputApplicationCommandInteraction;
 }
 
 export interface ChatInputCommandContextData {
-	options?: unknown;
+	options?: Record<string, OptionData>;
 }
 
 export type ChatInputCommandHandlerConstructor = NonAbstractConstructor<ChatInputCommandHandler>;
 export type ChatInputCommandHandlerDeclareOptions = Omit<CreateChatInputApplicationCommand, 'options' | 'type'>;
+
+export type NormalizeOptions<OptionsObject> = OptionsObject extends undefined ? Record<string, never> : OptionsObject;
