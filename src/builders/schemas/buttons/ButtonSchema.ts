acc33c222f96327b7273133662dbaf5e2ef4f91f
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-24 18:42:22 +0100

    chore: delete old structures

--- src/builders/schemas/buttons/ButtonSchema.ts
+++
@@ -1,7 +0,0 @@
-import { boolean, literal } from 'zod';
-import { ComponentType } from '#types/index.js';
-import { IdSchema } from '../shared/IdSchema.js';
-
-export const ButtonDisabledSchema = boolean();
-export const ButtonIdSchema = IdSchema;
-export const ButtonTypeSchema = literal(ComponentType.Button);
