62ca0d4a601dbb11d32718cc69804357dc7014ef
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-01-28 19:07:19 +0100

    chore: things

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-export * from './client/index.js';
 export * from './configuration/index.js';
-export * from './gateway/index.js';
-export * from './rest/index.js';
-export * from './structures/index.js';
